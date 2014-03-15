92378a9a19b51f6355fd2433c449af3884b8ab08
snicolet <cassio@free.fr>
2014-03-15 01:30:36 +0100

    Auto-correct the CUT/ALL status of a node after a few moves without a fail-high (take 3 : R = ONE_PLY / 2)

--- src/search.cpp
+++ src/search.cpp
@@ -885,7 +885,7 @@ moves_loop: // When in check and at SpNode search starts from here
           &&  move != ss->killers[0]
           &&  move != ss->killers[1])
       {
-          ss->reduction = reduction<PvNode>(improving, depth, moveCount) + 3 * ONE_PLY / 4 ;
+          ss->reduction = reduction<PvNode>(improving, depth, moveCount) + ONE_PLY / 2 ;
 
           if (!PvNode && cutNode)
               ss->reduction += ONE_PLY;
