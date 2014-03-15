13da28f1a1d010e51eb2abf4dbef856d09aaec96
snicolet <cassio@free.fr>
2014-03-15 01:35:54 +0100

    (take 4 : R = ONE_PLY / 4)

--- src/search.cpp
+++ src/search.cpp
@@ -885,7 +885,7 @@ moves_loop: // When in check and at SpNode search starts from here
           &&  move != ss->killers[0]
           &&  move != ss->killers[1])
       {
-          ss->reduction = reduction<PvNode>(improving, depth, moveCount) + ONE_PLY / 2 ;
+          ss->reduction = reduction<PvNode>(improving, depth, moveCount) + ONE_PLY / 4 ;
 
           if (!PvNode && cutNode)
               ss->reduction += ONE_PLY;
