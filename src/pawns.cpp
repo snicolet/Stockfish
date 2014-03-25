965b1d53c1f44ec22da7154a3c22eb5a30b2df94
snicolet <cassio@free.fr>
2014-03-25 01:53:08 +0100

    not bad either

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -170,8 +170,8 @@ namespace {
             e->passedPawns[Us] |= s;
 
         // Score this pawn
-        if (isolated && !passed)
-            value -= Isolated[opposed][f];
+        if (isolated)
+            value -= (passed ? Isolated[opposed][f] / 2 : Isolated[opposed][f]);
         
         if (unsupported && !isolated)
             value -= UnsupportedPawnPenalty;
