abab322d116035735ca579737dca94660fbeb9db
snicolet <cassio@free.fr>
2014-04-11 22:52:26 +0200

    Higher penalty

--- src/material.cpp
+++ src/material.cpp
@@ -133,7 +133,7 @@ namespace {
         int b = pieceCount[Them][BISHOP] - pieceCount[Us][BISHOP];
 
         if ((n == 2 && b == 1) || (n == 1 && b == 2))
-            value += 66 * (pieceCount[Us][PAWN] - pieceCount[Them][PAWN] - 9 ) * 16;
+            value += 100 * (pieceCount[Us][PAWN] - pieceCount[Them][PAWN] - 4 ) * 16;
     }
 
     return value;
