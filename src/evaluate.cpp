eecb24214217489439f1d85f980a72a19b1454f1
Stéphane Nicolet <cassio@free.fr>
2015-02-19 09:20:53 +0100

    Tweak KingOnOne and KingOnMany values
    Tweak KingOnOne and KingOnMany values

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -152,8 +152,8 @@ namespace {
   };
 
   // Assorted bonuses and penalties used by evaluation
-  const Score KingOnOne          = S( 2, 58);
-  const Score KingOnMany         = S( 6,125);
+  const Score KingOnOne          = S(-75, 68);
+  const Score KingOnMany         = S(-75,135);
   const Score RookOnPawn         = S( 7, 27);
   const Score RookOnOpenFile     = S(43, 21);
   const Score RookOnSemiOpenFile = S(19, 10);
