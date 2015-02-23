76842c05d18fd5b81966d3b70bbc5297afcc6390
Stéphane Nicolet <cassio@free.fr>
2015-02-24 00:42:25 +0100

    Take 3
    4 * history + eval

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -186,7 +186,7 @@ void MovePicker::score<QUIETS>() {
 
           p.do_move(m, st, p.gives_check(m, ci));
           Value v = -Eval::evaluate(p);
-          it->value =  history[pos.moved_piece(m)][to_sq(m)] + 2 * v;
+          it->value =  4 * history[pos.moved_piece(m)][to_sq(m)] + v ;
           p.undo_move(m);
       }
   }
