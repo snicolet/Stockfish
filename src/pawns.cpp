dca09915cdf2f2ff50216b36d6a3ccaa22b22d97
Stéphane Nicolet <cassio@free.fr>
2015-03-26 08:36:01 +0100

    no backward penalty
    no backward penalty for advanced pawns

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -151,9 +151,10 @@ namespace {
         // Test for backward pawn.
         // If the pawn is passed, isolated, lever or connected it cannot be
         // backward. If there are friendly pawns behind on adjacent files
-        // it cannot be backward either.
+        // or if it is in the enemy camp it cannot be backward either.
         if (   (passed | isolated | lever | connected)
-            || (ourPawns & pawn_attack_span(Them, s)))
+            || (ourPawns & pawn_attack_span(Them, s))
+            || (relative_rank(Us, s) >= RANK_5))
             backward = false;
         else
         {
@@ -177,23 +178,19 @@ namespace {
         if (passed && !doubled)
             e->passedPawns[Us] |= s;
 
-        // Score penalties for this pawn
-        if (relative_rank(Us, s) <= RANK_4)
-        {
-            if (isolated)
-                score -= Isolated[opposed][f];
+        // Score the pawn
+        if (isolated)
+            score -= Isolated[opposed][f];
 
-            if (!supported && !isolated)
-                score -= UnsupportedPawnPenalty;
+        if (!supported && !isolated)
+            score -= UnsupportedPawnPenalty;
 
-            if (doubled)
-                score -= Doubled[f] / distance<Rank>(s, frontmost_sq(Us, doubled));
+        if (doubled)
+            score -= Doubled[f] / distance<Rank>(s, frontmost_sq(Us, doubled));
 
-            if (backward)
-                score -= Backward[opposed][f];
-        }
+        if (backward)
+            score -= Backward[opposed][f];
 
-        // Score bonuses for this pawn
         if (connected)
             score += Connected[opposed][!!phalanx][more_than_one(supported)][relative_rank(Us, s)];
 
