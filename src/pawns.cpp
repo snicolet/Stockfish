fd6eef2c8a51f30d26ecee63be1796b97ec856d2
Stéphane Nicolet <cassio@free.fr>
2015-05-28 09:11:37 +0200

    Backward pawns teaks
    Backward pawns teaks

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -44,7 +44,7 @@ namespace {
     S(40, 35), S(40, 35), S(36, 35), S(25, 30) } };
 
   // Backward pawn penalty by opposed flag
-  const Score Backward[2] = { S(67, 56), S(49, 40) };
+  const Score Backward[2] = { S(72, 61), S(54, 45) };
 
   // Connected pawn bonus by opposed, phalanx, twice supported and rank
   Score Connected[2][2][2][RANK_NB];
@@ -149,7 +149,8 @@ namespace {
         // backward. If there are friendly pawns behind on adjacent files
         // it cannot be backward either.
         if (   (passed | isolated | lever | connected)
-            || (ourPawns & pawn_attack_span(Them, s)))
+            || (ourPawns & pawn_attack_span(Them, s))
+            || (relative_rank(Us, s) >= RANK_5))
             backward = false;
         else
         {
