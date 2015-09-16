2962ccc28543c7a23e5ea025f950622dc7a756de
Stéphane Nicolet <cassio@free.fr>
2015-09-16 17:31:57 +0200

    Take 5, weight=35
    Keep heavy pieces when attacking: take 5, weight=35

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -695,7 +695,7 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int attacker_bonus =    7 * pawns - 98
-                         + 30 * (heavy >= 4);
+                         + 35 * (heavy >= 4);
 
     // Now apply the bonus: note that we find the attacking side by extracting the sign 
     // of the endgame value of "positionnal_score", and that we carefully cap the bonus 
