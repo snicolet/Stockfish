b84fac4ffbf349010ac1dd499e1f09ac488849e4
Stéphane Nicolet <cassio@free.fr>
2015-10-12 18:21:49 +0200

    Pawn asymetry
    Pawn asymmetry (on top of king separation)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -707,14 +707,15 @@ namespace {
 
   // evaluate_initiative() computes the initiative correction value for the position, i.e. 
   // second order bonus/malus based on the known attacking/defending status of the players. 
-  Score evaluate_initiative(const Position& pos, const Score positional_score) {
+  Score evaluate_initiative(const Position& pos, const EvalInfo& ei, const Score positional_score) {
 
-    int pawns           =   pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
-    int king_separation =   distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
+    int pawns           =  pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
+    int king_separation =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
+    int asymmetry       =  ei.pi->pawn_asymmetry();
 
     // Compute the initiative bonus for the attacking side
-    int attacker_bonus =    8 * pawns - 96
-                         +  8 * king_separation ;
+    int attacker_bonus =  (6 + asymmetry) * pawns - 96
+                         + 8 * king_separation ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the sign 
     // of the endgame value of "positional_score", and that we carefully cap the bonus so
@@ -722,7 +723,7 @@ namespace {
     int eg = eg_value(positional_score);
     int value = ((eg > 0) - (eg < 0)) * std::max( attacker_bonus , -abs( eg / 2 ) );
 
-    return make_score( 0 , value ) ; 
+    return make_score( -value / 4 , 3 * value / 2 ) ; 
   }
 
 } // namespace
@@ -808,7 +809,7 @@ Value Eval::evaluate(const Position& pos) {
       score += (evaluate_space<WHITE>(pos, ei) - evaluate_space<BLACK>(pos, ei)) * Weights[Space];
   
   // Evaluate initiative
-  score += evaluate_initiative(pos, score);
+  score += evaluate_initiative(pos, ei, score);
 
   // Scale winning side if position is more drawish than it appears
   Color strongSide = eg_value(score) > VALUE_DRAW ? WHITE : BLACK;
