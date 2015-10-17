f3dca7402cc3e58216fe7528841ee5e5f9e3c126
Stéphane Nicolet <cassio@free.fr>
2015-10-18 00:43:08 +0200

    Take 2 (half malus for blocked pawns)
    Take 2 (half malus for blocked pawns)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -697,7 +697,9 @@ namespace {
     int asymmetry       =  ei.pi->pawn_asymmetry();
 
     // Compute the initiative bonus for the attacking side
-    int attacker_bonus =  8 * (pawns - blocked + asymmetry + king_separation) - 120 ;
+    int attacker_bonus =  8 * (pawns + asymmetry + king_separation) 
+                        - 4 * blocked 
+                        - 120 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the sign 
     // of the endgame value of "positional_score", and that we carefully cap the bonus so
