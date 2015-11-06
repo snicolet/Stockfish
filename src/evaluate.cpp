e85e7cc24523517597a369fd26aeab88944b585a
Stéphane Nicolet <cassio@free.fr>
2015-11-06 06:59:27 +0100

    Reduce material when ahead in endgame (take 3)
    Reduce material when ahead in endgame (take 3, double bonus)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -681,10 +681,10 @@ namespace {
 
     int kingDistance = distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     int pawns = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
-    int material = (pos.non_pawn_material(WHITE) + pos.non_pawn_material(BLACK)) / 2048;
+    int material = (pos.non_pawn_material(WHITE) + pos.non_pawn_material(BLACK)) / 512;
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (pawns + asymmetry + kingDistance - material - 10);
+    int initiative = 8 * (pawns + asymmetry + kingDistance - material - 5);
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
