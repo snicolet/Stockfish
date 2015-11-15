c8afee94c5f85ce72d1733133282362dfc9b6d0f
Stéphane Nicolet <cassio@free.fr>
2015-11-15 12:54:06 +0100

    matrix = {0,0} {0,50}
    matrix = {0,0} {0,50}
    bench 8847638

--- src/search.cpp
+++ src/search.cpp
@@ -78,7 +78,7 @@ namespace {
   // PruningSafety[rootColor][fail high/low] : pruning safety table
   const int PruningSafety[2][2] = {
      { 0 , 0 },  // ~rootColor : high,low
-     { 50 , 0 }  //  rootColor : high,low
+     { 0 , 50 }  //  rootColor : high,low
   };
   template <bool forAlpha> int pruning_safety(const Position& pos) {
       return PruningSafety[pos.side_to_move() == pos.this_thread()->rootColor][forAlpha];
