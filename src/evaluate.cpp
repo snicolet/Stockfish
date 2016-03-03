697120f4b70525ca804d6488939be9976a635a44
Stéphane Nicolet <cassio@free.fr>
2016-03-03 07:38:42 +0100

    Take 3, bonus=S(0,15)
    Take 3, bonus=S(0,15)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -186,7 +186,7 @@ namespace {
   const Score Checked             = S(20, 20);
   const Score ThreatByHangingPawn = S(70, 63);
   const Score Hanging             = S(48, 28);
-  const Score LoosePieces         = S( 0, 30);
+  const Score LoosePieces         = S( 0, 15);
   const Score ThreatByPawnPush    = S(31, 19);
   const Score Unstoppable         = S( 0, 20);
 
