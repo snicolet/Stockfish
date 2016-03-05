7a90a35ac7611912a60013ca7991fecdc8c7a047
Stéphane Nicolet <cassio@free.fr>
2016-03-05 14:23:20 +0100

    Change initiative offset
    Change initiative offset (112)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -684,7 +684,7 @@ namespace {
     int pawns = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (asymmetry + kingDistance) + 12 * pawns - 120;
+    int initiative = 8 * (asymmetry + kingDistance) + 12 * pawns - 112;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
