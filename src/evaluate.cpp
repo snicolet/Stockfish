611b8d3525860c4213633ff1e197ee444cde1572
Stéphane Nicolet <cassio@free.fr>
2016-03-06 07:51:51 +0100

    Use blocked pawns info
    Use blocked pawns info

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -682,9 +682,10 @@ namespace {
     int kingDistance =   distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                        - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     int pawns = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
+    int blocked = popcount<Max15>(shift_bb<DELTA_N>(pos.pieces(WHITE, PAWN)) & pos.pieces(BLACK, PAWN));
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (asymmetry + kingDistance) + 12 * pawns - 128;
+    int initiative = 12 * pawns + 8 * (asymmetry + kingDistance + blocked) - 126;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
