84978b985f8d79c4d8e5c89c78c8c1ef3be2b5aa
Stéphane Nicolet <cassio@free.fr>
2016-04-19 23:55:11 +0200

    Lever bonus by weakness flag and rank
    A lever is weak when our lever pawn is unsupported or opposed by an
    enemy pawn on the same file.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -53,12 +53,13 @@ namespace {
     S(11, 34), S(17, 38), S(19, 38), S(19, 38),
     S(19, 38), S(19, 38), S(17, 38), S(11, 34) };
 
-  // Lever bonus by opposed flag and rank
+  // Lever bonus by weakness flag and rank. A lever is weak when our lever
+  // pawn is unsupported or opposed by an enemy pawn on the same file.
   const Score Lever[2][RANK_NB] = {
-    { S( 0,  0), S(  0,  0), S( 0, 0), S(8, 0),
-      S(22, 16), S( 38, 32), S( 0, 0), S(0, 0) },
-    { S( 0,  0), S(-10,  0), S(-5, 0), S(0, 0),
-      S(12, 16), S( 28, 22), S( 0, 0), S(0, 0) } };
+    { S( 0,  0), S( 0,  0), S(0, 0), S(8, 0),
+      S(22, 16), S(38, 32), S(0, 0), S(0, 0) },
+    { S( 0,  0), S( 0,  0), S(0, 0), S(0, 0),
+      S(12, 16), S(28, 22), S(0, 0), S(0, 0) } };
 
   // Weakness of our pawn shelter in front of the king by [distance from edge][rank]
   const Value ShelterWeakness[][RANK_NB] = {
@@ -178,7 +179,7 @@ namespace {
             score -= Doubled[f] / distance<Rank>(s, frontmost_sq(Us, doubled));
 
         if (lever)
-            score += Lever[opposed][relative_rank(Us, s)];
+            score += Lever[!supported && opposed][relative_rank(Us, s)];
     }
 
     b = e->semiopenFiles[Us] ^ 0xFF;
