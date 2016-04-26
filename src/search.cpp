133d32d0fa9c9763b2775e93b622aa7c5ca7be71
Stéphane Nicolet <cassio@free.fr>
2016-04-26 22:42:33 +0200

    Take 3
    Take 3: margin=150, probcut depth= 3

--- src/search.cpp
+++ src/search.cpp
@@ -821,7 +821,7 @@ namespace {
     // safely prune the previous move.
     if (   !PvNode
         &&  depth >= 5 * ONE_PLY
-        &&  eval + PieceValue[MG][pos.captured_piece_type()] >= beta
+        &&  eval + PieceValue[MG][pos.captured_piece_type()] >= beta + 150
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
         Value rbeta = std::min(beta + 200, VALUE_INFINITE);
