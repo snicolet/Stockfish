2374af7b7c885d8a5d80d4140c07f70bb1555b07
Stéphane Nicolet <cassio@free.fr>
2016-04-27 15:58:37 +0200

    Change move picker threshold for probcut
    Change move picker threshold for probcut

--- src/search.cpp
+++ src/search.cpp
@@ -816,9 +816,8 @@ namespace {
     }
 
     // Step 9. ProbCut (skipped when in check)
-    // If we have a very good capture (i.e. SEE > seeValues[captured_piece_type])
-    // and a reduced search returns a value much above beta, we can (almost)
-    // safely prune the previous move.
+    // If we have a very good capture and a reduced search returns a value 
+    // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
         &&  depth >= 5 * ONE_PLY
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
@@ -830,7 +829,8 @@ namespace {
         assert((ss-1)->currentMove != MOVE_NONE);
         assert((ss-1)->currentMove != MOVE_NULL);
 
-        MovePicker mp(pos, ttMove, PieceValue[MG][pos.captured_piece_type()]);
+        Value threshold = std::max(PieceValue[MG][pos.captured_piece_type()], beta - eval) + 50;
+        MovePicker mp(pos, ttMove, threshold);
         CheckInfo ci(pos);
 
         while ((move = mp.next_move()) != MOVE_NONE)
