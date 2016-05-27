48bf7f5d6098489dc399aa9966545c8a8cf376ac
Stéphane Nicolet <cassio@free.fr>
2016-05-27 20:38:10 +0200

    Initial commit
    Initial commit
    Bench: 8749172

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -689,12 +689,12 @@ namespace {
     int pawns = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (asymmetry + kingDistance - 15) + 12 * pawns;
+    int initiative = 8 * (asymmetry + kingDistance - 18) + 12 * pawns;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
-    // that the endgame score will never be divided by more than two.
-    int value = ((eg > 0) - (eg < 0)) * std::max(initiative, -abs(eg / 2));
+    // that the endgame score does not change sign too violently.
+    int value = ((eg > 0) - (eg < 0)) * std::max(initiative, -abs(eg) - 25);
 
     return make_score(0, value);
   }
