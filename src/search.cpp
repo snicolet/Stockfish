c96d82932f21c89693b4257fc853e67400de6af3
Stéphane Nicolet <cassio@free.fr>
2016-06-15 22:26:47 +0200

    midgameLove = 10
    Take 3, midgameLove = 10.
    
    Bench = 7885280

--- src/search.cpp
+++ src/search.cpp
@@ -261,8 +261,8 @@ void MainThread::search() {
   int contempt = Options["Contempt"] * PawnValueEg / 100; // From centipawns
   DrawValue[ us] = VALUE_DRAW - Value(contempt);
   DrawValue[~us] = VALUE_DRAW + Value(contempt);
-  Eval::midgameLove[ us] = Value( 100);
-  Eval::midgameLove[~us] = Value(-100);
+  Eval::midgameLove[ us] = Value( 10);
+  Eval::midgameLove[~us] = Value(-10);
 
   if (rootMoves.empty())
   {
