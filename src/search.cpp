8db06708f0141d90df023268ed9eb57e06867c11
Stéphane Nicolet <cassio@free.fr>
2016-06-28 19:00:49 +0200

    Take 5: try the resulting values of tuning #3
    Take 5: try the resulting values of tuning #3 (with fixed
    OPTIMISM_PAWNS=10 for us)

--- src/search.cpp
+++ src/search.cpp
@@ -262,9 +262,9 @@ void MainThread::search() {
   DrawValue[ us] = VALUE_DRAW - Value(contempt);
   DrawValue[~us] = VALUE_DRAW + Value(contempt);
 
-  Optimism[OPTIMISM_PIECES    ][us] = 0;
-  Optimism[OPTIMISM_PAWNS     ][us] = 0;
-  Optimism[OPTIMISM_MOBILITY  ][us] = 0;
+  Optimism[OPTIMISM_PIECES    ][us] = -10;
+  Optimism[OPTIMISM_PAWNS     ][us] =  10;
+  Optimism[OPTIMISM_MOBILITY  ][us] = -9;
   Optimism[OPTIMISM_ASYMMETRY ][us] = 0;
   Optimism[OPTIMISM_SPACE     ][us] = 0;
   Optimism[OPTIMISM_IMBALANCE ][us] = 0;
