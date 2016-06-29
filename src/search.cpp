43ba19bddf9fc2b17bbe7157d1f43f6c8777c3c3
Stéphane Nicolet <cassio@free.fr>
2016-06-29 13:25:01 +0200

    Interesting style of play
    Interesting style of play

--- src/search.cpp
+++ src/search.cpp
@@ -262,9 +262,9 @@ void MainThread::search() {
   DrawValue[ us] = VALUE_DRAW - Value(contempt);
   DrawValue[~us] = VALUE_DRAW + Value(contempt);
 
-  Optimism[OPTIMISM_PIECES    ][us] = -5;
-  Optimism[OPTIMISM_PAWNS     ][us] = 5;
-  Optimism[OPTIMISM_MOBILITY  ][us] = -5;
+  Optimism[OPTIMISM_PIECES    ][us] = 10;
+  Optimism[OPTIMISM_PAWNS     ][us] = 0;
+  Optimism[OPTIMISM_MOBILITY  ][us] = 0;
   Optimism[OPTIMISM_ASYMMETRY ][us] = 0;
   Optimism[OPTIMISM_SPACE     ][us] = 0;
   Optimism[OPTIMISM_IMBALANCE ][us] = 0;
@@ -272,8 +272,8 @@ void MainThread::search() {
   Optimism[OPTIMISM_SAFETY    ][us] = 0;
 
   Optimism[OPTIMISM_PIECES    ][~us] = 0;
-  Optimism[OPTIMISM_PAWNS     ][~us] = 0;
-  Optimism[OPTIMISM_MOBILITY  ][~us] = 0;
+  Optimism[OPTIMISM_PAWNS     ][~us] = -5;
+  Optimism[OPTIMISM_MOBILITY  ][~us] = 10;
   Optimism[OPTIMISM_ASYMMETRY ][~us] = 0;
   Optimism[OPTIMISM_SPACE     ][~us] = 0;
   Optimism[OPTIMISM_IMBALANCE ][~us] = 0;
