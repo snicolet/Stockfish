0030c942a08d6a7d979b935a572216b314e3e636
Stéphane Nicolet <cassio@free.fr>
2016-07-18 11:16:22 +0200

    Take 1
    Use space evaluation not only in openings, but also in middle game.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -847,7 +847,7 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Evaluate space for both sides, only during opening
-  if (pos.non_pawn_material(WHITE) + pos.non_pawn_material(BLACK) >= 12222)
+  if (pos.non_pawn_material(WHITE) + pos.non_pawn_material(BLACK) >= 10222)
       score +=  evaluate_space<WHITE>(pos, ei)
               - evaluate_space<BLACK>(pos, ei);
 
