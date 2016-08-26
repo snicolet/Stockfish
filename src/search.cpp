c3d82c020226ddd165e8289790a8e38228613de3
Stéphane Nicolet <cassio@free.fr>
2016-08-26 11:22:44 +0200

    No null move trick
    See what happens if we remove the null move trick

--- src/search.cpp
+++ src/search.cpp
@@ -708,9 +708,7 @@ namespace {
     }
     else
     {
-        eval = ss->staticEval =
-        (ss-1)->currentMove != MOVE_NULL ? evaluate(pos)
-                                         : -(ss-1)->staticEval + 2 * Eval::Tempo;
+        eval = ss->staticEval = evaluate(pos);
 
         tte->save(posKey, VALUE_NONE, BOUND_NONE, DEPTH_NONE, MOVE_NONE,
                   ss->staticEval, TT.generation());
@@ -1248,9 +1246,7 @@ moves_loop: // When in check search starts from here
                     bestValue = ttValue;
         }
         else
-            ss->staticEval = bestValue =
-            (ss-1)->currentMove != MOVE_NULL ? evaluate(pos)
-                                             : -(ss-1)->staticEval + 2 * Eval::Tempo;
+            ss->staticEval = bestValue = evaluate(pos);
 
         // Stand pat. Return immediately if static value is at least beta
         if (bestValue >= beta)
