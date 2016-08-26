1df80c5fae71c32e395aa7d43e2a71fe9024d32d
Stéphane Nicolet <cassio@free.fr>
2016-08-26 18:56:18 +0200

    Prefer pawn chains toward the center (take 1)
    Prefer pawn chains toward the center (take 1)

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -43,6 +43,9 @@ namespace {
 
   // Connected pawn bonus by opposed, phalanx, twice supported and rank
   Score Connected[2][2][2][RANK_NB];
+  
+  // Centrality bonus for strong pawns
+  Score Centrality[SQUARE_NB];
 
   // Doubled pawn penalty
   const Score Doubled = S(18,38);
@@ -163,7 +166,8 @@ namespace {
             score -= Unsupported[more_than_one(neighbours & pawnAttacksBB[s])];
 
         if (connected)
-            score += Connected[opposed][!!phalanx][more_than_one(supported)][relative_rank(Us, s)];
+            score += Connected[opposed][!!phalanx][more_than_one(supported)][relative_rank(Us, s)]
+                   + Centrality[s];
 
         if (doubled)
             score -= Doubled;
@@ -195,6 +199,13 @@ void init()
       int v = (Seed[r] + (phalanx ? (Seed[r + 1] - Seed[r]) / 2 : 0)) >> opposed;
       v += (apex ? v / 2 : 0);
       Connected[opposed][phalanx][apex][r] = make_score(v, v * 5 / 8);
+  }
+
+  for (Square s = SQ_A1; s <= SQ_H8; ++s)
+  {
+      int d = distance(s, SQ_D4) + distance(s, SQ_D5) + distance(s, SQ_E4) + distance(s, SQ_E5);
+      d = 24 - 2 * d;
+      Centrality[s] = make_score(d , -d);
   }
 }
 
