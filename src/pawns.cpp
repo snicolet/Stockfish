644e14fc44d19099e828e6e2aa1bd2cdfc5debb1
Stéphane Nicolet <cassio@free.fr>
2016-08-27 18:41:00 +0200

    Weight = 3 for the bonus

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -18,6 +18,7 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <iostream>
 #include <algorithm>
 #include <cassert>
 
@@ -43,6 +44,9 @@ namespace {
 
   // Connected pawn bonus by opposed, phalanx, twice supported and rank
   Score Connected[2][2][2][RANK_NB];
+  
+  // Centrality bonus for strong pawns
+  Score Centrality[SQUARE_NB];
 
   // Doubled pawn penalty
   const Score Doubled = S(18,38);
@@ -162,6 +166,9 @@ namespace {
         else if (!supported)
             score -= Unsupported[more_than_one(neighbours & pawnAttacksBB[s])];
 
+        if (supported)
+            score += Centrality[s];
+
         if (connected)
             score += Connected[opposed][!!phalanx][more_than_one(supported)][relative_rank(Us, s)];
 
@@ -195,6 +202,20 @@ void init()
       int v = (Seed[r] + (phalanx ? (Seed[r + 1] - Seed[r]) / 2 : 0)) >> opposed;
       v += (apex ? v / 2 : 0);
       Connected[opposed][phalanx][apex][r] = make_score(v, v * 5 / 8);
+  }
+
+  for (Square s = SQ_A1; s <= SQ_H8; ++s)
+  {
+      int d = distance(s, SQ_D4) + distance(s, SQ_D5) + distance(s, SQ_E4) + distance(s, SQ_E5);
+      d = 3 * (12 - d);
+      Centrality[s] = make_score(d , -d);
+
+      if (0)
+      {
+      std::cerr << d << "  ";
+      if (file_of(s) == FILE_H)
+         std::cerr << std::endl;
+      }
   }
 }
 
