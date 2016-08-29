ded90b8d47e01653f5c22a919c0a60dfa3e9d410
Stéphane Nicolet <cassio@free.fr>
2016-08-29 21:43:36 +0200

    Fork bonus = S(20,20)
    Take 2, fork bonus = S(20,20)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,7 +191,7 @@ namespace {
   const Score CloseEnemies        = S( 7,  0);
   const Score SafeCheck           = S(20, 20);
   const Score OtherCheck          = S(10, 10);
-  const Score Fork                = S(50, 50);
+  const Score Fork                = S(20, 20);
   const Score ThreatByHangingPawn = S(71, 61);
   const Score LooseEnemies        = S( 0, 25);
   const Score WeakQueen           = S(35,  0);
