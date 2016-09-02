fc9f73d860ecc8ac010ae52130ee4eb87215d73d
Stéphane Nicolet <cassio@free.fr>
2016-09-02 10:56:38 +0200

    Don't evaluate space if there are open file(s)
    Don't evaluate space if there are open file(s)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -848,8 +848,8 @@ Value Eval::evaluate(const Position& pos) {
           score -= Unstoppable * int(relative_rank(BLACK, frontmost_sq(BLACK, b)));
   }
 
-  // Evaluate space for both sides, only during opening
-  if (pos.non_pawn_material(WHITE) + pos.non_pawn_material(BLACK) >= 12222)
+  // Evaluate space for both sides
+  if (!ei.pi->open_files())
       score +=  evaluate_space<WHITE>(pos, ei)
               - evaluate_space<BLACK>(pos, ei);
 
