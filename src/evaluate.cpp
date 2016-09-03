1dfdad0ffa9604bbbfff444b2bf8d6ca9f1b17df
Stéphane Nicolet <cassio@free.fr>
2016-09-03 16:25:00 +0200

    Endgame malus for doubly attacked weaknesses
    Endgame malus for doubly attacked weaknesses
    
    Bench: 6849149

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -195,6 +195,7 @@ namespace {
   const Score LooseEnemies        = S( 0, 25);
   const Score WeakQueen           = S(35,  0);
   const Score Hanging             = S(48, 27);
+  const Score DoublyAttacked      = S( 0, 15);
   const Score ThreatByPawnPush    = S(38, 22);
   const Score Unstoppable         = S( 0, 20);
 
@@ -551,6 +552,7 @@ namespace {
     // Enemies not defended by a pawn and under our attack
     weak =   pos.pieces(Them)
           & ~ei.attackedBy[Them][PAWN]
+          & ~ei.attackedBy2[Them]
           &  ei.attackedBy[Us][ALL_PIECES];
 
     // Add a bonus according to the kind of attacking pieces
@@ -565,6 +567,8 @@ namespace {
             score += Threat[Rook ][type_of(pos.piece_on(pop_lsb(&b)))];
 
         score += Hanging * popcount(weak & ~ei.attackedBy[Them][ALL_PIECES]);
+
+        score += DoublyAttacked * popcount(weak & ei.attackedBy2[Us] & ei.attackedBy[Them][ALL_PIECES]);
 
         b = weak & ei.attackedBy[Us][KING];
         if (b)
