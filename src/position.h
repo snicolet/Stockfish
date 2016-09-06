743a8e5a203117d007b6a5f2d28212bb249308bb
Stéphane Nicolet <cassio@free.fr>
2016-09-07 00:45:00 +0200

    Tweak advanced_pawn_push() for passed pawns
    Helper function advanced_pawn_push() now returns true for a normal pawn
    push to rank 6 (as before) and a passed pawn push to rank 5 (this is
    one rank less than before).

--- src/position.h
+++ src/position.h
@@ -318,7 +318,8 @@ inline bool Position::pawn_passed(Color c, Square s) const {
 
 inline bool Position::advanced_pawn_push(Move m) const {
   return   type_of(moved_piece(m)) == PAWN
-        && relative_rank(sideToMove, from_sq(m)) > RANK_4;
+        && relative_rank(sideToMove, to_sq(m)) > RANK_5 - pawn_passed(sideToMove, to_sq(m))
+        && type_of(m) != ENPASSANT;
 }
 
 inline Key Position::key() const {
