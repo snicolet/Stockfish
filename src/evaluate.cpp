9cdd6c41ea6221cd4236881873acd9c1f8b668f0
Stéphane Nicolet <cassio@free.fr>
2016-09-08 09:24:43 +0200

    Prefer positions with pawns on both flanks in initiative()
    More weight for pawns in initiative if both flanks are active

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,7 +723,7 @@ namespace {
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (asymmetry + kingDistance + 2 * bothFlanks - 17) + 12 * pawns;
+    int initiative = 8 * (asymmetry + kingDistance - 15) + (8 + 4 * bothFlanks) * pawns;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
