24a49644dbc15b836fed781da8b591cab2c4f1b3
Stéphane Nicolet <cassio@free.fr>
2016-10-15 01:10:13 +0200

    Use futility depth = 4
    Use futility depth = 4

--- src/search.cpp
+++ src/search.cpp
@@ -739,7 +739,7 @@ namespace {
 
     // Step 7. Futility pruning: child node (skipped when in check)
     if (   !rootNode
-        &&  depth < 5 * ONE_PLY
+        &&  depth < 4 * ONE_PLY
         &&  eval - futility_margin(depth) >= beta
         &&  eval < VALUE_KNOWN_WIN  // Do not return unproven wins
         &&  pos.non_pawn_material(pos.side_to_move()))
