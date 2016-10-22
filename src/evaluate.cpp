c73f235a6ea2150282f74802079d3121e57868a5
Stéphane Nicolet <cassio@free.fr>
2016-10-22 13:00:15 +0200

    Remove empty lines in code

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -355,8 +355,6 @@ namespace {
                     && !ei.pi->semiopen_side(Us, file_of(ksq), file_of(s) < file_of(ksq)))
                     score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
             }
-            
-            
         }
 
         if (Pt == QUEEN)
