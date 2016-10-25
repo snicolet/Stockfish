a5c99a156308e9aca7a38b8c1cb3c50cef09398a
Stéphane Nicolet <cassio@free.fr>
2016-10-26 00:50:11 +0200

    Take 2, bonus = S(5, 0)
    Take 2, bonus = S(5, 0)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -199,7 +199,7 @@ namespace {
   const Score ThreatByPawnPush    = S(38, 22);
   const Score Unstoppable         = S( 0, 20);
   const Score PawnlessFlank       = S(20, 80);
-  const Score CenterControl       = S( 7,  0);
+  const Score CenterControl       = S( 5,  0);
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
