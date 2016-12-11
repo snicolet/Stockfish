a2eeb6a42dad5daceda1bf8ba645fb2e583827fe
Stéphane Nicolet <cassio@free.fr>
2016-12-11 18:03:02 +0100

    Take 3
    Attack last stopper to threaten a passed pawn creation: take 3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -197,7 +197,7 @@ namespace {
   const Score WeakQueen           = S(50, 10);
   const Score Hanging             = S(48, 27);
   const Score ThreatByPawnPush    = S(38, 22);
-  const Score PassedPawnCreation  = S(20, 20);
+  const Score PassedPawnCreation  = S(30, 10);
   const Score Unstoppable         = S( 0, 20);
   const Score PawnlessFlank       = S(20, 80);
   const Score HinderPassedPawn    = S( 7,  0);
@@ -580,14 +580,17 @@ namespace {
 
         score += Hanging * popcount(weak & ~ei.attackedBy[Them][ALL_PIECES]);
 
+        b =   weak
+            & ~ei.attackedBy2[Them]
+            &  ei.attackedBy2[Us]
+            & ~ei.attackedBy[Us][PAWN]
+            & ei.pi->critical_stoppers(Them);
+        if (b)
+            score += PassedPawnCreation * popcount(b);
+
         b = weak & ei.attackedBy[Us][KING];
         if (b)
             score += ThreatByKing[more_than_one(b)];
-
-        b =  weak & ei.pi->critical_stoppers(Them) & ~ei.attackedBy[Us][PAWN] 
-           & (~ei.attackedBy[Them][ALL_PIECES] | ei.attackedBy2[Us]);
-        if (b)
-            score += popcount(b) * PassedPawnCreation;
     }
 
     // Bonus if some pawns can safely push and attack an enemy piece
