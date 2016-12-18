709daa620a8f5545d421e1e91e7868756738c678
Stéphane Nicolet <cassio@free.fr>
2016-12-18 10:31:04 +0100

    initial commit
    Unstoppable tweak

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -197,7 +197,7 @@ namespace {
   const Score WeakQueen           = S(50, 10);
   const Score Hanging             = S(48, 27);
   const Score ThreatByPawnPush    = S(38, 22);
-  const Score Unstoppable         = S( 0, 45);
+  const Score Unstoppable         = S( 0, 100);
   const Score PawnlessFlank       = S(20, 80);
   const Score HinderPassedPawn    = S( 7,  0);
   const Score ThreatByRank        = S(16,  3);
@@ -617,6 +617,11 @@ namespace {
 
     b = ei.pi->passed_pawns(Us);
 
+    // Bonus for potentially unstoppable pawn
+    if (   b
+        && pos.non_pawn_material(WHITE) + pos.non_pawn_material(BLACK) <= 2 * KnightValueMg)
+        score += Unstoppable;
+
     while (b)
     {
         Square s = pop_lsb(&b);
@@ -851,16 +856,6 @@ Value Eval::evaluate(const Position& pos) {
   // Evaluate passed pawns, we need full attack information including king
   score +=  evaluate_passed_pawns<WHITE, DoTrace>(pos, ei)
           - evaluate_passed_pawns<BLACK, DoTrace>(pos, ei);
-
-  // If both sides have only pawns, score for potential unstoppable pawns
-  if (!pos.non_pawn_material(WHITE) && !pos.non_pawn_material(BLACK))
-  {
-      if (ei.pi->passed_pawns(WHITE))
-          score += Unstoppable;
-
-      if (ei.pi->passed_pawns(BLACK))
-          score -= Unstoppable;
-  }
 
   // Evaluate space for both sides, only during opening
   if (pos.non_pawn_material(WHITE) + pos.non_pawn_material(BLACK) >= 12222)
