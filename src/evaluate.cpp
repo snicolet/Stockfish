38242c92ded2f2e53745d9e1ea3c9f1d2cb8e708
Stéphane Nicolet <cassio@free.fr>
2016-12-29 09:30:03 +0100

    Take 3 (half bonus)
    Take 3 (half bonus)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -740,7 +740,7 @@ namespace {
     Value eg = eg_value(score);
   
     int horizontalDistance = distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
-    int oppositeCastling = 64 * (horizontalDistance >= 4);
+    int oppositeCastling = 16 * (horizontalDistance >= 4);
     int outflanking = horizontalDistance - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     int pawns = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
 
