79cc2e2ae8ef44097462643cb26c2f3bf00dc1e9
Stéphane Nicolet <cassio@free.fr>
2017-01-05 21:26:36 +0100

    king tropism tweak
    king tropism tweak

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -481,7 +481,7 @@ namespace {
 
     // King tropism: firstly, find squares that opponent attacks in our king flank
     File kf = file_of(ksq);
-    b = ei.attackedBy[Them][ALL_PIECES] & KingFlank[kf] & Camp;
+    b = (ei.attackedBy[Them][ALL_PIECES] | pos.pieces(Them, PAWN)) & KingFlank[kf] & Camp;
 
     assert(((Us == WHITE ? b << 4 : b >> 4) & b) == 0);
     assert(popcount(Us == WHITE ? b << 4 : b >> 4) == popcount(b));
