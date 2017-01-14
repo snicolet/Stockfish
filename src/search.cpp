18ba38f80ffca45f8952dd892d6e955a69919a3b
Stéphane Nicolet <cassio@free.fr>
2017-01-15 00:13:23 +0100

    Bigger margin for captures
    Bigger singular margin for captures and promotions

--- src/search.cpp
+++ src/search.cpp
@@ -891,8 +891,12 @@ moves_loop: // When in check search starts from here
           && !extension
           &&  pos.legal(move))
       {
-          Value rBeta = std::max(ttValue - 2 * depth / ONE_PLY, -VALUE_MATE);
+          int x = depth / ONE_PLY;
+          int margin = captureOrPromotion ? 4 * x : 3 * x;
+          Value rBeta = std::max(ttValue - margin, -VALUE_MATE);
+
           Depth d = (depth / (2 * ONE_PLY)) * ONE_PLY;
+
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, rBeta - 1, rBeta, d, cutNode, true);
           ss->excludedMove = MOVE_NONE;
