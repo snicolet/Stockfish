6d65e0d7436577309e68b978471451d2b7ff9574
Stéphane Nicolet <cassio@free.fr>
2017-01-14 10:16:58 +0100

    Tweak negative SEE pruning margin
    To avoid pruning "bishop takes knight" moves at low depth

--- src/search.cpp
+++ src/search.cpp
@@ -934,7 +934,7 @@ moves_loop: // When in check search starts from here
 
               // Prune moves with negative SEE
               if (   lmrDepth < 8
-                  && !pos.see_ge(move, Value(-35 * lmrDepth * lmrDepth)))
+                  && !pos.see_ge(move, Value(-70 - 35 * lmrDepth * lmrDepth)))
                   continue;
           }
           else if (    depth < 7 * ONE_PLY
