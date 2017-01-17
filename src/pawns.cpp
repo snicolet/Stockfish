47ca5c439f00e7a21f04236267bc61569c63bd8b
Stéphane Nicolet <cassio@free.fr>
2017-01-17 07:51:19 +0100

    Take 2
    Take 2

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -99,9 +99,9 @@ namespace {
     const Square Right = (Us == WHITE ? NORTH_EAST : SOUTH_WEST);
     const Square Left  = (Us == WHITE ? NORTH_WEST : SOUTH_EAST);
 
-    Bitboard b, neighbours, stoppers, doubled, supported, phalanx;
+    Bitboard b, neighbours, stoppers, lever, doubled, supported, phalanx;
     Square s;
-    bool opposed, lever, connected, backward;
+    bool opposed, connected, backward;
     Score score = SCORE_ZERO;
     const Square* pl = pos.squares<PAWN>(Us);
     const Bitboard* pawnAttacksBB = StepAttacksBB[make_piece(Us, PAWN)];
@@ -176,6 +176,8 @@ namespace {
 
 
         Bitboard attackSpan = pawn_attack_span(Us, s);
+        if (stoppers && more_than_one(stoppers))
+            stoppers &= ~lever;
         if (stoppers)
         {
             Rank firstStopper = rank_of(backmost_sq(Us, stoppers));
