f5a26c3108b158d48ed7be7e3e21cf9a77bbe540
Stéphane Nicolet <cassio@free.fr>
2017-01-20 15:06:37 +0100

    Clarify comment
    Clarify comment

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -152,8 +152,8 @@ namespace {
             assert(!backward || !(pawn_attack_span(Them, s + Up) & neighbours));
         }
 
-        // Passed pawns will be properly scored in evaluation because we need
-        // full attack info to evaluate them.
+        // Passed pawns and candidate passed pawns will be properly scored 
+        // in evaluation because we need full attack info to evaluate them.
         if (  !((ourPawns | theirPawns) & forward_bb(Us, s))
             && popcount(supported) >= popcount(lever)
             && popcount(connected) >= popcount(stoppers))
