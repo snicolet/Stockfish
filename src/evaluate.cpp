b14f06d072e9b4c831117dddd0bf1efedc595ec0
Stéphane Nicolet <cassio@free.fr>
2017-01-25 01:17:59 +0100

    take 3
    Division by 20 instead of division by 18

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -735,7 +735,7 @@ namespace {
     bonus = std::min(16, bonus);
     int weight = pos.count<ALL_PIECES>(Us) - 2 * ei.pe->open_files();
 
-    return make_score(bonus * weight * weight / 16, 0);
+    return make_score(bonus * weight * weight / 20, 0);
   }
 
 
