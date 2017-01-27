9382c633d76167a95ca1017faa96cc0776f3ea52
Stéphane Nicolet <cassio@free.fr>
2017-01-27 15:58:26 +0100

    Pawn span = S(10,5)

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -47,7 +47,7 @@ namespace {
   const Score Doubled = S(18, 38);
 
   // Pawn span bonus
-  const Score PawnSpan = S(5, 0);
+  const Score PawnSpan = S(10, 5);
 
   // Lever bonus by rank
   const Score Lever[RANK_NB] = {
