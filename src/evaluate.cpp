8d3cb8eb28272735fc8a14b4ab335d46e6c8a8d1
Stéphane Nicolet <cassio@free.fr>
2017-02-03 18:21:48 +0100

    Entry points in the opponent camp
    Bonus for entry points in the opponent camp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -196,6 +196,7 @@ namespace {
   const Score Hanging             = S(48, 27);
   const Score ThreatByPawnPush    = S(38, 22);
   const Score HinderPassedPawn    = S( 7,  0);
+  const Score EntryPoints         = S( 4,  0);
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
@@ -516,6 +517,9 @@ namespace {
     const Square Right      = (Us == WHITE ? NORTH_EAST : SOUTH_WEST);
     const Bitboard TRank2BB = (Us == WHITE ? Rank2BB    : Rank7BB);
     const Bitboard TRank7BB = (Us == WHITE ? Rank7BB    : Rank2BB);
+    const Bitboard OpponentCamp = 
+        (Us == WHITE ? Rank4BB | Rank5BB | Rank6BB | Rank7BB | Rank8BB
+                     : Rank5BB | Rank4BB | Rank3BB | Rank2BB | Rank1BB);
 
     Bitboard b, weak, defended, safeThreats;
     Score score = SCORE_ZERO;
@@ -591,6 +595,10 @@ namespace {
        & ~ei.attackedBy[Us][PAWN];
 
     score += ThreatByPawnPush * popcount(b);
+    
+    // Entry points in the opponent camp
+    b = ei.attackedBy2[Us] & ~ei.attackedBy[Them][PAWN] & OpponentCamp;
+    score += EntryPoints * popcount(b);
 
     if (DoTrace)
         Trace::add(THREAT, Us, score);
