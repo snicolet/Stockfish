7913e7adc60184ca4d528e31ebc5beceefa82f30
Stéphane Nicolet <cassio@free.fr>
2017-02-11 15:36:03 +0100

    initiative down to draw
    Let (lack of) initiative lower endgame score down to draw value.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -736,8 +736,8 @@ namespace {
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
-    // that the endgame score will never be divided by more than two.
-    int value = ((eg > 0) - (eg < 0)) * std::max(initiative, -abs(eg / 2));
+    // that the endgame score will never change sign after the bonus.
+    int value = ((eg > 0) - (eg < 0)) * std::max(initiative, -abs(eg));
 
     return make_score(0, value);
   }
