692834fda89bb65b63ce1b7a071cd11a0220e709
Stéphane Nicolet <cassio@free.fr>
2017-02-17 10:35:36 +0100

    With factor 16 for pawns in initiative()

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -729,7 +729,7 @@ namespace {
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (asymmetry + kingDistance + 2 * bothFlanks - 26) + 20 * pawns;
+    int initiative = 8 * (asymmetry + kingDistance - 22) + 16 * (pawns + bothFlanks);
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
