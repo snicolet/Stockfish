e96154a464d2b7a0221b0eb0c1f1057960607e36
Stéphane Nicolet <cassio@free.fr>
2017-03-01 15:02:31 +0100

    Simplify condition for countermoves based pruning (take 2)
    Simplify condition for countermoves based pruning (take 2)

--- src/search.cpp
+++ src/search.cpp
@@ -913,10 +913,10 @@ moves_loop: // When in check search starts from here
               int lmrDepth = std::max(newDepth - reduction<PvNode>(improving, depth, moveCount), DEPTH_ZERO) / ONE_PLY;
 
               // Countermoves based pruning
-              if (      lmrDepth < 3
-                  && (  (*cmh )[moved_piece][to_sq(move)]
-                      + (*fmh )[moved_piece][to_sq(move)]
-                      + (*fmh2)[moved_piece][to_sq(move)] < VALUE_ZERO))
+              if (   lmrDepth < 3
+                  &&  (*cmh )[moved_piece][to_sq(move)] <= VALUE_ZERO
+                  &&  (*fmh )[moved_piece][to_sq(move)] <= VALUE_ZERO
+                  && ((*fmh2)[moved_piece][to_sq(move)] <= VALUE_ZERO || ((ss-1)->is_ok() && (ss-2)->is_ok())))
                   continue;
 
               // Futility pruning: parent node
