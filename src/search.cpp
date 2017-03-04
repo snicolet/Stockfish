d756f9b451499a827bd1ec93d6487d3d2d20398e
Stéphane Nicolet <cassio@free.fr>
2017-03-04 08:17:32 +0100

    Take 3
    Take 3, threshold = 2600

--- src/search.cpp
+++ src/search.cpp
@@ -902,7 +902,7 @@ moves_loop: // When in check search starts from here
       {
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(pos.side_to_move()) >= 2400))
+              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(pos.side_to_move()) >= 2600))
           {
               // Move count based pruning
               if (moveCountPruning)
