07e84c6a1f1f90665956e92147072abd947b6928
Stéphane Nicolet <cassio@free.fr>
2017-03-16 13:00:31 +0100

    Singular extension for the second move
    Allow singular extension for the second move

--- src/search.cpp
+++ src/search.cpp
@@ -877,10 +877,10 @@ moves_loop: // When in check search starts from here
       // Singular extension search. If all moves but one fail low on a search of
       // (alpha-s, beta-s), and just one fails high on (alpha, beta), then that move
       // is singular and should be extended. To verify this we do a reduced search
-      // on all the other moves but the ttMove and if the result is lower than
-      // ttValue minus a margin then we extend the ttMove.
+      // on all the other moves but the current move and if the result is lower than
+      // ttValue minus a margin then we extend the current move.
       if (    singularExtensionNode
-          &&  move == ttMove
+          &&  moveCount <= 2
           && !extension
           &&  pos.legal(move))
       {
