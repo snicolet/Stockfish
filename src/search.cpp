23a331f3771347e31dd8378c65eb61520d622158
Stéphane Nicolet <cassio@free.fr>
2017-03-20 20:35:45 +0100

    Nicer coding style

--- src/search.cpp
+++ src/search.cpp
@@ -936,11 +936,9 @@ moves_loop: // When in check search starts from here
           else if (    depth < 7 * ONE_PLY
                    && !extension
                    && !pos.see_ge(move, -PawnValueEg * (depth / ONE_PLY)))
-              {
-                  if (moveCountPruning && depth < 2)
-                     break;
-                  continue;
-              }
+               {
+                  if (moveCountPruning && depth < 2) break; else continue;
+               }
       }
 
       // Speculative prefetch as early as possible
