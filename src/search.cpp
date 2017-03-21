66caed299b0b562cbaeea7fb66526d5c8368a87c
Stéphane Nicolet <cassio@free.fr>
2017-03-22 00:13:41 +0100

    Take 2

--- src/search.cpp
+++ src/search.cpp
@@ -933,9 +933,9 @@ moves_loop: // When in check search starts from here
                   && !pos.see_ge(move, Value(-35 * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (    depth < 7 * ONE_PLY
+          else if (    depth < 8 * ONE_PLY
                    && !extension
-                   && !pos.see_ge(move, -4 * PawnValueEg / moveCount))
+                   && !pos.see_ge(move, -PawnValueEg * (depth / ONE_PLY)))
                   continue;
       }
 
