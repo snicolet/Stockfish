8c3f7898819a43c03e4d8c7e0baf22823204fc0d
Stéphane Nicolet <cassio@free.fr>
2017-03-29 17:33:52 +0200

    Take 2: increase OtherCheck bonus in midgame
    Take 2: increase OtherCheck bonus in midgame

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -189,7 +189,7 @@ namespace {
   const Score RookOnPawn          = S(  8, 24);
   const Score TrappedRook         = S( 92,  0);
   const Score WeakQueen           = S( 50, 10);
-  const Score OtherCheck          = S( 10, 10);
+  const Score OtherCheck          = S( 20, 10);
   const Score CloseEnemies        = S(  7,  0);
   const Score PawnlessFlank       = S( 20, 80);
   const Score ThreatByHangingPawn = S( 71, 61);
