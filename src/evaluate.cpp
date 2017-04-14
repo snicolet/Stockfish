f16fd2c8e1d9350bb8ae0fa22efa7e11138e905a
Stéphane Nicolet <cassio@free.fr>
2017-04-14 09:51:45 +0200

    Use A XOR B instead of A != B

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -735,8 +735,8 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
-    int imbalance =   (pos.count<KNIGHT>(WHITE) != pos.count<KNIGHT>(BLACK))
-                    + (pos.count<BISHOP>(WHITE) != pos.count<BISHOP>(BLACK));
+    int imbalance =   (pos.count<KNIGHT>(WHITE) ^ pos.count<KNIGHT>(BLACK))
+                    + (pos.count<BISHOP>(WHITE) ^ pos.count<BISHOP>(BLACK));
 
     // Compute the initiative bonus for the attacking side
     int initiative =    8 * (asymmetry + kingDistance - imbalance - 16) 
