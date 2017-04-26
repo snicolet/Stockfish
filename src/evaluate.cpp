e53ab775d1fa47fcf6faaed5a8a3ba0115c2183c
Stéphane Nicolet <cassio@free.fr>
2017-04-26 07:50:37 +0200

    factor 1/3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -682,7 +682,7 @@ namespace {
         // Phalanx of passed pawns
         if (   relative_rank(Us, s) >= RANK_4
             && (ei.pe->passed_pawns(Us) & StepAttacksBB[make_piece(Us, PAWN)][s]))
-            mbonus += mbonus / 5, ebonus += ebonus / 5;
+            mbonus += mbonus / 3, ebonus += ebonus / 3;
 
         score += make_score(mbonus, ebonus) + PassedFile[file_of(s)];
     }
