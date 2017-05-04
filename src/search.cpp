94f45cb491325f998cc9a3284210dc4aec63b7bd
Stéphane Nicolet <cassio@free.fr>
2017-05-04 10:02:07 +0200

    Lowering beta: margin of 50

--- src/search.cpp
+++ src/search.cpp
@@ -656,10 +656,10 @@ namespace {
         && ttHit
         && tte->depth() == depth
         && ttValue != VALUE_NONE // Possible in case of TT access race
-        && ttValue < beta
-        && ttValue >= alpha
-        && (tte->bound() & BOUND_UPPER))
-        beta = ttValue + 1;
+        && (tte->bound() & BOUND_UPPER)
+        && ttValue > alpha
+        && ttValue <= beta - 50)
+        beta = ttValue + 50;
 
     // Step 4a. Tablebase probe
     if (!rootNode && TB::Cardinality)
