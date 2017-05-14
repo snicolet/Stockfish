bb903d3781cfe8a9352814600d11f2707402f753
Stéphane Nicolet <cassio@free.fr>
2017-05-14 17:10:49 +0200

    Take 3: more compensation

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -424,11 +424,11 @@ namespace {
                     + 143 * (popcount(b) + !!pos.pinned_pieces(Us))
                     - 848 * !pos.count<QUEEN>(Them)
                     -  28 * mg_value(score) / 25 
-                    +   7;
+                    +  50;
 
         // King luft
         int luft = popcount(DistanceRingBB[ksq][0] & ~pos.pieces());
-        kingDanger -= luft * luft;
+        kingDanger -= 2 * luft * luft;
 
         // Analyse the safe enemy's checks which are possible on next move
         safe  = ~pos.pieces(Them);
