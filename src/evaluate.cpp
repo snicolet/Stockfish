2a8366f781185ef2aaf5196ab4037f607fe66d61
Stéphane Nicolet <cassio@free.fr>
2017-05-16 09:39:08 +0200

    King luft
    Another try at king luft

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -426,7 +426,12 @@ namespace {
                     + 201 * popcount(undefended)
                     + 143 * (popcount(b) + !!pos.pinned_pieces(Us))
                     - 848 * !pos.count<QUEEN>(Them)
-                    -  28 * mg_value(score) / 25 - 5;
+                    -  28 * mg_value(score) / 25 
+                    -  40;
+
+        // King luft
+        int luft = popcount(DistanceRingBB[ksq][0] & ~pos.pieces());
+        kingDanger -= 2 * luft * luft;
 
         // Analyse the safe enemy's checks which are possible on next move
         safe  = ~pos.pieces(Them);
