669d33b3207d26903ee739056b7407f6c618cafa
Stéphane Nicolet <cassio@free.fr>
2017-05-22 21:11:55 +0200

    Initialize kingDanger with non_pawn_material()
    Instead of using the queen/no-queen flag to initialize the kingDanger
    variable, we use the smoother estimate of remaining material given by
    non_pawn_material().

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -425,9 +425,9 @@ namespace {
                     + 102 * ei.kingAdjacentZoneAttacksCount[Them]
                     + 201 * popcount(undefended)
                     + 143 * (popcount(b) + !!pos.pinned_pieces(Us))
-                    - 848 * !pos.count<QUEEN>(Them)
+                    +       pos.non_pawn_material(Them) / 64
                     -   9 * mg_value(score) / 8
-                    +  40;
+                    - 40;
 
         // Analyse the safe enemy's checks which are possible on next move
         safe  = ~pos.pieces(Them);
