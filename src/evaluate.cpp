547ee34132d0bbb9762d86d58bd6e641a49cbab0
Stéphane Nicolet <cassio@free.fr>
2017-05-24 09:36:35 +0200

    Double bonus

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -427,7 +427,8 @@ namespace {
                     + 143 * (popcount(b) + !!pos.pinned_pieces(Us))
                     - 848 * !pos.count<QUEEN>(Them)
                     -   9 * mg_value(score) / 8
-                    +   4 * pos.count<ALL_PIECES>(Them);
+                    +   8 * pos.count<ALL_PIECES>(Them)
+                    -  40;
 
         // Analyse the safe enemy's checks which are possible on next move
         safe  = ~pos.pieces(Them);
