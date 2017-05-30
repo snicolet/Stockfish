2ab3b70a2eb1a25788adbe5b6575736d01046330
Stéphane Nicolet <cassio@free.fr>
2017-05-30 19:29:52 +0200

    Part 2

--- src/search.cpp
+++ src/search.cpp
@@ -1267,8 +1267,10 @@ moves_loop: // When in check search starts from here
       if (   !InCheck
           && !givesCheck
           &&  futilityBase > -VALUE_KNOWN_WIN
-          &&  type_of(move) == NORMAL)
+          && !pos.advanced_pawn_push(move))
       {
+          assert(type_of(move) != ENPASSANT); // Due to !pos.advanced_pawn_push		
+          
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
 
           if (futilityValue <= alpha)
