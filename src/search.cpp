7640b4c52350f09580be984e6de35c6317154cae
Stéphane Nicolet <cassio@free.fr>
2017-05-30 19:33:35 +0200

    part 2

--- src/search.cpp
+++ src/search.cpp
@@ -1269,8 +1269,8 @@ moves_loop: // When in check search starts from here
           &&  futilityBase > -VALUE_KNOWN_WIN
           && !pos.advanced_pawn_push(move))
       {
-          assert(type_of(move) != ENPASSANT); // Due to !pos.advanced_pawn_push		
-          
+          assert(type_of(move) != ENPASSANT); // Due to !pos.advanced_pawn_push
+
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
 
           if (futilityValue <= alpha)
