67ce6c45a6be2e1528509326ae1c9be0fb79ca02
Stéphane Nicolet <cassio@free.fr>
2017-06-23 08:27:36 +0200

    Take 3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -739,10 +739,9 @@ namespace {
     // ...count safe + (behind & safe) with a single popcount.
     int bonus = popcount((Us == WHITE ? safe << 32 : safe >> 32) | (behind & safe));
 
-    int weight =  pos.count<ALL_PIECES>(Us)
-                - 2 * pe->open_files()
-                - eg_value(mobility[Them]) / 64
-                + 3;
+    bonus += abs(mg_value(mobility[Us] - mobility[Them])) / 64;
+
+    int weight = pos.count<ALL_PIECES>(Us) - 2 * pe->open_files();
 
     return make_score(bonus * weight * weight / 16, 0);
   }
