4b078bc377a18b6f8e5ba11871ace443d2509e8f
Stéphane Nicolet <cassio@free.fr>
2017-06-26 23:20:45 +0200

    Mobility term in initiative()

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -751,12 +751,15 @@ namespace {
   template<Tracing T>
   Score Evaluation<T>::evaluate_initiative(Value eg) {
 
+    int mobil        =  eg_value(mobility[WHITE] + mobility[BLACK]) / 256;
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (pe->pawn_asymmetry() + kingDistance - 17) + 12 * pos.count<PAWN>() + 16 * bothFlanks;
+    int initiative =   8 * (pe->pawn_asymmetry() + kingDistance + mobil - 17) 
+                    + 12 * pos.count<PAWN>() 
+                    + 16 * bothFlanks;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
