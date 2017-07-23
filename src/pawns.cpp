816131e2b0da1ccc5353f237f006a7d8c2f4068e
Stéphane Nicolet <cassio@free.fr>
2017-07-23 20:00:51 +0200

    +5 for connected pawns seed[]
    Bench: 5907477

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -198,7 +198,7 @@ namespace Pawns {
 
 void init() {
 
-  static const int Seed[RANK_NB] = { 0, 3, 14, 10, 66, 88, 163, 318 };
+  static const int Seed[RANK_NB] = { 0, 18, 29, 23, 81, 106, 181, 336 };
 
   for (int opposed = 0; opposed <= 1; ++opposed)
       for (int phalanx = 0; phalanx <= 1; ++phalanx)
