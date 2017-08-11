6eaa81080cd3bdb4c71ae5b810924a6469fb74bc
Stéphane Nicolet <cassio@free.fr>
2017-08-11 19:12:12 +0200

    Try 1/4th the delta from master given by tuning

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -198,7 +198,7 @@ namespace Pawns {
 
 void init() {
 
-  static const int Seed[RANK_NB] = { 0, 13, 24, 18, 76, 100, 175, 330 };
+  static const int Seed[RANK_NB] = { 0, 28, 59, 50, 81, 56, 57, 115 };
 
   for (int opposed = 0; opposed <= 1; ++opposed)
       for (int phalanx = 0; phalanx <= 1; ++phalanx)
