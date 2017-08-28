de6252c505b5a412601722e65cefb7ad0ec94358
Stéphane Nicolet <cassio@free.fr>
2017-08-28 09:32:04 +0200

    Bishop and initiative()

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -750,12 +750,15 @@ namespace {
   template<Tracing T>
   Score Evaluation<T>::evaluate_initiative(Value eg) {
 
-    int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
-                      - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
+    int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
+                     - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (pe->pawn_asymmetry() + kingDistance - 17) + 12 * pos.count<PAWN>() + 16 * bothFlanks;
+    int initiative =   8 * (pe->pawn_asymmetry() + outflanking - 17)
+                    + 12 * pos.count<PAWN>()
+                    +  4 * pos.count<BISHOP>()
+                    + 16 * bothFlanks;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
