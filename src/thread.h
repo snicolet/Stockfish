7be8f9bd837d76d5a85bb23e017d0151f53ab3ac
Stéphane Nicolet <cassio@free.fr>
2017-08-29 10:10:02 +0200

    Take 4
    Asymmetric risk: take 4

--- src/thread.h
+++ src/thread.h
@@ -67,6 +67,7 @@ public:
   Search::RootMoves rootMoves;
   Depth rootDepth, completedDepth;
   CounterMoveHistory counterMoves;
+  Color rootColor;
   ButterflyHistory mainHistory;
   ContinuationHistory contHistory;
 };
