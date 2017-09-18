277e06a6ebb5142a912b0dbcf75ecf28ade19fbf
Stéphane Nicolet <cassio@free.fr>
2017-09-18 11:21:31 +0200

    Lower outpost bonus for protected by pawn

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -162,8 +162,8 @@ namespace {
   // supported by a pawn. If the minor piece occupies an outpost square
   // then score is doubled.
   const Score Outpost[][2] = {
-    { S(22, 6), S(36,12) }, // Knight
-    { S( 9, 2), S(15, 5) }  // Bishop
+    { S(22, 6), S(30, 8) }, // Knight
+    { S( 9, 2), S(12, 3) }  // Bishop
   };
 
   // RookOnFile[semiopen/open] contains bonuses for each rook when there is no
