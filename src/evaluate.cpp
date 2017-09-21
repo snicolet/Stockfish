a6cfde1478ecaf539aaf19cc4b7bb237bd095474
Stéphane Nicolet <cassio@free.fr>
2017-09-21 10:25:21 +0200

    take 3: only the ThreatByPieces part

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -578,6 +578,10 @@ namespace {
         {
             Square s = pop_lsb(&b);
             score += ThreatByMinor[type_of(pos.piece_on(s))];
+
+            if (superweak & s)
+                score += ThreatByMinor[type_of(pos.piece_on(s))];
+
             if (type_of(pos.piece_on(s)) != PAWN)
                 score += ThreatByRank * (int)relative_rank(Them, s);
         }
@@ -587,6 +591,10 @@ namespace {
         {
             Square s = pop_lsb(&b);
             score += ThreatByRook[type_of(pos.piece_on(s))];
+
+            if (superweak & s)
+                score += ThreatByRook[type_of(pos.piece_on(s))];
+
             if (type_of(pos.piece_on(s)) != PAWN)
                 score += ThreatByRank * (int)relative_rank(Them, s);
         }
@@ -596,8 +604,6 @@ namespace {
         b = weak & attackedBy[Us][KING];
         if (b)
             score += ThreatByKing[more_than_one(b)];
-        if (superweak & b)
-            score += ThreatByKing[more_than_one(superweak & b)];
     }
 
     // Bonus for opponent unopposed weak pawns
