ba1a565863162b64d338441dde818086e5295cf4
Stéphane Nicolet <cassio@free.fr>
2017-09-26 23:05:08 +0200

    Opposite colored bishops
    Only apply the "opposite colored bishop" scaling factor in positions without material imbalance.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -783,7 +783,8 @@ namespace {
     // types of endgames, and use a lower scale for those.
     if (sf == SCALE_FACTOR_NORMAL || sf == SCALE_FACTOR_ONEPAWN)
     {
-        if (pos.opposite_bishops())
+        if (   pos.opposite_bishops()
+            && pos.non_pawn_material(WHITE) == pos.non_pawn_material(BLACK))
         {
             // Endgame with opposite-colored bishops and no other pieces (ignoring pawns)
             // is almost a draw, in case of KBP vs KB, it is even more a draw.
