c63fd8010a52977b37ade7095984a2270813ab57
Stéphane Nicolet <cassio@free.fr>
2017-10-06 11:46:44 +0200

    Time shifted shuffling, divisor 4

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -789,7 +789,7 @@ namespace {
 
     Color strongSide = eg > VALUE_DRAW ? WHITE : BLACK;
     int factor       = me->scale_factor(pos, strongSide);
-    int shuffling    = pos.rule50_count() / 3;
+    int shuffling    = (pos.rule50_count() - 8) / 4;
 
     // If we don't already have an unusual scale factor, check for certain
     // types of endgames, and use a lower scale for those.
