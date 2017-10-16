3e47b533e7a810e60916728d73fd946c1df34e35
Stéphane Nicolet <cassio@free.fr>
2017-10-16 22:21:39 +0200

    Same as take 1, but half bonus

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -773,12 +773,10 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
     // that the endgame score will never change sign after the bonus.
-    int sign = (eg > 0) - (eg < 0);
+    int u = initiative <= 0 ? 0 : initiative * initiative / 128;
+    int v = std::max(initiative, -abs(eg));
 
-    int u = initiative <= 0 ? 0 : sign * initiative * initiative / 64;
-    int v = sign * std::max(initiative, -abs(eg));
-
-    return make_score(u, v);
+    return make_score(u, v) * ((eg > 0) - (eg < 0));
   }
 
 
