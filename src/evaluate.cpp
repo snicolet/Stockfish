e1060f7e1f7d5446f1ea6b6679e190d5766fe03f
Stéphane Nicolet <cassio@free.fr>
2017-10-20 09:25:43 +0200

    Linear inititiative for eg

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -772,14 +772,11 @@ namespace {
                     + 12 * pos.count<PAWN>() 
                     + 16 * bothFlanks;
 
-    // Quadratic transform, keeping the sign
-    initiative = abs(initiative) * initiative / 64;
-
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
     // that the endgame score will never change sign after the bonus.
-    int u = std::max(initiative / (1 + !!pos.pieces(QUEEN)), 0);
-    int v = std::max(initiative, -abs(eg));
+    int u = std::max(abs(initiative) * initiative / 64, 0);
+    int v = std::max(2 * initiative, -abs(eg));
 
     return make_score(u, v) * ((eg > 0) - (eg < 0));
   }
