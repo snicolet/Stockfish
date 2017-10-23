e5d067f91cb997be4446d35d7569ee61fea3ba29
Stéphane Nicolet <cassio@free.fr>
2017-10-24 00:34:36 +0200

    16 * aging()

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -770,8 +770,7 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int initiative =   8 * (pe->pawn_asymmetry() + kingDistance - 16)
                     + 12 * pos.count<PAWN>()
-                    + 16 * bothFlanks
-                    - 32 * pos.aging();
+                    + 16 * (bothFlanks - pos.aging());
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
