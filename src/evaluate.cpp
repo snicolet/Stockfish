a32c3a0ef0e7bb3da049a5df3c13c9dc184730a3
Stéphane Nicolet <cassio@free.fr>
2017-10-24 18:17:26 +0200

    Revert to a promising version

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -766,11 +766,14 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
+    //int shuffling = pos.rule50_count() <= 8 ? 0 : 8 - pos.rule50_count();
+    int shuffling = -pos.rule50_count();
 
     // Compute the initiative bonus for the attacking side
-    int initiative =   8 * (pe->pawn_asymmetry() + kingDistance - 16)
+    int initiative =   8 * (pe->pawn_asymmetry() + kingDistance - 17)
                     + 12 * pos.count<PAWN>()
-                    + 16 * (bothFlanks - pos.aging());
+                    + 16 * bothFlanks
+                    +  4 * shuffling;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
