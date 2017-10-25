119f9f943283698a62828a4f60b259e83308104b
Stéphane Nicolet <cassio@free.fr>
2017-10-25 08:29:38 +0200

    Material balance in initiative()

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -766,9 +766,13 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
+    int imbalance =   (pos.count<KNIGHT>(WHITE) != pos.count<KNIGHT>(BLACK))
+                    + (pos.count<BISHOP>(WHITE) != pos.count<BISHOP>(BLACK));
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (pe->pawn_asymmetry() + kingDistance - 17) + 12 * pos.count<PAWN>() + 16 * bothFlanks;
+    int initiative =    8 * (pe->pawn_asymmetry() + kingDistance - imbalance - 16)
+                     + 25 * pos.count<PAWN>() / 2
+                     + 16 * bothFlanks;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
