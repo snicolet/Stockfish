409ee657b5829cc8807d66973b545bf56450a7ed
Stéphane Nicolet <cassio@free.fr>
2017-11-07 16:27:33 +0100

    Create montecarlo.cpp and montecarlo.h

---
+++ src/montecarlo.h
@@ -0,0 +1,53 @@
+/*
+  Stockfish, a UCI chess playing engine derived from Glaurung 2.1
+  Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
+  Copyright (C) 2008-2015 Marco Costalba, Joona Kiiski, Tord Romstad
+  Copyright (C) 2015-2017 Marco Costalba, Joona Kiiski, Gary Linscott, Tord Romstad
+
+  Stockfish is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Stockfish is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MONTECARLO_H_INCLUDED
+#define MONTECARLO_H_INCLUDED
+
+#include <cassert>
+#include <deque>
+#include <memory> // For std::unique_ptr
+#include <string>
+
+#include "bitboard.h"
+#include "types.h"
+
+
+/// UCTInfo class stores information in a node to perform the UCT algorithm
+
+class UCTInfo {
+public:
+  
+private:
+  uint64_t number_of_visits;         // number of visits by the UCT algorithm
+  uint64_t number_of_sons;           // total number of legal moves
+  uint64_t number_of_expanded_sons;  // number of sons expanded by the UCT algorithm
+  double   reward;                   // reward from the point of view of the side to move
+};
+
+/// A list to keep track of the position states along the setup moves (from the
+/// start position to the position just before the search starts). Needed by
+/// 'draw by repetition' detection. Use a std::deque because pointers to
+/// elements are not invalidated upon list resizing.
+typedef std::unique_ptr<std::deque<StateInfo>> StateListPtr;
+
+
+
+#endif // #ifndef MONTECARLO_H_INCLUDED
