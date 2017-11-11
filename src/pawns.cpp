a5304b75a3eefd4c4d7da9a2c2f4666183666ac2
Stéphane Nicolet <cassio@free.fr>
2017-11-11 16:41:00 +0100

    Pivot pawn of the chain on files C..F, bonus=S(5,5)

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -192,7 +192,7 @@ namespace {
         if (lever)
             score += Lever[relative_rank(Us, s)];
 
-        if (supported && supporting && (f >= FILE_D && f <= FILE_E))
+        if (supported && supporting && (f >= FILE_C && f <= FILE_F))
             score += Chain;
     }
 
