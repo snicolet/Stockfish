b035f825b7249a63c3f986bfc3f2dde84d96ccaf
Stéphane Nicolet <cassio@free.fr>
2017-11-21 20:01:48 +0100

    Double values

--- src/search.cpp
+++ src/search.cpp
@@ -205,7 +205,7 @@ void MainThread::search() {
 
   int contempt = Options["Contempt"] * PawnValueEg / 100; // From centipawns
 
-  contempt += 8;
+  contempt += 50;
 
   DrawValue[ us] = VALUE_DRAW - Value(contempt);
   DrawValue[~us] = VALUE_DRAW + Value(contempt);
