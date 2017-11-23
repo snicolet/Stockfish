557bb2a2366495a086c22833a9a3868512ce8f95
Stéphane Nicolet <cassio@free.fr>
2017-11-23 09:20:39 +0100

    Keep more pieces when attacking

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -750,7 +750,7 @@ namespace {
     int initiative_mg, initiative_eg;
 
     Value mg = mg_value(s);
-    Value eg = eg_value(s) + 20 * Optimism[ALL_PIECES][WHITE];
+    Value eg = eg_value(s);
 
     int asymmetry   = pe->pawn_asymmetry();
     int pawns       = pos.count<PAWN>();
