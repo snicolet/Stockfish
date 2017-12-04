0d70fb3b3b2247b7cf3e20a47d8de333a0be5788
Stéphane Nicolet <cassio@free.fr>
2017-12-04 18:22:35 +0100

    Simplify away the improving flag for FutilityMoveCounts[]

--- src/search.cpp
+++ src/search.cpp
@@ -72,7 +72,7 @@ namespace {
   Value futility_margin(Depth d) { return Value(150 * d / ONE_PLY); }
 
   // Futility and reductions lookup tables, initialized at startup
-  int FutilityMoveCounts[2][16]; // [improving][depth]
+  int FutilityMoveCounts[16];    // [depth]
   int Reductions[2][2][64][64];  // [pv][improving][depth][moveNumber]
 
   template <bool PvNode> Depth reduction(bool i, Depth d, int mn) {
@@ -160,10 +160,7 @@ void Search::init() {
           }
 
   for (int d = 0; d < 16; ++d)
-  {
-      FutilityMoveCounts[0][d] = int(2.4 + 0.74 * pow(d, 1.78));
-      FutilityMoveCounts[1][d] = int(5.0 + 1.00 * pow(d, 2.00));
-  }
+      FutilityMoveCounts[d] = int(2.4 + 0.74 * pow(d, 1.78));
 }
 
 
@@ -814,7 +811,7 @@ moves_loop: // When in check search starts from here
                   : pos.gives_check(move);
 
       moveCountPruning =   depth < 16 * ONE_PLY
-                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
+                        && moveCount >= FutilityMoveCounts[depth / ONE_PLY];
 
       // Step 12. Singular and Gives Check Extensions
 
