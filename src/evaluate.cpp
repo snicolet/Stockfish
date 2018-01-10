89c78b5e30ed5a1caadf87325f9ab58d043765c8
Stéphane Nicolet <cassio@free.fr>
2018-01-11 00:10:52 +0100

    A simplification on top of ks_mobility2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -484,16 +484,12 @@ namespace {
                      + 143 * popcount(pos.pinned_pieces(Us) | unsafeChecks)
                      - 848 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
-                     +  40;
+                     +  40
+                     +  mg_value(mobility[Them] - mobility[Us]);
 
         // Transform the kingDanger units into a Score, and substract it from the evaluation
         if (kingDanger > 0)
-        {
-            int mobilityDanger = mg_value(mobility[Them] - mobility[Us]);
-            kingDanger = std::max(0, kingDanger + mobilityDanger);
-
             score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
-        }
     }
 
     // King tropism: firstly, find squares that opponent attacks in our king flank
