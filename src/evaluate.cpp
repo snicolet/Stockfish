e75a116e3b9ef19139c99964890001dc0bf99ff3
Stéphane Nicolet <cassio@free.fr>
2018-02-02 12:02:33 +0100

    Trying CloseEnemies=S(6,0);
    Bench: 5210088

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -222,7 +222,7 @@ namespace {
   const Score RookOnPawn            = S(  8, 24);
   const Score TrappedRook           = S( 92,  0);
   const Score WeakQueen             = S( 50, 10);
-  const Score CloseEnemies          = S(  5,  0);
+  const Score CloseEnemies          = S(  6,  0);
   const Score PawnlessFlank         = S( 20, 80);
   const Score ThreatBySafePawn      = S(192,175);
   const Score ThreatByRank          = S( 16,  3);
