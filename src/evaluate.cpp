d307055e6a350314fe70c31ad1c36fb61af00cbd
Stéphane Nicolet <cassio@free.fr>
2018-02-03 16:14:46 +0100

    Use opposite colored bishop for attack with queen

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -769,9 +769,13 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
+    int oppositeBishops = !pos.opposite_bishops() ? 0 : 
+                           pos.pieces(QUEEN) ? 1 : -1;
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (pe->pawn_asymmetry() + kingDistance - 17) + 12 * pos.count<PAWN>() + 16 * bothFlanks;
+    int initiative =    8 * (pe->pawn_asymmetry() + kingDistance + oppositeBishops - 17)
+                     + 12 * pos.count<PAWN>()
+                     + 16 * bothFlanks;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
