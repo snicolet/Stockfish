057aca7ca30f87daa7aa0d5d765526913956579e
Stéphane Nicolet <cassio@free.fr>
2018-02-05 18:21:37 +0100

    Tweak reductions

--- src/search.cpp
+++ src/search.cpp
@@ -933,7 +933,7 @@ moves_loop: // When in check search starts from here
           else
           {
               // Decrease reduction if opponent's move count is high
-              if ((ss-1)->moveCount > 15)
+              if ((ss-1)->moveCount > 10)
                   r -= ONE_PLY;
 
               // Decrease reduction for exact PV nodes
