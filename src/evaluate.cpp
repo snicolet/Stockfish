91beccafc005d24a6abe308e7047b83176e8af58
Stéphane Nicolet <cassio@free.fr>
2018-02-09 15:17:06 +0100

    Tweak rankFactor[]
    Suggested by https://github.com/jerrydonaldwatson/Stockfish/compare/d71adc5...4a4d5c3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -210,7 +210,7 @@ namespace {
   };
 
   // Rank factor applied on some bonus for passed pawn on rank 4 or beyond
-  const int RankFactor[RANK_NB] = {0, 0, 0, 2, 7, 12, 19};
+  const int RankFactor[RANK_NB] = {0, 0, 0, 3, 8, 13, 20};
 
   // KingProtector[PieceType-2] contains a bonus according to distance from king
   const Score KingProtector[] = { S(-3, -5), S(-4, -3), S(-3, 0), S(-1, 1) };
