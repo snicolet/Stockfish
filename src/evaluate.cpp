a3f2d886f5084c669a3f2fd0741859faa4faf564
Stéphane Nicolet <cassio@free.fr>
2018-02-11 19:02:16 +0100

    Remove pieces
    Bench: 5072009

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -502,8 +502,8 @@ namespace {
                      + 143 * popcount(pos.pinned_pieces(Us) | unsafeChecks)
                      - 848 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
-                     +   4 * (pos.count<ALL_PIECES>(Them) + tropism)
-                     -  30 ;
+                     +   8 * tropism
+                     -  40 ;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)
