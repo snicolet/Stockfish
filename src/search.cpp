0e78abb9969625169a0db7854ac4eb4ec6999a07
Stéphane Nicolet <cassio@free.fr>
2018-02-13 11:54:00 +0100

    Soft futility pruning, take 2. Bench: 4336799

--- src/search.cpp
+++ src/search.cpp
@@ -68,7 +68,7 @@ namespace {
 
   // Razoring and futility margins
   const int RazorMargin = 600;
-  Value futility_margin(Depth d) { return Value(150 * d / ONE_PLY); }
+  Value futility_margin(Depth d) { return Value(30 + 150 * d / ONE_PLY); }
 
   // Futility and reductions lookup tables, initialized at startup
   int FutilityMoveCounts[2][16]; // [improving][depth]
@@ -665,22 +665,7 @@ namespace {
     if (skipEarlyPruning || !pos.non_pawn_material(pos.side_to_move()))
         goto moves_loop;
 
-    // Step 7. Futility pruning: child node (skipped when in check)
-    if (   !rootNode
-        &&  depth < 7 * ONE_PLY
-        &&  eval - 30 > beta
-        &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
-    {
-        if (eval - futility_margin(depth) >= beta)
-            return eval;
-        
-        if (depth < 4 * ONE_PLY)
-            depth = std::max(DEPTH_ZERO, depth - ONE_PLY);
-    }
-    
-    assert(depth >= DEPTH_ZERO);
-
-    // Step 8. Razoring (skipped when in check)
+    // Step 7. Razoring (skipped when in check)
     if (   !PvNode
         &&  depth < 4 * ONE_PLY
         &&  eval + RazorMargin <= alpha)
@@ -693,6 +678,21 @@ namespace {
         if (v <= ralpha)
             return v;
     }
+
+    // Step 8. Futility pruning: child node (skipped when in check)
+    if (   !rootNode
+        &&  depth < 7 * ONE_PLY
+        &&  eval - 30 > beta
+        &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
+    {
+        if (depth < 4 * ONE_PLY)
+            depth = std::max(DEPTH_ZERO, depth - ONE_PLY);
+
+        if (eval - futility_margin(depth) >= beta)
+            return eval;
+    }
+
+    assert(depth >= DEPTH_ZERO);
 
     // Step 9. Null move search with verification search
     if (   !PvNode
