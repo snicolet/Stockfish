aa78c31819761b2de39fc1cbeedb9a070325371d
Stéphane Nicolet <cassio@free.fr>
2018-02-16 22:28:51 +0100

    Multiplier 24, constant 20. Bench: 4901394

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -486,8 +486,9 @@ namespace {
                      + 191 * popcount(kingRing[Us] & weak)
                      + 143 * popcount(pos.pinned_pieces(Us) | unsafeChecks)
                      - 848 * !pos.count<QUEEN>(Them)
+                     +  24 * pos.count<ROOK>(Them)
                      -   9 * mg_value(score) / 8
-                     +  40;
+                     +  20;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)
