d67e154d924ad5f2df1bc1a20094388bd1e807e2
Stéphane Nicolet <cassio@free.fr>
2018-02-20 23:43:35 +0100

    Same bonus for the endgame. Bench: 5619413

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -617,7 +617,7 @@ namespace {
       return std::min(distance(pos.square<KING>(c), s), 5);
     };
 
-    Bitboard b, bb, squaresToQueen, defendedSquares, unsafeSquares, supported;
+    Bitboard b, bb, squaresToQueen, defendedSquares, unsafeSquares;
     Score score = SCORE_ZERO;
 
     b = pe->passed_pawns(Us);
@@ -675,14 +675,13 @@ namespace {
 
                 else if (defendedSquares & blockSq)
                     k += 4;
+                
+                // Passed pawn doubly supported
+                if (   more_than_one(pos.attacks_from<PAWN>(s, Them) & pos.pieces(Us, PAWN))
+                    && pos.count<PAWN>(Us) > pos.count<PAWN>(Them))
+                    k += 4;
 
                 bonus += make_score(k * w, k * w);
-
-                // Passed pawns protected by own pawns
-                supported = pos.attacks_from<PAWN>(s, Them) & pos.pieces(Us, PAWN);
-                if (   more_than_one(supported)
-                    && pos.count<PAWN>(Us) > pos.count<PAWN>(Them))
-                    bonus += make_score(3 * w, 0);
             }
             else if (pos.pieces(Us) & blockSq)
                 bonus += make_score(w + r * 2, w + r * 2);
