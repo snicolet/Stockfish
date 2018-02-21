14ec2d4fa2a9ab6a0da7cb53495298e0e54bbb6d
Stéphane Nicolet <cassio@free.fr>
2018-02-21 22:39:18 +0100

    Try k+=2. Bench: 5619406

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -674,6 +674,11 @@ namespace {
                 else if (defendedSquares & blockSq)
                     k += 4;
 
+                // Passed pawn doubly supported
+                if (   more_than_one(pos.attacks_from<PAWN>(s, Them) & pos.pieces(Us, PAWN))
+                    && pos.count<PAWN>(Us) > pos.count<PAWN>(Them))
+                    k += 2;
+
                 bonus += make_score(k * w, k * w);
             }
             else if (pos.pieces(Us) & blockSq)
