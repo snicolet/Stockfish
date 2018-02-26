bff0c7da491586c1513930c8b3939f15874839e8
Stéphane Nicolet <cassio@free.fr>
2018-02-26 12:00:28 +0100

    Take 3: PawnMobility=S(5, 10)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -763,7 +763,7 @@ namespace {
                     +  8 * pe->pawn_asymmetry()
                     + 12 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
-                    -136 ;
+                    - 136;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
