15a7a6f86df863ce5ec12995839026981efecc41
Stéphane Nicolet <cassio@free.fr>
2018-03-04 23:10:35 +0100

    PawnMobility=S(3,9)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -170,7 +170,7 @@ namespace {
   const Score LongRangedBishop  = S( 22,  0);
   const Score MinorBehindPawn   = S( 16,  0);
   const Score PawnlessFlank     = S( 20, 80);
-  const Score PawnMobility      = S(  6, 10);
+  const Score PawnMobility      = S(  3,  9);
   const Score RookOnPawn        = S(  8, 24);
   const Score ThreatByPawnPush  = S( 47, 26);
   const Score ThreatByRank      = S( 16,  3);
