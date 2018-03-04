13cfd2b4f7437ae847d8a1b3888879e4a73b2880
Stéphane Nicolet <cassio@free.fr>
2018-03-05 00:19:13 +0100

    Pawn mobility in initiative: half bonus

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -760,12 +760,12 @@ namespace {
     int pawnMobility = popcount(mobilePawns[WHITE] | mobilePawns[BLACK]);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   8 * outflanking
-                    +  8 * pawnMobility
+    int complexity =   4 * pawnMobility
+                    +  8 * outflanking
                     +  8 * pe->pawn_asymmetry()
                     + 12 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
-                    -200 ;
+                    -170 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
