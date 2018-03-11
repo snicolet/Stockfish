5d5d67f51b4800f737203b934be02a9f809c3121
Stéphane Nicolet <cassio@free.fr>
2018-03-11 05:37:57 +0100

    Malus = S(25,15)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -172,7 +172,7 @@ namespace {
   const Score LongRangedBishop  = S( 22,  0);
   const Score MinorBehindPawn   = S( 16,  0);
   const Score PawnlessFlank     = S( 20, 80);
-  const Score QueenOverload     = S( 16,  8);
+  const Score QueenOverload     = S( 25, 15);
   const Score RookOnPawn        = S(  8, 24);
   const Score SliderOnQueen     = S( 42, 21);
   const Score ThreatByPawnPush  = S( 47, 26);
