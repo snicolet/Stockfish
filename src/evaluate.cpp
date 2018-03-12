28eb22ff7df078ac74f9d9e30da493cda44497da
Stephane Nicolet <cassio@free.fr>
2018-03-12 14:56:29 +0100

    Avoid engames with blocked center and lots of pawns

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -172,6 +172,7 @@ namespace {
   const Score LongRangedBishop  = S( 22,  0);
   const Score MinorBehindPawn   = S( 16,  0);
   const Score PawnlessFlank     = S( 20, 80);
+  const Score PawnMobility      = S(  0,  8);
   const Score RookOnPawn        = S(  8, 24);
   const Score SliderOnQueen     = S( 42, 21);
   const Score ThreatByPawnPush  = S( 47, 26);
@@ -576,6 +577,10 @@ namespace {
     // Keep only the squares which are not completely unsafe
     b &= ~attackedBy[Them][PAWN]
         & (attackedBy[Us][ALL_PIECES] | ~attackedBy[Them][ALL_PIECES]);
+
+    // Pawn mobility, to keep fluidity of position
+    if (pos.count<PAWN>() >= 10)
+        score += PawnMobility * popcount(b & CenterFiles);
 
     // Bonus for safe pawn threats on the next move
     b =   pawn_attacks_bb<Us>(b)
