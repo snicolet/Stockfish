31fc603449daea3a8f30e20d54ffa7233da73956
Stéphane Nicolet <cassio@free.fr>
2018-03-17 01:57:21 +0100

    Playing the quiet moves by the bigger pieces first

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -119,7 +119,8 @@ void MovePicker::score() {
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if (Type == QUIETS)
-          m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
+          m.value =  16 * type_of(pos.piece_on(from_sq(m)))
+                   + (*mainHistory)[pos.side_to_move()][from_to(m)]
                    + (*contHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + (*contHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*contHistory[3])[pos.moved_piece(m)][to_sq(m)];
