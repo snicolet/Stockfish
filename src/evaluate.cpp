abc71674667cb9bb0d1f9b9b53ec6d84cc4aa9c3
Stéphane Nicolet <cassio@free.fr>
2018-03-18 02:06:14 +0100

    Initiative pawn weight=14

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -764,10 +764,10 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * outflanking
                     +  8 * pe->pawn_asymmetry()
-                    + 12 * pos.count<PAWN>()
+                    + 14 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    -136 ;
+                    -156 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
