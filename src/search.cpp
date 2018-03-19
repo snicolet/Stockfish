410d603177b6394c323b7db4603fe7b63d895b9f
Stéphane Nicolet <cassio@free.fr>
2018-03-19 23:03:02 +0100

    Denominator 256

--- src/search.cpp
+++ src/search.cpp
@@ -350,7 +350,7 @@ void Thread::search() {
               ct =  Options["Contempt"] * PawnValueEg / 100; // From centipawns
 
               // Adjust contempt based on current bestValue (dynamic contempt)
-              ct += int(std::round(48 * atan(float(bestValue) / 32)));
+              ct += int(std::round(48 * atan(float(bestValue) / 256)));
 
               Eval::Contempt = (us == WHITE ?  make_score(ct, ct / 2)
                                             : -make_score(ct, ct / 2));
