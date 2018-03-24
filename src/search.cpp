a8f48bc61d2dc35dfd1caf2ddc4f9e458f18861e
Stéphane Nicolet <cassio@free.fr>
2018-03-24 10:55:06 +0100

    depth >= 7

--- src/search.cpp
+++ src/search.cpp
@@ -789,7 +789,7 @@ namespace {
     }
 
     // Step 11. Internal iterative deepening (skipped when in check)
-    if (    depth >= 6 * ONE_PLY
+    if (    depth >= 7 * ONE_PLY
         && !ttMove
         && (PvNode || ss->staticEval + 128 >= beta))
     {
