1b293f25b493ee6f7d7973f1d45171dcfc7cbbed
Stéphane Nicolet <cassio@free.fr>
2018-03-24 23:56:58 +0100

    Mobile pawn anywhere, bonus=S(0,16)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -173,7 +173,7 @@ namespace {
   constexpr Score LongDiagonalBishop = S( 22,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score PawnlessFlank      = S( 20, 80);
-  constexpr Score PawnMobility       = S(  0,  8);
+  constexpr Score PawnMobility       = S(  0, 16);
   constexpr Score RookOnPawn         = S(  8, 24);
   constexpr Score SliderOnQueen      = S( 42, 21);
   constexpr Score ThreatByPawnPush   = S( 47, 26);
