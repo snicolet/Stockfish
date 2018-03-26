cda0fbec1e561609ce1baf1da8d7c6ebdb429f59
Stéphane Nicolet <cassio@free.fr>
2018-03-26 12:37:06 +0200

    Mobile center with pawn levers, bonus = S(0,16)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -173,6 +173,7 @@ namespace {
   constexpr Score LongDiagonalBishop = S( 22,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score PawnlessFlank      = S( 20, 80);
+  constexpr Score PawnMobility       = S(  0, 16);
   constexpr Score RookOnPawn         = S(  8, 24);
   constexpr Score SliderOnQueen      = S( 42, 21);
   constexpr Score ThreatByPawnPush   = S( 47, 26);
@@ -583,6 +584,12 @@ namespace {
     // Keep only the squares which are not completely unsafe
     b &= ~attackedBy[Them][PAWN]
         & (attackedBy[Us][ALL_PIECES] | ~attackedBy[Them][ALL_PIECES]);
+
+    b |=  pos.pieces(Them, PAWN)
+        & attackedBy[Us][PAWN]
+        & ~attackedBy[Them][ALL_PIECES];
+
+    score += PawnMobility * bool(b & CenterFiles);
 
     // Bonus for safe pawn threats on the next move
     b =   pawn_attacks_bb<Us>(b)
