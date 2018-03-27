d06f3ebceeb0cdce8d23fc24f0b277b2e7345704
Stéphane Nicolet <cassio@free.fr>
2018-03-28 00:51:09 +0200

    Take 3

--- src/search.cpp
+++ src/search.cpp
@@ -157,7 +157,7 @@ void Search::init() {
               double r = log(d) * log(mc) / 1.95;
 
               Reductions[NonPV][imp][d][mc] = !imp + int(std::round(r));
-              Reductions[PV][imp][d][mc] = std::max(Reductions[NonPV][imp][d][mc] - 1, 0);
+              Reductions[PV][imp][d][mc] = std::max(Reductions[NonPV][imp][d][mc], 0);
           }
 
   for (int d = 0; d < 16; ++d)
