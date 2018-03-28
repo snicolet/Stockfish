4238ec22f79e481d1d8f5ceab1f525adf55d7883
Stéphane Nicolet <cassio@free.fr>
2018-03-28 15:37:10 +0200

    kd_queen = 888

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -473,7 +473,7 @@ namespace {
                      + 102 * kingAttacksCount[Them]
                      + 191 * popcount(kingRing[Us] & weak)
                      + 143 * popcount(pinned | unsafeChecks)
-                     - 868 * !pos.count<QUEEN>(Them)
+                     - 888 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
                      +  40;
 
