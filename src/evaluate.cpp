a524b07ecad9b53a575ddb83a00e22d84150f0cc
Stéphane Nicolet <cassio@free.fr>
2018-03-29 02:50:45 +0200

    Count passed pawns in pure opposite bishops endings

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -805,7 +805,8 @@ namespace {
             // is almost a draw, in case of KBP vs KB, it is even more a draw.
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = more_than_one(pos.pieces(PAWN)) ? 31 : 9;
+                sf = more_than_one(pos.pieces(PAWN)) ? 31 + 4 * popcount(pe->passed_pawns(strongSide))
+                                                     : 9;
 
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
