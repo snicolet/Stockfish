273bee37f4cc6e9131e12ea29ceadc9011c23e04
Stéphane Nicolet <cassio@free.fr>
2018-04-02 12:35:03 +0200

    Take 2

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -158,7 +158,7 @@ namespace {
         else if (   stoppers == SquareBB[s + Up]
                  && relative_rank(Us, s) >= RANK_5)
         {
-            b = phalanx | (shift<Up>(supported) & ~theirPawns);
+            b = shift<Up>(supported) & ~theirPawns;
             while (b)
             {
                 s2 = pop_lsb(&b);
