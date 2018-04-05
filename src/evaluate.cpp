403c10504e5d4d552478d2a8bc27867085c8f2d8
Stéphane Nicolet <cassio@free.fr>
2018-04-06 01:08:40 +0200

    Endgame, take 3 (5806509)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -401,7 +401,7 @@ namespace {
                 score -= WeakQueen;
         }
 
-        if (Pt == BISHOP && pos.count<BISHOP>() <= 2)
+        if (Pt == BISHOP && pos.count<BISHOP>() <= 1)
         {
             int x = (pos.key() & 7) - 3;
             score += make_score(0, x);
