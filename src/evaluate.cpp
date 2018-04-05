d7cd7d443a25408d60974f6fd0f885894afa2fa9
Stephane Nicolet <cassio@free.fr>
2018-04-05 22:10:26 +0200

    Take 3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -401,7 +401,7 @@ namespace {
                 score -= WeakQueen;
         }
 
-        if (Pt == BISHOP && pos.count<BISHOP>() <= 2)
+        if (Pt == BISHOP && pos.count<BISHOP>(Them) <= 1)
         {
             int x = (pos.key() & 7) - 3;
             score += make_score(x, 0);
