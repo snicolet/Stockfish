7886999cd90e2585a06cbba1f726e2118593f378
Stéphane Nicolet <cassio@free.fr>
2018-04-14 10:06:24 +0200

    Take the singular extension with probability 75%

--- src/search.cpp
+++ src/search.cpp
@@ -878,7 +878,7 @@ moves_loop: // When in check, search starts from here
           value = search<NonPV>(pos, ss, rBeta - 1, rBeta, depth / 2, cutNode, true);
           ss->excludedMove = MOVE_NONE;
 
-          if (value < rBeta)
+          if (value < rBeta && (pos.key() & 3))
               extension = ONE_PLY;
       }
       else if (    givesCheck // Check extension (~2 Elo)
