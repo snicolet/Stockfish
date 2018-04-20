3777bb3b6eff8ea7e7b191e4762f3cbe3e163e95
Stéphane Nicolet <cassio@free.fr>
2018-04-20 09:59:36 +0200

    One more pawn

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -807,7 +807,7 @@ namespace {
             // Endgame with opposite-colored bishops and no other pieces is almost a draw
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = 31;
+                sf = (pos.count<PAWN>(strongSide) - pos.count<PAWN>(~strongSide) == 1) ? 20 : 31;
 
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
