e68e0a28fb43c654d8f77506a484504f1ffe29b2
Stéphane Nicolet <cassio@free.fr>
2018-05-02 00:59:36 +0200

    Take2: lazy=1800+300*ocb
    bench 5190909

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -850,7 +850,7 @@ namespace {
 
     // Early exit if score is high
     Value v = (mg_value(score) + eg_value(score)) / 2;
-    if (abs(v) > LazyThreshold + 600 * pos.opposite_bishops())
+    if (abs(v) > LazyThreshold + 300 * pos.opposite_bishops())
        return pos.side_to_move() == WHITE ? v : -v;
 
     // Main evaluation begins here
