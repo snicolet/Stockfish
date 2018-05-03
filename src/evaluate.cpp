9735319ffa9da89f4753cc01bcebf5b681a9f845
Stéphane Nicolet <cassio@free.fr>
2018-05-03 10:06:45 +0200

    Try 8*pawns in initiative()
    bench : 5300395

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -774,7 +774,7 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * outflanking
                     +  8 * pe->pawn_asymmetry()
-                    + 12 * pos.count<PAWN>()
+                    +  8 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
                     -136 ;
