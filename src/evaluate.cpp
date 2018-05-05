881ea3a048723c8a1675a25e97300edc033a8045
Stéphane Nicolet <cassio@free.fr>
2018-05-05 02:43:08 +0200

    Trying to get more active rooks: take 1
    Bench: 4986286

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -127,7 +127,7 @@ namespace {
 
   // RookOnFile[semiopen/open] contains bonuses for each rook when there is
   // no (friendly) pawn on the rook file.
-  constexpr Score RookOnFile[] = { S(20, 7), S(45, 20) };
+  constexpr Score RookOnFile[] = { S(25, 12), S(50, 25) };
 
   // ThreatByMinor/ByRook[attacked PieceType] contains bonuses according to
   // which piece type attacks which one. Attacks on lesser pieces which are
@@ -384,7 +384,7 @@ namespace {
                 score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
 
             // Bonus for rook on an open or semi-open file
-            if (pe->semiopen_file(Us, file_of(s)))
+            if (!(pos.pieces(Us, PAWN) & forward_file_bb(Us, s)))
                 score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
