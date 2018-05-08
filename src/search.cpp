8e5706110cdd2dde5423d8f605175e07bad75bc9
Stéphane Nicolet <cassio@free.fr>
2018-05-08 14:39:58 +0200

    Take 3
    Bench: 5018755

--- src/search.cpp
+++ src/search.cpp
@@ -996,7 +996,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Increase reduction for cut nodes (~5 Elo)
-              if ((depth / ONE_PLY) & 1)
+              if (ss->staticEval > beta + 200)
                   r += ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
