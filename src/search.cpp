72efe378efd67dc05ade274f842f45a19ce98039
Stéphane Nicolet <cassio@free.fr>
2018-05-08 14:40:43 +0200

    Take 4
    Bench: 4691210

--- src/search.cpp
+++ src/search.cpp
@@ -997,7 +997,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (ss->staticEval > beta + 200)
-                  r += ONE_PLY;
+                  r += 2 * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
