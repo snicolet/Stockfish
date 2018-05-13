7eec4a2eb3379e10057a1640abb44723c1b08bd2
Stéphane Nicolet <cassio@free.fr>
2018-05-13 15:42:40 +0200

    Combo with Stefan

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -479,7 +479,7 @@ namespace {
                      + 143 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                      - 848 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
-                     +  40;
+                     -  40;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)
