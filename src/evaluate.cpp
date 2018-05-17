7d8dd38b695e77c2418f4113f2b6844ec7bc9ae6
Stéphane Nicolet <cassio@free.fr>
2018-05-17 22:09:55 +0200

    Take 2ter: (pawn_span >= 5) and compensate (-131)
    Bench: 4732385

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -773,7 +773,7 @@ namespace {
                     + 12 * pos.count<PAWN>()
                     + 16 * (pe->pawn_span() >= 5)
                     + 48 * !pos.non_pawn_material()
-                    -141 ;
+                    -131 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
