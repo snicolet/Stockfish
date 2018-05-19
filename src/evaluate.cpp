95e8ec5961cb250ef592059f983b9508d7b5d183
Stéphane Nicolet <cassio@free.fr>
2018-05-19 08:00:05 +0200

    Double count, and pawns in the connex composant
    Bench: 4816597

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -614,8 +614,8 @@ namespace {
     }
 
     // Connectivity: ensure that knights, bishops, rooks, and queens are protected
-    b = (pos.pieces(Us) ^ pos.pieces(Us, PAWN, KING)) & attackedBy[Us][ALL_PIECES];
-    score += Connectivity * popcount(b);
+    b = (pos.pieces(Us) ^ pos.pieces(Us, KING)) & attackedBy[Us][ALL_PIECES];
+    score += Connectivity * (popcount(b) + popcount(b & attackedBy2[Us]));
 
     if (T)
         Trace::add(THREAT, Us, score);
