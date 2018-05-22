caaa783a139dd573fd5d2728b840796125240d9c
Stéphane Nicolet <cassio@free.fr>
2018-05-22 15:46:11 +0200

    Cycle detection in normal search: only when in check
    Bench: 4870117

--- src/search.cpp
+++ src/search.cpp
@@ -582,6 +582,7 @@ namespace {
         // earlier move to this position.
         if (   pos.rule50_count() >= 3
             && alpha < VALUE_DRAW
+            && inCheck
             && pos.has_game_cycle(ss->ply))
         {
             alpha = VALUE_DRAW;
