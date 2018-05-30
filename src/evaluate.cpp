71ba6b2876d20111f764a2b6eca9ea2ef1e43d53
Stéphane Nicolet <cassio@free.fr>
2018-05-30 11:12:33 +0200

    Take 6
    Bench: 4781512

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -488,7 +488,7 @@ namespace {
                      - 848 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
                      +   4 * tropism
-                     +  30;
+                     +  40;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)
