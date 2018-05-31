5bcb6fe016c3b73ec1de294c338b6c70cae93b02
Stéphane Nicolet <cassio@free.fr>
2018-05-31 09:37:21 +0200

    midgame -= 1/16
    Bench: 4696369

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -617,7 +617,7 @@ namespace {
     b = (pos.pieces(Us) ^ pos.pieces(Us, PAWN, KING)) & attackedBy[Us][ALL_PIECES];
     score += Connectivity * popcount(b);
 
-    score += make_score(mg_value(score) / 16, 0);
+    score -= make_score(mg_value(score) / 16, 0);
 
     if (T)
         Trace::add(THREAT, Us, score);
