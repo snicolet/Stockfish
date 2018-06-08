4c7cea463235a8182d708aafd6d98895e4cb77d8
Stéphane Nicolet <cassio@free.fr>
2018-06-08 16:22:58 +0200

    Revert to the contempt used in TCEC 11
    Bench: 4924236

--- src/search.cpp
+++ src/search.cpp
@@ -379,7 +379,9 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + 88 * previousScore / (abs(previousScore) + 200);
+              int dct = ct + (previousScore >  500 ?  50 :
+                              previousScore < -500 ? -50 :
+                              previousScore / 10);
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));
