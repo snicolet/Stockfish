2407868001a951de21bee61f9365cde41cade2bc
Stéphane Nicolet <cassio@free.fr>
2018-06-11 17:49:25 +0200

    Increase contempt mg->eg gradient
    Bench: 4594100

--- src/search.cpp
+++ src/search.cpp
@@ -319,8 +319,8 @@ void Thread::search() {
           : ct;
 
   // In evaluate.cpp the evaluation is from the white point of view
-  contempt = (us == WHITE ?  make_score(ct, ct / 2)
-                          : -make_score(ct, ct / 2));
+  contempt = (us == WHITE ?  make_score(ct, ct / 4)
+                          : -make_score(ct, ct / 4));
 
   // Iterative deepening loop until requested to stop or the target depth is reached
   while (   (rootDepth += ONE_PLY) < DEPTH_MAX
@@ -372,8 +372,8 @@ void Thread::search() {
               // Adjust contempt based on root move's previousScore (dynamic contempt)
               int dct = ct + 88 * previousScore / (abs(previousScore) + 200);
 
-              contempt = (us == WHITE ?  make_score(dct, dct / 2)
-                                      : -make_score(dct, dct / 2));
+              contempt = (us == WHITE ?  make_score(dct, dct / 4)
+                                      : -make_score(dct, dct / 4));
           }
 
           // Start with a small aspiration window and, in the case of a fail
