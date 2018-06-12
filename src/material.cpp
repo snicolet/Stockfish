356e5cc8340cb26ceb09a2ad19700f3a5f22994c
Stéphane Nicolet <cassio@free.fr>
2018-06-12 11:37:41 +0200

    Tweak game phase to avoid minor exchanges
    Bench: 4714003

--- src/material.cpp
+++ src/material.cpp
@@ -132,7 +132,8 @@ Entry* probe(const Position& pos) {
 
   Value npm_w = pos.non_pawn_material(WHITE);
   Value npm_b = pos.non_pawn_material(BLACK);
-  Value npm = std::max(EndgameLimit, std::min(npm_w + npm_b, MidgameLimit));
+  Value npm = npm_w + npm_b + 32 * (pos.count<BISHOP>() + pos.count<KNIGHT>()) - 128;
+  npm = std::max(EndgameLimit, std::min(npm, MidgameLimit));
 
   // Map total non-pawn material into [PHASE_ENDGAME, PHASE_MIDGAME]
   e->gamePhase = Phase(((npm - EndgameLimit) * PHASE_MIDGAME) / (MidgameLimit - EndgameLimit));
