0a4b66218f3d7557ac4cebbf05b005156c9a0f46
Stéphane Nicolet <cassio@free.fr>
2018-06-30 08:51:51 +0200

    Take 3ter, bonus=S(25,0)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -572,7 +572,7 @@ namespace {
     if (pos.pieces(Us, ROOK, QUEEN))
         score += WeakUnopposedPawn * pe->weak_unopposed(Them);
 
-    constexpr Score weakPawn = make_score(15, 0);
+    constexpr Score weakPawn = make_score(25, 0);
     b =  pos.pieces(Them, PAWN)
        & attackedBy2[Us]
        & ~attackedBy[Them][PAWN];
