dab9694c75779261987e6e959ed19dd4de145c0f
Stéphane Nicolet <cassio@free.fr>
2018-07-26 08:32:49 +0200

    Double effect for Stefan's pinned threat: take 2
    Bench: 4777861

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -538,10 +538,12 @@ namespace {
         {
             Square s = pop_lsb(&b);
             score += ThreatByMinor[type_of(pos.piece_on(s))];
-
             if (   type_of(pos.piece_on(s)) != PAWN
                 || (pos.blockers_for_king(Them) & s))
                 score += ThreatByRank * (int)relative_rank(Them, s);
+
+            else if (pos.blockers_for_king(Them) & s)
+                score += ThreatByRank * (int)relative_rank(Them, s) / 2;
         }
 
         b = weak & attackedBy[Us][ROOK];
@@ -549,11 +551,10 @@ namespace {
         {
             Square s = pop_lsb(&b);
             score += ThreatByRook[type_of(pos.piece_on(s))];
-            if (type_of(pos.piece_on(s)) != PAWN)
-                score += ThreatByRank * (int)relative_rank(Them, s);
 
-            else if (pos.blockers_for_king(Them) & s)
-                score += ThreatByRank * (int)relative_rank(Them, s) / 2;
+            if (   type_of(pos.piece_on(s)) != PAWN
+                || (pos.blockers_for_king(Them) & s))
+                score += ThreatByRank * (int)relative_rank(Them, s);
         }
 
         if (weak & attackedBy[Us][KING])
