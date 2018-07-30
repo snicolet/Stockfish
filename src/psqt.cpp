c3fae0f3f422c09383840a5f5d89d76731c1f8b1
Stéphane Nicolet <cassio@free.fr>
2018-07-30 09:07:42 +0200

    Look for pivot pawn of chains, take 3
    Bench: 4559714

--- src/psqt.cpp
+++ src/psqt.cpp
@@ -31,20 +31,23 @@ namespace PSQT {
 
 #define S(mg, eg) make_score(mg, eg)
 
+constexpr Score A = S(0, 2);
+constexpr Score B = S(0, 3);
+
 // Bonus[PieceType][Square / 2] contains Piece-Square scores. For each piece
 // type on a given square a (middlegame, endgame) score pair is assigned. Table
 // is defined for files A..D and white side: it is symmetric for black side and
 // second half of the files.
-constexpr Score Bonus[][RANK_NB][int(FILE_NB) / 2] = {
+const Score Bonus[][RANK_NB][int(FILE_NB) / 2] = {
   { },
   { // Pawn
-   { S(  0, 0), S(  0, 0), S(  0, 0), S( 0, 0) },
-   { S(-11, 7), S(  6,-4), S(  7, 8), S( 3,-2) },
-   { S(-18,-4), S( -2,-5), S( 19, 5), S(24, 4) },
-   { S(-17, 3), S( -9, 3), S( 20,-8), S(35,-3) },
-   { S( -6, 8), S(  5, 9), S(  3, 7), S(21,-6) },
-   { S( -6, 8), S( -8,-5), S( -6, 2), S(-2, 4) },
-   { S( -4, 3), S( 20,-9), S( -8, 1), S(-4,18) }
+   { S(  0, 0), S(  0, 0), A + S(  0, 0), B + S( 0, 0) },
+   { S(-11, 7), S(  6,-4), A + S(  7, 8), B + S( 3,-2) },
+   { S(-18,-4), S( -2,-5), A + S( 19, 5), B + S(24, 4) },
+   { S(-17, 3), S( -9, 3), A + S( 20,-8), B + S(35,-3) },
+   { S( -6, 8), S(  5, 9), A + S(  3, 7), B + S(21,-6) },
+   { S( -6, 8), S( -8,-5), A + S( -6, 2), B + S(-2, 4) },
+   { S( -4, 3), S( 20,-9), A + S( -8, 1), B + S(-4,18) }
   },
   { // Knight
    { S(-161,-105), S(-96,-82), S(-80,-46), S(-73,-14) },
