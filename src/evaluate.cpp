e3dccccb083ac50ad641b57adb9adf7121a0b6ed
Stéphane Nicolet <cassio@free.fr>
2018-08-01 22:17:53 +0200

    Take 2, but offset = -160
    Bench: 4960998

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -773,7 +773,9 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    -152 ;
+                    -160 ;
+
+    //dbg_mean_of(complexity);
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
