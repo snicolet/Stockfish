47af0ab39878418d00e742e27f80c8589941b572
Stéphane Nicolet <cassio@free.fr>
2018-08-02 12:40:01 +0200

    quadratic kingAttackersCount
    Bench: 4692166

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -474,7 +474,7 @@ namespace {
         // the square is in the attacker's mobility area.
         unsafeChecks &= mobilityArea[Them];
 
-        kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
+        kingDanger +=        kingAttackersCount[Them] * kingAttackersCount[Them] * kingAttackersWeight[Them] / 3
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
                      + 129 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
