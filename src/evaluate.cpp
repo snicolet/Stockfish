0863f51cded707c88d8507572b6177a46838ee33
Stéphane Nicolet <cassio@free.fr>
2018-08-03 06:33:09 +0200

    Compensate KingAttackWeights (+20)
    Bench: 4882692

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -89,7 +89,7 @@ namespace {
   constexpr Value SpaceThreshold = Value(12222);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
-  constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 87, 65, 54, 20 };
+  constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 97, 75, 64, 30 };
 
   // Penalties for enemy's safe checks
   constexpr int QueenSafeCheck  = 780;
