e64aa324348b1180b60544a28eac031557ca099b
Stéphane Nicolet <cassio@free.fr>
2018-08-03 11:40:00 +0200

    Revert to master

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -474,14 +474,14 @@ namespace {
         // the square is in the attacker's mobility area.
         unsafeChecks &= mobilityArea[Them];
 
-        kingDanger +=       (kingAttackersCount[Them] - 1) * kingAttackersWeight[Them]
+        kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
                      + 129 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                      +   4 * tropism
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
-                     -  35 ;
+                     -  30 ;
 
         // dbg_mean_of(kingDanger);
 
