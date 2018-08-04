96e971059f2d17296b70506fed37ecf4395a68b5
Stéphane Nicolet <cassio@free.fr>
2018-08-04 10:14:19 +0200

    Back to the drawing board, take 5
    Bench: 4738563

--- src/search.cpp
+++ src/search.cpp
@@ -87,7 +87,7 @@ namespace {
 
   // PruningSafety[rootColor][cut type] : pruning safety table
   const int PruningSafety[2][2] = {
-     { 0 , -50 },  // ~rootColor : alpha, beta
+     { 0 , -25 },  // ~rootColor : alpha, beta
      { 75 , 0  }   //  rootColor : alpha, beta
   };
   enum CutType { ALPHA, BETA };
