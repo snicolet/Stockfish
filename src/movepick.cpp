022ae6cbf09f6401ffea85453c67892c9f22b46e
Stéphane Nicolet <cassio@free.fr>
2018-08-14 13:11:23 +0200

    Take 4
    Bench: 4349733

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -176,7 +176,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(move, Value(-100 - 55 * (cur-1)->value / 2048)) ?
+                       return pos.see_ge(move, Value(-55 - 55 * (cur-1)->value / 1024)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = move, false); }))
           return move;
