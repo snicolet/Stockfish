ab37525af7c8842ea88d62e270e21afbffdacf8e
Stéphane Nicolet <cassio@free.fr>
2018-08-18 15:30:46 +0200

    Deal will null move case. Also increase contempt.
    Bench: 4163140

--- src/search.cpp
+++ src/search.cpp
@@ -543,8 +543,9 @@ namespace {
     // Dive into quiescence search when the depth reaches zero
     if (depth < ONE_PLY)
     {
-        int p = (ss-1)->statScore;
-        int malus = p < 0 ? (p - 5000) / 1024 : 0;
+        int p = (ss-1)->currentMove == MOVE_NULL ? 0 : (ss-1)->statScore;
+        int malus = p > 0 ? (p + 5000) / 1024 :
+                    p < 0 ? (p - 5000) / 1024 : 0;
         Value v = qsearch<NT>(pos, ss, alpha, beta);
         return v == VALUE_DRAW ? v : v - malus;
     }
