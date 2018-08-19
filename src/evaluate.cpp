e07fce4424afcb8b38ce2cabb65b0afd7f75966f
Stéphane Nicolet <cassio@free.fr>
2018-08-19 09:41:19 +0200

    No threat bonus from pinned pawns: take 3
    Bench: 3952971

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -169,7 +169,7 @@ namespace {
   constexpr Score RookOnPawn         = S( 10, 30);
   constexpr Score SliderOnQueen      = S( 42, 21);
   constexpr Score ThreatByKing       = S( 23, 76);
-  constexpr Score ThreatByPawnPush   = S( 45, 40);
+  constexpr Score ThreatByPawnPush   = S( 50, 45);
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(173,102);
   constexpr Score TrappedRook        = S( 92,  0);
