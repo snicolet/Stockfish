9438116758f8aeff978f23e1d06541922786be24
Stéphane Nicolet <cassio@free.fr>
2018-08-20 08:37:38 +0200

    Keep more pawns for the storms
    Bench: 3985098

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -763,11 +763,11 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * pe->pawn_asymmetry()
-                    + 12 * pos.count<PAWN>()
+                    + 16 * pos.count<PAWN>()
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    -136 ;
+                    -166 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
