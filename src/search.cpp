7a292650f3af05c2b591b21f2c8b050f6145bfbf
Stéphane Nicolet <cassio@free.fr>
2018-08-21 09:26:29 +0200

    Try 3000 offset for statScore in master
    Bench: 4922778

--- src/search.cpp
+++ src/search.cpp
@@ -1024,7 +1024,7 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 5000;
+                             - 3000;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= 0 && (ss-1)->statScore < 0)
