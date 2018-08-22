350d776b091d2c371cf950841ffa5f14c55f9215
Stéphane Nicolet <cassio@free.fr>
2018-08-22 07:16:32 +0200

    After all initiative is an endgame term: try the opposite
    bench: 4424773

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -772,8 +772,8 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    +      kingDanger / 128
-                    -123;
+                    -      kingDanger / 128
+                    -136;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
