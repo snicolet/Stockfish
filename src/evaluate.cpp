ec27bd5023c1f6a66ab0858daf5a1d663cbbb0f7
Stéphane Nicolet <cassio@free.fr>
2018-08-29 03:45:35 +0200

    Take 2
    Bench: 4562896

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -482,7 +482,7 @@ namespace {
                      + 185 * popcount(kingRing[Us] & weak)
                      + 129 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                      +   4 * tropism
-                     +   4 * pieceDifference
+                     +   4 * pieceDifference * (abs(pieceDifference) >= 2)
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      -   30;
