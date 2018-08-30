fa6cfd5c496dea7410e7455e9ac9d2aa51f5d6d7
Stéphane Nicolet <cassio@free.fr>
2018-08-30 17:33:19 +0200

    Bold test: replace pawn count by pawn span in initiative
    Bench: 4226217

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -760,11 +760,10 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * pe->pawn_asymmetry()
-                    + 12 * pos.count<PAWN>()
                     + 12 * outflanking
-                    +  8 * (pe->pawn_span() >= 5)
+                    + 16 * pe->pawn_span()
                     + 48 * !pos.non_pawn_material()
-                    -117 ;
+                    - 110 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
