447fe66c2c88a47baed1af83b86a11e8408be890
Stéphane Nicolet <cassio@free.fr>
2018-09-05 08:04:10 +0200

    Last try for today
    Bench: 4362721

--- src/search.cpp
+++ src/search.cpp
@@ -530,7 +530,7 @@ namespace {
 
     int drawBonus = 0;
     if ((ss-1)->currentMove != MOVE_NULL)
-        drawBonus = 4 * ((ss-1)->statScore > 2000);
+        drawBonus = 2 * ((ss-1)->statScore > 0) - ((ss-1)->statScore < 0);
 
     // Check if we have an upcoming move which draws by repetition, or
     // if the opponent had an alternative move earlier to this position.
