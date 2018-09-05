a8133a5c36cad53cc6a636246b8322e5c152f18f
Stéphane Nicolet <cassio@free.fr>
2018-09-05 07:46:42 +0200

    Bonus for positive stat scores (2000)
    Bench: 4297063

--- src/search.cpp
+++ src/search.cpp
@@ -530,7 +530,7 @@ namespace {
 
     int drawBonus = 0;
     if ((ss-1)->currentMove != MOVE_NULL)
-        drawBonus = 4 * ((ss-1)->statScore > 1000);
+        drawBonus = 4 * ((ss-1)->statScore > 2000);
 
     // Check if we have an upcoming move which draws by repetition, or
     // if the opponent had an alternative move earlier to this position.
