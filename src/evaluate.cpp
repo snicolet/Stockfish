68aac8cc676179455476d81856c4a0e234aea789
Stéphane Nicolet <cassio@free.fr>
2018-09-06 09:47:48 +0200

    Count pawn reserve for estimating king danger attack
    Bench: 4618326

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -479,9 +479,10 @@ namespace {
                      + 185 * popcount(kingRing[Us] & weak)
                      + 129 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                      +   4 * tropism
+                     +   4 * pos.count<PAWN>(Them)
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
-                     -   30;
+                     -   50;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)
