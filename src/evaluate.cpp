3a8cba7d6aad9ea691dfc2aad0a5d2e6364c793e
Stéphane Nicolet <cassio@free.fr>
2018-09-11 09:57:45 +0200

    Like take 3, but with slightly bigger bonuses
    Bench: 4240844

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -161,13 +161,13 @@ namespace {
   constexpr Score Hanging            = S( 57, 32);
   constexpr Score HinderPassedPawn   = S(  8,  0);
   constexpr Score KingProtector      = S(  6,  6);
-  constexpr Score KnightOnQueen      = S( 21, 11);
+  constexpr Score KnightOnQueen      = S( 26, 16);
   constexpr Score LongDiagonalBishop = S( 46,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 13,  6);
   constexpr Score PawnlessFlank      = S( 19, 84);
   constexpr Score RookOnPawn         = S( 10, 30);
-  constexpr Score SliderOnQueen      = S( 42, 21);
+  constexpr Score SliderOnQueen      = S( 47, 26);
   constexpr Score ThreatByKing       = S( 23, 76);
   constexpr Score ThreatByPawnPush   = S( 45, 40);
   constexpr Score ThreatByRank       = S( 16,  3);
