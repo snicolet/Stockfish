3beb14850f9453d9cf23525f35ba043f8b844eb8
Stéphane Nicolet <cassio@free.fr>
2018-09-28 10:55:46 +0200

    Pawn avalanche, take 5
    Bench: 4150538

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -478,7 +478,7 @@ namespace {
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
                      + 129 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
-                     +   5 * tropism
+                     +   6 * tropism
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      -   30;
