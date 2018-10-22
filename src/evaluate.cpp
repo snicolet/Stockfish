e7bb060f9bbd34c3922e4c4da2a3e533baaa41b2
Stéphane Nicolet <cassio@free.fr>
2018-10-22 13:12:15 +0200

    Half effect
    Bench: 4368250

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -757,7 +757,7 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    - 48 * (blocked >= 4)
+                    - 24 * (blocked >= 4)
                     -118 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
