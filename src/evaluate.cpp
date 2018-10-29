0da0e2f8ce0a7a86e377bac1f3bbf048529cb6f5
Stéphane Nicolet <cassio@free.fr>
2018-10-29 20:22:02 +0100

    Try coordination bonus for RookOnFile
    Bench: 3335625

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -125,9 +125,12 @@ namespace {
     { S( 9, 2), S(15, 5) }  // Bishop
   };
 
-  // RookOnFile[semiopen/open] contains bonuses for each rook when there is
-  // no (friendly) pawn on the rook file.
-  constexpr Score RookOnFile[] = { S(20, 7), S(45, 20) };
+  // RookOnFile[semiopen/open][connected] contains bonuses for each rook when there is
+  // no friendly pawn on the rook file, bigger if rook is connected to another major.
+  constexpr Score RookOnFile[][2] = { 
+     { S(20,  7), S(25, 10) }, //semi open
+     { S(45, 20), S(60, 30) }  //open
+  };
 
   // ThreatByMinor/ByRook[attacked PieceType] contains bonuses according to
   // which piece type attacks which one. Attacks on lesser pieces which are
@@ -377,7 +380,8 @@ namespace {
 
             // Bonus for rook on an open or semi-open file
             if (pe->semiopen_file(Us, file_of(s)))
-                score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
+                score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))]
+                                   [bool(PseudoAttacks[ROOK][s] & pos.pieces(Us, ROOK, QUEEN))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)
