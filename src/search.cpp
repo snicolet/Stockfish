a754d85872101fec20c432b73f92c1536064c82c
Stéphane Nicolet <cassio@free.fr>
2018-12-27 17:42:04 +0100

    take 4
    Bench: 3152903

--- src/search.cpp
+++ src/search.cpp
@@ -969,7 +969,7 @@ moves_loop: // When in check, search starts from here
       {
           if (   !captureOrPromotion
               && !givesCheck
-              && !pos.advanced_pawn_push(move))
+              && (!pos.advanced_pawn_push(move) || pos.pawn_passed(move)))
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
@@ -1346,7 +1346,7 @@ moves_loop: // When in check, search starts from here
       if (   !inCheck
           && !givesCheck
           &&  futilityBase > -VALUE_KNOWN_WIN
-          && (!pos.advanced_pawn_push(move) || pos.pawn_passed(move)))
+          && !pos.advanced_pawn_push(move))
       {
           assert(type_of(move) != ENPASSANT); // Due to !pos.advanced_pawn_push
 
