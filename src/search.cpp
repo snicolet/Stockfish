afa7ca27566ab1651ba407cf1aee35f987901e43
Stéphane Nicolet <cassio@free.fr>
2018-12-27 16:44:15 +0100

    take 1 (2 bishops)
    Bench: 3295471

--- src/search.cpp
+++ src/search.cpp
@@ -964,7 +964,7 @@ moves_loop: // When in check, search starts from here
 
       // Step 14. Pruning at shallow depth (~170 Elo)
       if (  !rootNode
-          && pos.non_pawn_material(us)
+          && pos.non_pawn_material() >= 2 * BishopValueMg
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
           if (   !captureOrPromotion
