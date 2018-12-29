b521f3dfcfbbf225891040eefb73369373274fae
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2018-12-29 12:25:25 +0100

    CloseEnemies=S(10,-3)
    Bench: 3242046

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -153,7 +153,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  constexpr Score CloseEnemies       = S(  9, -3);
+  constexpr Score CloseEnemies       = S( 10, -3);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 69, 36);
   constexpr Score KingProtector      = S(  7,  8);
