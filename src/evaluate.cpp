3a82e33cddcb7fdb50d01c66caa95dd836f6f830
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-01-04 17:07:56 +0100

    Take 2
    Bench: 3512046

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -159,7 +159,7 @@ namespace {
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
-  constexpr Score LooseEnemies       = S(  6,  6);
+  constexpr Score LooseEnemies       = S(  0, 12);
   constexpr Score MinorBehindPawn    = S( 18,  3);
   constexpr Score PawnlessFlank      = S( 17, 95);
   constexpr Score RestrictedPiece    = S(  7,  7);
