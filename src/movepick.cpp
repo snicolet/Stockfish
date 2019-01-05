c5e5b85fd18681f141abadc56cbac4a4dd76456f
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-01-05 14:17:51 +0100

    favor recaptures in move ordering (300)
    Bench: 3332964

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -109,7 +109,7 @@ void MovePicker::score() {
       if (Type == CAPTURES)
           m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))] / 8
-                   + 512 * (to_sq(m) == recaptureSquare);
+                   + 300 * (to_sq(m) == recaptureSquare);
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
