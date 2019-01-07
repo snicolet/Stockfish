59f6ab431fdc33ec3b7611f26ce918c663855615
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-01-07 17:15:25 +0100

    More opposite castling
    Bench: 3051971

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -736,8 +736,8 @@ namespace {
   template<Tracing T>
   Score Evaluation<T>::initiative(Value eg) const {
 
-    int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
-                     - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
+    int outflanking =  2 * distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
+                         - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
