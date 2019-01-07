8d3a88f48d4231f2a8442f2a89f1d05be6112d8c
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-01-07 17:42:22 +0100

    Increase discover check too (190)
    Bench: 3085796

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -469,12 +469,12 @@ namespace {
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
-                 + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
+                 + 190 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  +       tropism * tropism / 4
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
-                 -   40;
+                 -   50;
 
     // Penalty when the opponent takes material by discovered check, 
     // like in the windmill combinaison
