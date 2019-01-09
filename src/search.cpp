2e0e503f4a89edc07396209537cb47a5aa94fd6e
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-01-09 15:47:22 +0100

    No movecount pruning if in the critical search tree
    Bench: 3163066

--- src/search.cpp
+++ src/search.cpp
@@ -932,7 +932,8 @@ moves_loop: // When in check, search starts from here
       givesCheck = gives_check(pos, move);
 
       moveCountPruning =   depth < 16 * ONE_PLY
-                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
+                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY]
+                        && !pvHit;
 
       // Step 13. Extensions (~70 Elo)
 
