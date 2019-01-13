1caa3976f4289ae2479446132cb798a8cb468352
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-01-13 23:12:32 +0100

    (45 - 2 * depth) * pvHit
    Bench: 3338538

--- src/search.cpp
+++ src/search.cpp
@@ -947,7 +947,7 @@ moves_loop: // When in check, search starts from here
           &&  pos.legal(move))
       {
           int d = depth / ONE_PLY;
-          int margin = 2 * d + (d < 18 ? 25 * pvHit : 0);
+          int margin = 2 * d + (45 - 2 * d) * pvHit;
 
           Value singularBeta = std::max(ttValue - margin, -VALUE_MATE);
 
