2b4421776df8a1d5319b34599741725b9f0129b6
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-01-14 13:59:04 +0100

    Revert to master

--- src/search.cpp
+++ src/search.cpp
@@ -937,7 +937,7 @@ moves_loop: // When in check, search starts from here
       // then that move is singular and should be extended. To verify this we do
       // a reduced search on all the other moves but the ttMove and if the
       // result is lower than ttValue minus a margin then we will extend the ttMove.
-      if (    depth >= 9 * ONE_PLY
+      if (    depth >= 8 * ONE_PLY
           &&  move == ttMove
           && !rootNode
           && !excludedMove // Avoid recursive singular search
