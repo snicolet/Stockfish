e51c09f20fbcdec04d205a97caba0672c2268896
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-01-15 10:56:42 +0100

    Use a bool instead of an int, coeff = 8
    Bench: 3505884

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -750,7 +750,7 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * pe->pawn_asymmetry()
-                    + 12 * majority
+                    +  8 * majority
                     + 12 * pos.count<PAWN>()
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
