b32a596359fed90c658c0c0fdff8f2cc93ccfff3
Stéphane Nicolet <cassio@free.fr>
2019-01-18 11:38:44 +0100

    Another try at fawn pawns
    Bench: 3432415

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,6 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
+  constexpr Score Fawn     = S(30, 30);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
@@ -139,6 +140,9 @@ namespace {
 
         if (doubled && !support)
             score -= Doubled;
+
+        if (relative_rank(Us, s) == RANK_6 && opposed && !lever)
+            score += Fawn;
     }
 
     return score;
