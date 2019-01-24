90c23d0b8698bb0de4720d6358dc64c96f48e0d3
Stéphane Nicolet <cassio@free.fr>
2019-01-24 10:54:06 +0100

    TrappedRook for mob <= 4
    Bench: 3928858

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -373,7 +373,7 @@ namespace {
                 score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
-            else if (mob <= 3)
+            else if (mob <= 4)
             {
                 File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
