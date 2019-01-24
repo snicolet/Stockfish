96e3ba411206dbd3f229dbe146c56dc39615b6fe
Stéphane Nicolet <cassio@free.fr>
2019-01-24 10:56:02 +0100

    TrappedRook = S(42, 4)
    Bench: 3848597

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -168,7 +168,7 @@ namespace {
   constexpr Score ThreatByPawnPush   = S( 48, 39);
   constexpr Score ThreatByRank       = S( 13,  0);
   constexpr Score ThreatBySafePawn   = S(173, 94);
-  constexpr Score TrappedRook        = S( 47,  4);
+  constexpr Score TrappedRook        = S( 42,  4);
   constexpr Score WeakQueen          = S( 49, 15);
   constexpr Score WeakUnopposedPawn  = S( 12, 23);
 
