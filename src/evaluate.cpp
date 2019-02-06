44fcb50f91faa8fc8af93f8959d18f166cdaef3c
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-02-06 14:05:35 +0100

    Use king distance for outpost value
    Bench: 3834810

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -321,7 +321,8 @@ namespace {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & ~pe->pawn_attacks_span(Them);
             if (bb & s)
-                score += Outpost[Pt == BISHOP][bool(attackedBy[Us][PAWN] & s)] * 2;
+                score += Outpost[Pt == BISHOP][bool(attackedBy[Us][PAWN] & s)]
+                         * (17 - distance(s, pos.square<KING>(Them))) / 8;
 
             else if (bb &= b & ~pos.pieces(Us))
                 score += Outpost[Pt == BISHOP][bool(attackedBy[Us][PAWN] & bb)];
