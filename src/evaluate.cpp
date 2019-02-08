17deab976491ccfa714c9fe6c09c71164c5a9de8
Stéphane Nicolet <cassio@free.fr>
2019-02-08 13:08:39 +0100

    initiative -= 9 * aging()
    Bench: 3520043

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -760,7 +760,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     -  2 * pos.rule50_count()
-                    - 11 * pos.aging()
+                    -  9 * pos.aging()
                     -121 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
