cea712bf3e85474bff4a50a343fa1b505ccdbb68
Stéphane Nicolet <cassio@free.fr>
2019-02-08 15:13:08 +0100

    With popcount of blocked pawns, take 2
    Bench: 3646841

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -761,7 +761,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    - (2 + blocked) * pos.rule50_count()
+                    - (1 + blocked) * pos.rule50_count()
                     -121 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
