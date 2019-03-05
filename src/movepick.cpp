d69c8c5e2e2478e4eadd1dbb362aca93b829211b
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-03-05 23:24:37 +0100

    Take 1bis
    Bench: 3363749

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -116,9 +116,8 @@ void MovePicker::score() {
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
                    + (*guardHistory[0])[pos.moved_piece(m)][to_sq(m)] / 2
-                   + (*guardHistory[1])[pos.moved_piece(m)][to_sq(m)] / 2
                    + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)] / 2
-                   + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)] / 2
+                   + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2;
 
