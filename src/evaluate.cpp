d54ee943e58fdbb3add9545da51aa78d0fa66d33
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-04-28 23:36:37 +0200

    take 2
    Bench: 3564158

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -756,10 +756,10 @@ namespace {
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
-                    +      advance
+                    +      advance * advance
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -120 ;
+                    -130 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
