1b491a41293e588aed228c871bec6fc9ae4d252d
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-04-29 10:21:11 +0200

    Only advance (no compensation)
    Bench: 3044994

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -756,10 +756,10 @@ namespace {
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
-                    +  2 * advance
+                    +      advance
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -113 ;
+                    -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
