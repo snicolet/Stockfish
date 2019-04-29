8ff9a460ea2ce95356432f1f80f897232b8d7c08
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-04-29 13:04:01 +0200

    take 2, and compensation (-125)
    Bench: 3570579

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -752,7 +752,7 @@ namespace {
                     + 11 * pe->pawn_asymmetry()
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -103 ;
+                    -125 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
