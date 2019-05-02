c8edce6f62db97bd23a8c2b5fd6032c7bb44a2e1
Stéphane Nicolet <cassio@free.fr>
2019-05-02 10:08:56 +0200

    negative king danger, offset = -27
    Bench: 3609740

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -473,7 +473,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -   7;
+                 -  27;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     score -= make_score(kingDanger * abs(kingDanger) / 4096, kingDanger / 16);
