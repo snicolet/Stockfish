e64a5bda2cb11997621cc81048ad080e2d99ccfd
Stéphane Nicolet <cassio@free.fr>
2019-05-03 11:12:00 +0200

    Another try
    Bench: 3552998

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -472,11 +472,11 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -   7;
+                 +   3 ;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 0)
-        score -= make_score(kingDanger * kingDanger / 4096, (kingDanger - 220) / 16);
+        score -= make_score(kingDanger * kingDanger / 4096, (kingDanger - 200) / 16);
 
     // Penalty when our king is on a pawnless flank
     if (!(pos.pieces(PAWN) & KingFlank[file_of(ksq)]))
