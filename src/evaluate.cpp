09c75044ebe4ec5214327e3d7ce12e120044de6b
Stéphane Nicolet <cassio@free.fr>
2019-05-03 11:51:05 +0200

    shuffling start = 8 plies
    Bench: 3181775

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -744,13 +744,15 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
+    int shuffling = std::max(pos.rule50_count() - 8 , 0);
+
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -  2 * pos.rule50_count()
+                    -  2 * shuffling
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
