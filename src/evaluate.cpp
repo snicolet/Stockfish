efc31dd1c9af94adf96b07e98d70cfe65e3e108a
Stéphane Nicolet <cassio@free.fr>
2019-05-05 12:53:55 +0200

    Compensation = 145
    Bench: 3529372

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -751,8 +751,8 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     + 16 * pe->pawn_asymmetry()
-                    -165 ;
-//dbg_mean_of(complexity);
+                    -145 ;
+
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
     // that the endgame score will never change sign after the bonus.
