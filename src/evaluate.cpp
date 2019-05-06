8f443a5ae383d345ca7c4b4d7be8ae3f643bfc95
Stéphane Nicolet <cassio@free.fr>
2019-05-06 11:24:43 +0200

    Only for double rooks
    Bench: 3139660

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -661,7 +661,7 @@ namespace {
                 // assign a smaller bonus if the block square isn't attacked.
                 int k = !unsafeSquares ? 20 : !(unsafeSquares & blockSq) ? 9 : 0;
 
-                k += 2 * pos.count<ROOK>(Us);
+                k += 2 * (pos.count<ROOK>(Us) == 2);
 
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
