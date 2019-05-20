5c1c97a1d3e31e566db281628774b1e4441fb01d
Stéphane Nicolet <cassio@free.fr>
2019-05-20 07:47:42 +0200

    weight = 19
    bench: 3722027

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -746,7 +746,7 @@ namespace {
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
-                    + 17 * pawnsOnBothFlanks
+                    + 19 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     -108 ;
 
