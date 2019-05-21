174e878d03a5c7d18dd0919a0a330ce736da3900
Stéphane Nicolet <cassio@free.fr>
2019-05-21 07:59:32 +0200

    Combine Vizvezdenec's and protonspring's recent ideas
    bench 3424856

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -744,11 +744,13 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    + 11 * pos.count<PAWN>()
+                    + 12 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
-                    + 49 * !pos.non_pawn_material()
                     -103 ;
+
+    complexity = std::min(complexity, 2 * abs(eg) + 50);
+    complexity += 49 * !pos.non_pawn_material();
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
