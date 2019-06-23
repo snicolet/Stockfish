96a84056d3964d59e2e4aa9967667ede84eae748
Stéphane Nicolet <cassio@free.fr>
2019-06-23 12:43:04 +0200

    relax multi-cut pruning condition : eval >= beta - 50
    bench: 3398333

--- src/search.cpp
+++ src/search.cpp
@@ -915,7 +915,7 @@ moves_loop: // When in check, search starts from here
           // search without the ttMove. So we assume this expected Cut-node is not singular,
           // that multiple moves fail high, and we can prune the whole subtree by returning
           // a soft bound.
-          else if (   eval >= beta
+          else if (   eval >= beta - 50
                    && singularBeta >= beta)
               return singularBeta;
       }
