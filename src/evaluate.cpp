a397cb19f9a5dc4c790d34c88a37bb0660492185
Stéphane Nicolet <cassio@free.fr>
2019-07-12 09:34:46 +0200

    Reachable outposts not protected by pawn
    bench 3377371

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -303,8 +303,8 @@ namespace {
         if (Pt == BISHOP || Pt == KNIGHT)
         {
             // Bonus if piece is on an outpost square or can reach one
-            bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
-            if (bb & s)
+            bb = OutpostRanks & ~pe->pawn_attacks_span(Them);
+            if ((bb & attackedBy[Us][PAWN]) & s)
                 score += Outpost * (Pt == KNIGHT ? 4 : 2);
 
             else if (bb & b & ~pos.pieces(Us))
