292a5a0787a79231d05ad5280926207f5a434a55
Stéphane Nicolet <cassio@free.fr>
2019-07-31 11:42:03 +0200

    Variants of Miguel's idea. Take 3
    bench 4268126

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -704,7 +704,7 @@ namespace {
     behind |= shift<Down+Down>(behind);
 
     int bonus =  popcount(safe)
-               + popcount(behind & safe & b & ~attackedBy[Them][ALL_PIECES]);
+               + popcount(behind & safe & ~b & ~attackedBy[Them][ALL_PIECES]);
     int weight = pos.count<ALL_PIECES>(Us) - 1;
     Score score = make_score(bonus * weight * weight / 16, 0);
 
