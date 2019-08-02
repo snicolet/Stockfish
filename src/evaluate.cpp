97ca6f541f52c70da34f6fce18fe4fc75d60ec04
Stéphane Nicolet <cassio@free.fr>
2019-08-02 16:53:43 +0200

    double initiative
    bench 4280651

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -732,6 +732,8 @@ namespace {
                     + 49 * !pos.non_pawn_material()
                     -103 ;
 
+    complexity *= 2;
+
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
     // that the endgame score will never change sign after the bonus.
