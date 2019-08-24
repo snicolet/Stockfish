31a861ed5b8d8accdf519caed3ff917b25a87ba1
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-08-25 00:21:00 +0200

    Linear formula (weight 2)
    Bench: 3636576

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -368,7 +368,7 @@ namespace {
             
             // Bonus for queen proximity with opponent king
             const Square ksq = pos.square<KING>(Them);
-            constexpr Score QueenDistance = make_score(0, 3);
+            constexpr Score QueenDistance = make_score(0, 2);
             int dist1 = distance<File>(s, ksq);
             int dist2 = distance<Rank>(s, ksq);
             score -= QueenDistance * (dist1 + dist2);
