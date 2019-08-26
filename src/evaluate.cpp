54e23078f30bd2921b8dcbc18c2c410730d343b8
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-08-26 09:50:45 +0200

    QueenInSiberia = S(1, 1)
    Bench: 4014334

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -138,7 +138,7 @@ namespace {
   constexpr Score Outpost            = S( 18,  6);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
-  constexpr Score QueenInSiberia     = S(  1,  2);
+  constexpr Score QueenInSiberia     = S(  1,  1);
   constexpr Score RestrictedPiece    = S(  7,  7);
   constexpr Score RookOnPawn         = S( 10, 32);
   constexpr Score SliderOnQueen      = S( 59, 18);
