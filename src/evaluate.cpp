90c05e6485693089481aa91eac5321aa207cd746
Stéphane Nicolet <cassio@free.fr>
2019-08-27 21:50:31 +0200

    Pressure on weak pawns
    Bench: 3865843

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -138,6 +138,7 @@ namespace {
   constexpr Score Outpost            = S( 18,  6);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
+  constexpr Score PressureOnWeakPawns= S( 13,  7);
   constexpr Score RestrictedPiece    = S(  7,  7);
   constexpr Score RookOnPawn         = S( 10, 32);
   constexpr Score SliderOnQueen      = S( 59, 18);
@@ -548,6 +549,12 @@ namespace {
     b = pos.pieces(Us, PAWN) & safe;
     b = pawn_attacks_bb<Us>(b) & nonPawnEnemies;
     score += ThreatBySafePawn * popcount(b);
+
+    // Pressure on weak pawns
+    b =   pos.pieces(Them, PAWN)
+       & ~stronglyProtected
+       & attackedBy2[Us];
+    score += PressureOnWeakPawns * popcount(b);
 
     // Find squares where our pawns can push on the next move
     b  = shift<Up>(pos.pieces(Us, PAWN)) & ~pos.pieces();
