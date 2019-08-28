240423f567291dfc419527fe652d1665ab1755f5
Stéphane Nicolet <cassio@free.fr>
2019-08-28 21:21:08 +0200

    Half bonus
    Bench: 4013031

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -555,7 +555,7 @@ namespace {
        & ~attackedBy2[Them]
        &  attackedBy2[Us];
     int x = popcount(b);
-    score += PressureOnWeakPawns * x * x;
+    score += PressureOnWeakPawns * (x * (x + 1) / 2);
 
     // Find squares where our pawns can push on the next move
     b  = shift<Up>(pos.pieces(Us, PAWN)) & ~pos.pieces();
