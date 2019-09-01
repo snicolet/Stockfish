cec3a779ab6b8cd4c68f793149ae0e8fc799bf53
Stéphane Nicolet <cassio@free.fr>
2019-09-01 13:00:20 +0200

    Odd plies: random skip extensions, proba=1/16
    bench: 3833119

--- src/search.cpp
+++ src/search.cpp
@@ -1011,6 +1011,10 @@ moves_loop: // When in check, search starts from here
                && pos.pawn_passed(us, to_sq(move)))
           extension = ONE_PLY;
 
+      if (   (ss->ply & 1) == 0
+          && (thisThread->nodes.load(std::memory_order_relaxed) & 15) == 15)
+          extension = DEPTH_ZERO;
+
       // Calculate new depth for this move
       newDepth = depth - ONE_PLY + extension;
 
@@ -1036,8 +1040,8 @@ moves_loop: // When in check, search starts from here
 
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
-                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold + 100
-                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold + 100)
+                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
+                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
