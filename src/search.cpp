05a2254e5da791c4ce6d5dc0cde40d9a17413867
Stéphane Nicolet <cassio@free.fr>
2019-09-01 13:02:22 +0200

    Odd plies: random skip extensions, proba=1/8
    Bench: 3486288

--- src/search.cpp
+++ src/search.cpp
@@ -1012,7 +1012,7 @@ moves_loop: // When in check, search starts from here
           extension = ONE_PLY;
 
       if (   (ss->ply & 1) == 0
-          && (thisThread->nodes.load(std::memory_order_relaxed) & 31) == 31)
+          && (thisThread->nodes.load(std::memory_order_relaxed) & 7) == 7)
           extension = DEPTH_ZERO;
 
       // Calculate new depth for this move
