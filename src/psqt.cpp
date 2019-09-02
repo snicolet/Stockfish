c7fc9e4db36128f701cac7640af054bc54db79c8
Stéphane Nicolet <cassio@free.fr>
2019-09-02 19:54:03 +0200

    forward (mg), double effect
    Bench: 3444480

--- src/psqt.cpp
+++ src/psqt.cpp
@@ -124,7 +124,7 @@ void init() {
                                                       : Bonus[pc][rank_of(s)][f]);
           
           
-          psq[ pc][ s] += make_score(rank_of(s), rank_of(s));
+          psq[ pc][ s] += make_score(2 * rank_of(s), 0);
           
           psq[~pc][~s] = -psq[pc][s];
       }
