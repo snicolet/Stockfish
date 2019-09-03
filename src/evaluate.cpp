ecda520a1193a5163736aed49d96a1eef38fa756
Stéphane Nicolet <cassio@free.fr>
2019-09-03 11:02:40 +0200

    Add some of the engame threats score to midgame
    Bench: 3917776

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -576,6 +576,8 @@ namespace {
         score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
     }
 
+    score += make_score(eg_value(score) / 8, 0);
+
     if (T)
         Trace::add(THREAT, Us, score);
 
