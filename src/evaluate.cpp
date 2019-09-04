c7cf22d8768f8ccf9c81c19b105c7c772f3963d6
Stéphane Nicolet <cassio@free.fr>
2019-09-04 02:56:02 +0200

    Little combinaisons, take 2: exclude lever attacks
    Bench: 3671806

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -585,7 +585,8 @@ namespace {
     targets =   pos.pieces(Them, PAWN)
 			  & attackedBy2[Us]
 			  & attackedBy2[Them]
-			  & ~attackedBy[Them][PAWN];
+			  & ~attackedBy[Them][PAWN]
+			  & ~attackedBy[Us][PAWN];
     while (targets)
     {
         Bitboard s = targets & (targets ^ (targets - 1));
@@ -597,8 +598,7 @@ namespace {
                    + (s & attackedBy[Them][QUEEN])
                    + (s & attackedBy[Them][KING]);
 
-        attack  =    (s & attackedBy[Us][PAWN])
-                   + (s & attackedBy[Us][KNIGHT]) 
+        attack  =    (s & attackedBy[Us][KNIGHT]) 
                    + (s & attackedBy[Us][BISHOP])
                    + (s & attackedBy[Us][ROOK])
                    + (s & attackedBy[Us][QUEEN])
