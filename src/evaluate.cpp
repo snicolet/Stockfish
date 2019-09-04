0bc49bf5b4a3b096d741a1dc11bcc1de9748d3ab
Stéphane Nicolet <cassio@free.fr>
2019-09-04 15:55:15 +0200

    Consider pawns blocked by any piece
    Bench: 4225679

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -128,7 +128,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  constexpr Score BlockedPawnAttack  = S( 15,  5);
+  constexpr Score BlockedPawnAttack  = S( 20,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score FlankAttacks       = S(  8,  0);
   constexpr Score Hanging            = S( 69, 36);
@@ -579,7 +579,7 @@ namespace {
 
     // Bonus for threats on blocked pawns which has only one non-pawn defender
     b =  pos.pieces(Them, PAWN)
-       & shift<Up>(pos.pieces(Us, PAWN))
+       & shift<Up>(pos.pieces(Us))
        & attackedBy[Them][ALL_PIECES]
        & attackedBy[Us][ALL_PIECES]
        & ~attackedBy2[Them]
