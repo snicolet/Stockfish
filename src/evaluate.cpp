0284e60cff6fc87c2c85d0d1345b0ecba7ba28f2
Stéphane Nicolet <cassio@free.fr>
2019-09-06 21:00:52 +0200

    Bonus=S(45,0)
    Bench: 3836544

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -138,7 +138,7 @@ namespace {
   constexpr Score Outpost            = S( 18,  6);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
-  constexpr Score PawnPressure       = S( 20, 20);
+  constexpr Score PawnPressure       = S( 45,  0);
   constexpr Score RestrictedPiece    = S(  7,  7);
   constexpr Score RookOnPawn         = S( 10, 32);
   constexpr Score SliderOnQueen      = S( 59, 18);
