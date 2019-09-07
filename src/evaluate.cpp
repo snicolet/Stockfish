b4db559a9834f797ff1b66cf47a8317848ba97a3
Stéphane Nicolet <cassio@free.fr>
2019-09-07 21:50:07 +0200

    Bonus=S(1200,1200)
    Bench: 4013411

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -139,7 +139,7 @@ namespace {
   constexpr Score Outpost            = S( 18,  6);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
-  constexpr Score PinnedQueen        = S(1000,1000);
+  constexpr Score PinnedQueen        = S(1200,1200);
   constexpr Score RestrictedPiece    = S(  7,  7);
   constexpr Score RookOnPawn         = S( 10, 32);
   constexpr Score SliderOnQueen      = S( 59, 18);
