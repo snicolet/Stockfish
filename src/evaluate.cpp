93cf6265f79811970e79b75bc446837a354e343b
Stéphane Nicolet <cassio@free.fr>
2019-09-08 13:22:07 +0200

    Asymmetry of pawn structure in initiative()
    Bench: 3684500

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -722,11 +722,12 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
+                    +  8 * pe->asymmetry()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -103 ;
+                    -123 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
