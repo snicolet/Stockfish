e5454eac9f6f594a0b23f20443c64adccb1f94d9
Stéphane Nicolet <cassio@free.fr>
2019-09-08 09:13:22 +0200

    Introduce pinners

--- src/position.h
+++ src/position.h
@@ -107,6 +107,8 @@ public:
   // Checking
   Bitboard checkers() const;
   Bitboard blockers_for_king(Color c) const;
+  Bitboard pinners(Color c) const;
+  Bitboard pinners() const;
   Bitboard check_squares(PieceType pt) const;
   bool is_discovery_check_on_king(Color c, Move m) const;
 
@@ -311,6 +313,14 @@ inline Bitboard Position::checkers() const {
 
 inline Bitboard Position::blockers_for_king(Color c) const {
   return st->blockersForKing[c];
+}
+
+inline Bitboard Position::pinners(Color c) const {
+  return st->pinners[c];
+}
+
+inline Bitboard Position::pinners() const {
+  return pinners(WHITE) | pinners(BLACK);
 }
 
 inline Bitboard Position::check_squares(PieceType pt) const {
