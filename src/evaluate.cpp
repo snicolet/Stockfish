b98602abf439cf767e085f4a53378836e125cfd1
Stéphane Nicolet <cassio@free.fr>
2019-09-09 13:38:36 +0200

    pieces reserve for king danger
    Bench: 4041205

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -459,7 +459,8 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -   7;
+                 +   4 * pos.count<ALL_PIECES>(Them)
+                 -  40;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
