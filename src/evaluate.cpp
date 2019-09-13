f3c632e68420aedd11cd7997ab773fb44451124d
Stéphane Nicolet <cassio@free.fr>
2019-09-14 01:19:47 +0200

    Immediate malus for blocked pawns (-2)
    Bench: 4258664

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -738,7 +738,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
-                    -     (6 + pos.rule50_count()) * blockedPawns
+                    -  2 * (pos.rule50_count() - 1) * blockedPawns
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
