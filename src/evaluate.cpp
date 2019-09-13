c044322614e0d7e2ced4cfcac71a2c619f822e9a
Stéphane Nicolet <cassio@free.fr>
2019-09-14 00:24:18 +0200

    delay the shuffling malus by 8
    Bench: 3942154

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -738,7 +738,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
-                    -  2 * std::max(0, pos.rule50_count() - 6) * blockedPawns
+                    -  2 * std::max(0, pos.rule50_count() - 8) * blockedPawns
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
