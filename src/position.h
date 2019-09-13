152b00b6a310f1b1e0c75bf3ab25293e9e09b8b7
Stéphane Nicolet <cassio@free.fr>
2019-09-13 23:16:33 +0200

    No aging of position in hash if no blocked pawn
    Bench: 3910400

--- src/position.h
+++ src/position.h
@@ -368,7 +368,8 @@ inline int Position::rule50_count() const {
 }
 
 inline int Position::aging() const {
-  return st->rule50 / 8;
+  bool blockedPawns = pieces(WHITE, PAWN) & shift<SOUTH>(pieces(BLACK, PAWN));
+  return blockedPawns ? st->rule50 / 8 : 0;
 }
 
 inline bool Position::opposite_bishops() const {
