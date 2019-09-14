a69aac5c494ca8880006f786ed72eca10bdcdaf0
Stéphane Nicolet <cassio@free.fr>
2019-09-14 09:08:51 +0200

    Bad bishops: increase the penalty when behind blocked pawns
    Bench: 3706466

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -319,7 +319,8 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (1 + popcount(blocked & CenterFiles));
+                                     * (1 + popcount(blocked & CenterFiles)
+                                          + popcount(blocked & forward_ranks_bb(Us, s)));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
