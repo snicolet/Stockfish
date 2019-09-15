59b86022235011eba70fc099c25ea11d3a9b31ad
Stéphane Nicolet <cassio@free.fr>
2019-09-15 14:21:29 +0200

    offset = -98
    Bench: 3960823

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -739,7 +739,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
-                    - 93 ;
+                    - 98 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
