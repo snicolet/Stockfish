c011490940ee298064d4b297a42a84346cd85f7c
Stéphane Nicolet <cassio@free.fr>
2019-09-16 17:35:42 +0200

    Tweak ocb base scale factor (15)
    Bench: 4021616

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -767,14 +767,11 @@ namespace {
     {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 16 + 4 * pe->passed_count();
+            sf = 15 + 4 * pe->passed_count();
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
-
-        Bitboard blocked = pos.pieces(WHITE, PAWN) & shift<SOUTH>(pos.pieces(BLACK, PAWN));
-        int divisor = blocked ? 2 : 4;
         
-        sf = std::max(4, sf - (pos.rule50_count() - 12) / divisor);
+        sf = std::max(4, sf - (pos.rule50_count() - 12) / 4);
     }
 
     return ScaleFactor(sf);
