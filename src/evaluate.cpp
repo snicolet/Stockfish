780ff72e71e28f3171e259b6bdea977be40d5d5e
Stéphane Nicolet <cassio@free.fr>
2019-09-16 17:48:51 +0200

    Tweak ocb base scale factor (13)
    Bench: 3654569

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -767,7 +767,7 @@ namespace {
     {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 14 + 4 * pe->passed_count();
+            sf = 13 + 4 * pe->passed_count();
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
         
