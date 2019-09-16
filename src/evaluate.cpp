6a7bbcc189727ec6e7526a2080a5f564d97f6e48
Stéphane Nicolet <cassio@free.fr>
2019-09-16 15:23:05 +0200

    Accelarated shuffle decay in case of blocked pawn - take 1
    Bench: 3758906

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -771,7 +771,10 @@ namespace {
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
-        sf = std::max(0, sf - (pos.rule50_count() - 12) / 4  );
+        Bitboard blocked = pos.pieces(WHITE, PAWN) & shift<SOUTH>(pos.pieces(BLACK, PAWN));
+        int divisor = blocked ? 2 : 4;
+        
+        sf = std::max(0, sf - (pos.rule50_count() - 12) / divisor);
     }
 
     return ScaleFactor(sf);
