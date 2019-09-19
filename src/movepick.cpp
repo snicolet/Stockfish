a2f9b79c647e40438d5682f892963b068ec9b50d
Stéphane Nicolet <cassio@free.fr>
2019-09-20 01:21:31 +0200

    Defavor quiet pawn moves
    Bench: 3554442

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -116,7 +116,7 @@ void MovePicker::score() {
                    + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2
-                   + 64 * (type_of(pos.moved_piece(m)) == PAWN);
+                   - 64 * (type_of(pos.moved_piece(m)) == PAWN);
 
       else // Type == EVASIONS
       {
