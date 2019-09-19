5e8e127f22bd86b604c50515a9007fd3b3e3a559
Stéphane Nicolet <cassio@free.fr>
2019-09-20 01:27:14 +0200

    Avoid quiet moves putting a piece en prise by a pawn (weight 64)
    Bench: 3702161

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -119,7 +119,7 @@ void MovePicker::score() {
                    + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2
-                   - 32 * !!(riskySquares & to_sq(m));
+                   - 64 * !!(riskySquares & to_sq(m));
 
       else // Type == EVASIONS
       {
