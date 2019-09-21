65e08b3fa989966a0279d3e05f5d30d692e6673b
Stéphane Nicolet <cassio@free.fr>
2019-09-21 14:10:08 +0200

    Double effect: 32

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -743,7 +743,7 @@ namespace {
                        -103 ;
 
     int mg_complexity =  std::min(eg_complexity + 50, 0)
-                       + 16 * (horizontalDistance >= 4);
+                       + 32 * (horizontalDistance >= 4);
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
