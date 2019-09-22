2daf49b7286543db9581559643521ae56f0b82b8
Stéphane Nicolet <cassio@free.fr>
2019-09-22 16:08:31 +0200

    Take 2: factor = 2
    Bench: 3771523

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -740,7 +740,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    +  4 * (artillery - 12)
+                    +  2 * (artillery - 12)
                     - 36 * almostUnwinnable
                     -103 ;
 
