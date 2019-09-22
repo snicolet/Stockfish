68f6abc47aed0a1a5a763c1ac295faf4e0b47fc5
Stéphane Nicolet <cassio@free.fr>
2019-09-22 17:53:48 +0200

    Artillery in midgame: weight 2
    Bench: 3689509

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -744,7 +744,7 @@ namespace {
                        -103 ;
 
     int mg_complexity =   std::min(eg_complexity + 50, 0)
-                        + 4 * (artillery - 12);
+                        + 2 * (artillery - 12);
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
