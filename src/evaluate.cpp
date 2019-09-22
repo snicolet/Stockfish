616fd75a7a99c05bb82ae84b04f7a33d55f4bd3f
Stéphane Nicolet <cassio@free.fr>
2019-09-22 22:03:54 +0200

    Endgame artillery: weight=4, with compensation 29
    Bench: 3498145

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -739,7 +739,7 @@ namespace {
                        + 11 * pos.count<PAWN>()
                        +  9 * outflanking
                        + 18 * pawnsOnBothFlanks
-                       + 69 * !pos.non_pawn_material()
+                       + 29 * !pos.non_pawn_material()
                        +  4 * (artillery - 12)
                        - 36 * almostUnwinnable
                        -103 ;
