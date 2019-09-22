569738efd9c9a3cd7fabe00e0de660ffee883d6f
Stéphane Nicolet <cassio@free.fr>
2019-09-22 13:44:40 +0200

    Extension for captures with check
    Bench: 4529158

--- src/search.cpp
+++ src/search.cpp
@@ -991,7 +991,9 @@ moves_loop: // When in check, search starts from here
 
       // Check extension (~2 Elo)
       else if (    givesCheck
-               && (pos.is_discovery_check_on_king(~us, move) || pos.see_ge(move, -PawnValueMg)))
+               && (   pos.is_discovery_check_on_king(~us, move)
+                   || captureOrPromotion
+                   || pos.see_ge(move, Value(-150))))
           extension = ONE_PLY;
 
       // Castling extension
