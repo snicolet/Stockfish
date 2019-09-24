b088d222e65210a27d8237fe8ce92e37ee6f4604
Stéphane Nicolet <cassio@free.fr>
2019-09-24 16:46:20 +0200

    Take 10
    Bench: 4335598

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,8 +132,8 @@ namespace {
         {
             int v =  Connected[r] * (2 + bool(phalanx) - opposed);
             
-            v += popcount(support) * 20;
-            v += 2 * (support && ((FileDBB | FileEBB) & s));
+            v += popcount(support) * 21;
+            v += (r >= RANK_4) * (support && ((FileDBB | FileEBB) & s));
 
             score += make_score(v, v * (r - 2) / 4);
         }
