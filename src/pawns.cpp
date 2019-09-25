f0933458c94bed467aeb01dad695d230c2a71d67
Stéphane Nicolet <cassio@free.fr>
2019-09-25 02:07:22 +0200

    SteadyCenter = S(0, 8)
    Bench: 3892124

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -138,7 +138,7 @@ namespace {
             score += make_score(v, v * (r - 2) / 4);
 
             bool steadyCenter = (SteadyCenterSupport & support) && (SteadyCenterSquares & s);
-            score += make_score(r , 0) * steadyCenter;
+            score += make_score(0 , 8) * steadyCenter;
         }
 
         else if (!neighbours)
