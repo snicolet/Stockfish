97092b67bed0bb0f0bd6ea63276e199f56a98df3
Stéphane Nicolet <cassio@free.fr>
2019-10-01 00:58:21 +0200

    bishopPairEndgame = 100
    Bench: 4087817

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -737,7 +737,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    + 150 * bishopPairEndgame
+                    + 100 * bishopPairEndgame
                     - 36 * almostUnwinnable
                     -103 ;
 
