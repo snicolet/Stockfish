343ee76caa15a10ede8e244a7567b1a4edd4b789
Stéphane Nicolet <cassio@free.fr>
2019-10-02 23:39:58 +0200

    Make the Outpost bonus much smaller, take 3
    Bench: 3937633

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -135,7 +135,7 @@ namespace {
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
   constexpr Score MinorBehindPawn    = S( 18,  3);
-  constexpr Score Outpost            = S(  3,  1);
+  constexpr Score Outpost            = S(  6,  2);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
   constexpr Score RestrictedPiece    = S(  7,  7);
