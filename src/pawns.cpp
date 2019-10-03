5ffb89f29c1f970d8a077ed8d6ec8f32ddb75407
Stéphane Nicolet <cassio@free.fr>
2019-10-03 20:59:52 +0200

    take 2
    Bench: 3809021

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -36,7 +36,7 @@ namespace {
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
-  constexpr Score Supported     = S(21, 21);
+  constexpr Score Supported     = S(21, 15);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
 
