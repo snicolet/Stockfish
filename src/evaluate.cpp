2c29bb7b900a6f53c1f27b69887e041f21479b2e
Stéphane Nicolet <cassio@free.fr>
2019-10-06 20:46:59 +0200

    Another try at bad bishops
    Bench: 3589881

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -315,9 +315,11 @@ namespace {
                 // Penalty according to number of pawns on the same color square as the
                 // bishop, bigger when the center files are blocked with pawns.
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
+                bool lowMob = (mob <= 2);
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (1 + popcount(blocked & CenterFiles));
+                                     * (1 + popcount(blocked & CenterFiles))
+                                     * (1 + lowMob);
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
