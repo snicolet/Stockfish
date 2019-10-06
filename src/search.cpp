fd7ee2a95568ecc11ec48bb01a6935e0800eacf1
Stéphane Nicolet <cassio@free.fr>
2019-10-06 18:19:05 +0200

    Less aggressive null-move pruning: 1/4 effect
    Bench: 4219043

--- src/search.cpp
+++ src/search.cpp
@@ -805,7 +805,7 @@ namespace {
         && (ss-1)->statScore < 22661
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 33 * depth + 299 - improving * 30 + PieceValue[MG][priorCapture] / 3
+        &&  ss->staticEval >= beta - 33 * depth + 299 - improving * 30 + PieceValue[MG][priorCapture] / 4
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
