05ffab7c196faa593cabeea21f424d38a10028de
Stéphane Nicolet <cassio@free.fr>
2019-10-07 00:40:06 +0200

    Take 3
    Bench: 4087383

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -320,7 +320,7 @@ namespace {
                                    & shift<Down>(pos.pieces(Them, PAWN));
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (1 + popcount(blocked & CenterFiles) + popcount(blocked2) / 2);
+                                     * (1 + popcount(blocked & CenterFiles) + popcount(blocked2) / 3);
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
