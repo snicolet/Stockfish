498b0a378e80788ded264b25ee30803793b53954
Stéphane Nicolet <cassio@free.fr>
2019-10-07 23:13:28 +0200

    Take 1
    Bench: 4078266

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,7 +132,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
+            int v =  Connected[r] * (2 + popcount(phalanx)) * (1 + !opposed) / 2
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
