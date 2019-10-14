ca48f4bb103a228c667aabfc887fb97a12034c30
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-10-14 06:42:23 +0200

    9 * inversion
    Bench: 4134825

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,7 +723,7 @@ namespace {
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
-                    +  5 * inversion
+                    +  9 * inversion
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
