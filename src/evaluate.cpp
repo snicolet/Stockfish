5d427d9c5d8a2b59a7a127bc618993bbb8b7d3d2
Stéphane Nicolet <cassio@free.fr>
2019-10-17 16:16:42 +0200

    Last try
    Bench: 4515202

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -718,7 +718,7 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    + 14 * pos.count<PAWN>()
+                    + 13 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
