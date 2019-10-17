5b18f752d06131d5efa58b7d1bd38c067991f7af
Stéphane Nicolet <cassio@free.fr>
2019-10-17 13:37:58 +0200

    Keep more pawns on the board
    Bench: 4148444

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -718,7 +718,7 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    + 11 * pos.count<PAWN>()
+                    + 16 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
