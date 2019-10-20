365feb17272c191dd4fa8cce6ca0225cc700023b
Stéphane Nicolet <cassio@free.fr>
2019-10-20 16:58:59 +0200

    LooseEnemies = S(0, 12)
    Bench: 4526497

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -134,7 +134,7 @@ namespace {
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
-  constexpr Score LooseEnemies       = S(  0, 25);
+  constexpr Score LooseEnemies       = S(  0, 12);
   constexpr Score MinorBehindPawn    = S( 18,  3);
   constexpr Score Outpost            = S( 32, 10);
   constexpr Score PassedFile         = S( 11,  8);
