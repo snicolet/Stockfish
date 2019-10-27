4139019c6c6802f12756f0e99a44550bfbf5809a
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-10-27 14:04:30 +0100

    Compensate the non_pawn_material() term for pure pawn endgames
    Bench: 4498391

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -720,7 +720,7 @@ namespace {
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
-                    + 49 * !pos.non_pawn_material()
+                    + 71 * !pos.non_pawn_material()
                     - 22 * !pos.pieces(ROOK)
                     - 36 * almostUnwinnable
                     -103 ;
