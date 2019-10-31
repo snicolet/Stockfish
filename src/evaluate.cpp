0fdcf3aa4f736ecb2b9ca436cc3ccb97dadbac2f
Stéphane Nicolet <cassio@free.fr>
2019-10-31 17:01:00 +0100

    Opposite: increase tropism danger with separation
    Bench: 4684039

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -448,7 +448,7 @@ namespace {
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
-                 + (14 - 2 * separation) * kingFlankAttacks * kingFlankAttacks / 32
+                 + (24 + separation) * kingFlankAttacks * kingFlankAttacks / 64
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
