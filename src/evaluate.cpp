29e89cb8cabd858942b1940b8e0a4e3884dbbbc7
Stéphane Nicolet <cassio@free.fr>
2019-11-07 09:48:25 +0100

    Fawn pawns in initiative, take 2
    Bench: 4518565

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -720,7 +720,7 @@ namespace {
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 51 * !pos.non_pawn_material()
-                    +  9 * (wr + br - 7)
+                    +  9 * (wr + br - 8)
                     - 43 * almostUnwinnable
                     - 95 ;
 
