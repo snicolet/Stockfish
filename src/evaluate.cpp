e57579c1891e0aa2efa54fc96a0c3a2adb340da8
Stéphane Nicolet <cassio@free.fr>
2019-11-07 23:14:40 +0100

    Half effect, take 3
    Bench: 4661543

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -720,7 +720,7 @@ namespace {
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 51 * !pos.non_pawn_material()
-                    +  4 * (wr + br - 6)
+                    +  4 * (wr + br - 5)
                     - 43 * almostUnwinnable
                     - 95 ;
 
