56e432226be15954a60e0b1cb855ac3d863bc190
Stéphane Nicolet <cassio@free.fr>
2019-11-08 08:12:16 +0100

    Try factor 9 for best version
    Bench: 4725058

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -720,7 +720,7 @@ namespace {
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 51 * !pos.non_pawn_material()
-                    +  8 * (wr + br - 4)
+                    +  9 * (wr + br - 4)
                     - 43 * almostUnwinnable
                     - 95 ;
 
