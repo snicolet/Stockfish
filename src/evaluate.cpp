36e6bc7bf48eb4c373fb4989754990f9e1bd5842
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-12-13 08:21:39 +0100

    Thresehold = 0.8
    Bench: 5542456

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -296,7 +296,7 @@ namespace {
 
             th->badBishopAverage = (1.0 - rho) * th->badBishopAverage + rho * (mob2 <= 3);
 
-            if (th->badBishopAverage > 0.75)
+            if (th->badBishopAverage > 0.80)
                 score -= make_score(0, 40);
         }
 
