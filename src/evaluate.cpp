a8d05bafe35e158074ca1be18f9464e3086f4f24
Stéphane Nicolet <cassio@free.fr>
2020-01-20 20:30:18 +0100

    Take 3bis: 69 * kingFlankChains, offset = 37
    Bench: 4617241

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,7 +457,7 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 +  35;
+                 +  37;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
