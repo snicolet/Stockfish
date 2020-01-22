41de62679dd19822b7b15764fd6f856e4785fc20
Stéphane Nicolet <cassio@free.fr>
2020-01-22 03:06:46 +0100

    take 4
    Bench: 4710979

--- src/search.cpp
+++ src/search.cpp
@@ -1158,9 +1158,9 @@ moves_loop: // When in check, search starts from here
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
               // hence break make_move(). (~2 Elo)
-              else if (    type_of(move) == NORMAL
-                       && !inCheck
-                       && !pos.see_ge(reverse_move(move)))
+              if (    type_of(move) == NORMAL
+                  && !inCheck
+                  && !pos.see_ge(reverse_move(move)))
                   r -= 2;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
