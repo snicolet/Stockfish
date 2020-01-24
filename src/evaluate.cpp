55d7002e9bf518ff5da4707d764a53f0ad5ca56a
Stéphane Nicolet <cassio@free.fr>
2020-01-24 03:42:29 +0100

    Piece reserve
    Bench: 5002005

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -450,11 +450,12 @@ namespace {
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
+                 +   2 * pos.count<ALL_PIECES>(Them)
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 +  37;
+                 +  17;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
