2831775c41dd32fd910216158bb441a3e2d796e5
Stéphane Nicolet <cassio@free.fr>
2020-01-24 14:37:24 +0100

    Mixed approach: both boolean and integer bonus, same offset as master
    Bench: 4947955

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,7 +723,7 @@ namespace {
                     +      pawnsOnBothFlanks / 2
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    - 110 ;
+                    - 100 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
