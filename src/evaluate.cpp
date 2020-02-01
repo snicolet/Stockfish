32613ba526950d30a50642c2e967616a97572b1f
Stéphane Nicolet <cassio@free.fr>
2020-02-01 01:01:00 +0100

    Penalty for having loose piece(s): S(20,20)
    Bench: 4931280

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -134,6 +134,7 @@ namespace {
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
+  constexpr Score LoosePieces        = S( 20, 20);
   constexpr Score MinorBehindPawn    = S( 18,  3);
   constexpr Score Outpost            = S( 30, 21);
   constexpr Score PassedFile         = S( 11,  8);
@@ -526,19 +527,11 @@ namespace {
        &  attackedBy[Us][ALL_PIECES];
     score += RestrictedPiece * popcount(b);
 
-constexpr Score OverProtection = make_score(5,5);
-constexpr Score EverythingIsProtected = make_score(20,20);
-
-    // Avoid over-protection
-   // b =   (pos.pieces(Us) ^ pos.pieces(Us, KING, QUEEN))
-     //   & attackedBy2[Us];
-    //score -= OverProtection * popcount(b);
-    
-    // Avoid hanging pieces
+    // Avoid loose pieces
     b =   (pos.pieces(Us) ^ pos.pieces(Us, KING, QUEEN))
        & ~attackedBy[Us][ALL_PIECES];
     if (b)
-        score -= EverythingIsProtected;
+        score -= LoosePieces;
 
     // Protected or unattacked squares
     safe = ~attackedBy[Them][ALL_PIECES] | attackedBy[Us][ALL_PIECES];
