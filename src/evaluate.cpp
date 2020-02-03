fe1ee7be6b33b60ebc5fc69ab2307d896540a196
Stéphane Nicolet <cassio@free.fr>
2020-02-03 01:04:15 +0100

    LoosePieces = S(10, 0)
    Bench: 4971900

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -134,6 +134,7 @@ namespace {
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
+  constexpr Score LoosePieces        = S( 10,  0);
   constexpr Score MinorBehindPawn    = S( 18,  3);
   constexpr Score Outpost            = S( 30, 21);
   constexpr Score PassedFile         = S( 11,  8);
@@ -525,6 +526,12 @@ namespace {
        & ~stronglyProtected
        &  attackedBy[Us][ALL_PIECES];
     score += RestrictedPiece * popcount(b);
+
+    // Avoid loose pieces
+    b =   (pos.pieces(Us) ^ pos.pieces(Us, KING, QUEEN))
+       & ~attackedBy[Us][ALL_PIECES];
+    if (b)
+        score -= LoosePieces;
 
     // Protected or unattacked squares
     safe = ~attackedBy[Them][ALL_PIECES] | attackedBy[Us][ALL_PIECES];
