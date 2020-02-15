c541d28e9d0096c4e3261b466ccd489f4a765c7c
Stéphane Nicolet <cassio@free.fr>
2020-02-15 22:43:01 +0100

    Queen imbalance => scale factor down
    Bench: 4955089

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -753,6 +753,10 @@ namespace {
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
+        if (   pos.count<QUEEN>() == 1
+            && pos.rule50_count() > 0)
+            sf -= 10;
+
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
 
