cf55fcbca29b7223fc4243968b5c5eaf4b5e1098
Stéphane Nicolet <cassio@free.fr>
2020-02-16 19:02:23 +0100

    Take 3
    bench 4868765

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -754,8 +754,9 @@ namespace {
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
         if (   pos.count<QUEEN>() == 1
-            && pos.rule50_count() > 0)
-            sf -= 5;
+            && pos.count<QUEEN>(strongSide) == 1
+            && pos.rule50_count() >= 3 )
+            sf -= 15;
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
