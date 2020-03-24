cc28955dfe1a49928056946c38768def82eabe35
Stéphane Nicolet <cassio@free.fr>
2020-03-24 14:46:59 +0100

    Take 3
    Bench: 4640384

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -127,7 +127,7 @@ namespace {
   };
 
   // Assorted bonuses and penalties
-  constexpr Score BishopCoordination  = S(  4,  0);
+  constexpr Score BishopCoordination  = S(  5,  5);
   constexpr Score BishopPawns         = S(  3,  7);
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
