ab9639a382ea807846d1dbb29b10620c197784af
Stéphane Nicolet <cassio@free.fr>
2020-04-04 21:50:02 +0200

    mg threats +3/16
    Bench: 4651494

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -564,7 +564,7 @@ namespace {
         score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
     }
 
-    score += make_score(mg_value(score) / 8, 0);
+    score += make_score(3 * mg_value(score) / 16, 0);
 
     if (T)
         Trace::add(THREAT, Us, score);
