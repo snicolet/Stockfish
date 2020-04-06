da91a67c826251638f65b4fa131b346b64fce8c9
Stéphane Nicolet <cassio@free.fr>
2020-04-06 06:09:14 +0200

    Malus for no king mobility = S(20, 50)
    Bench: 5046783

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -475,7 +475,7 @@ namespace {
 
     // Malus when our king has very low mobility
     if (!mob)
-        score -= make_score(40, 100);
+        score -= make_score(20, 50);
 
     // Penalty when our king is on a pawnless flank
     if (!(pos.pieces(PAWN) & KingFlank[file_of(ksq)]))
