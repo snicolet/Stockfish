85bcf08ae6842c306021c390ca616ef495cbf8f9
Stéphane Nicolet <cassio@free.fr>
2020-04-12 19:28:05 +0200

    22 * support
    Bench: 4460109

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -135,7 +135,7 @@ namespace {
         if (support | phalanx)
         {
             int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
-                   + 20 * popcount(support);
+                   + 22 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
         }
