84f1bcb34ba88a8843ec747c37929aecf279166d
Stéphane Nicolet <cassio@free.fr>
2020-04-13 05:04:19 +0200

    After Stefan: increase opposed weight, support=22
    Bench: 4253827

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -134,8 +134,8 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
-                   + 21 * popcount(support);
+            int v =  Connected[r] * (4 + 2 * bool(phalanx) - 3 * bool(opposed)) / 2
+                   + 22 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
         }
