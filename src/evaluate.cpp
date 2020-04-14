8b3704e4e8620d66231dad9e1f94d27886625ec8
Stéphane Nicolet <cassio@free.fr>
2020-04-15 00:28:38 +0200

    Smaller effect
    Bench: 4683202

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -132,7 +132,7 @@ namespace {
   constexpr Score FlankAttacks        = S(  8,  0);
   constexpr Score Hanging             = S( 69, 36);
   constexpr Score KingProtector       = S(  7,  8);
-  constexpr Score KnightOnQueen       = S( 26, 12);
+  constexpr Score KnightOnQueen       = S( 21, 12);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);
   constexpr Score Outpost             = S( 30, 21);
@@ -140,7 +140,7 @@ namespace {
   constexpr Score PawnlessFlank       = S( 17, 95);
   constexpr Score RestrictedPiece     = S(  7,  7);
   constexpr Score RookOnQueenFile     = S(  7,  6);
-  constexpr Score SliderOnQueen       = S( 69, 18);
+  constexpr Score SliderOnQueen       = S( 64, 18);
   constexpr Score ThreatByKing        = S( 24, 89);
   constexpr Score ThreatByPawnPush    = S( 48, 39);
   constexpr Score ThreatBySafePawn    = S(173, 94);
