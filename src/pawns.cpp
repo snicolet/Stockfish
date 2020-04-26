0e6f3ad715e51053aa0b010d9c8eea9343f2b62d
Stéphane Nicolet <cassio@free.fr>
2020-04-26 19:41:24 +0200

    More connected bonus for passed pawns, with support=22 .
    Bench: 4763776

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -138,8 +138,8 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked)) / 2
-                   + 23 * popcount(support);
+            int v =  Connected[r] * (4 + 2 * bool(phalanx) + passed - 2 * bool(opposed) - bool(blocked)) / 2
+                   + 22 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
         }
