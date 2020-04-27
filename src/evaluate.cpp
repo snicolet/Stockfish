7678dd69960a00bdee53bba50df8364c1785dbb6
Stéphane Nicolet <cassio@free.fr>
2020-04-27 20:35:56 +0200

    Clean spaces

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -463,7 +463,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
                  +  37;
-        
+
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
