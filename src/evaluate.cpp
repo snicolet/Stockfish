882f543e5ca047e162eb095890f4efbc0ad9c219
Stéphane Nicolet <cassio@free.fr>
2020-05-07 00:35:15 +0200

    Damp down more aggressively
    Bench: 4631312

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -860,7 +860,7 @@ namespace {
 
     // Damp down the eval after 25 moves of shuffling
     if (pos.rule50_count() > 50)
-        v = v * (3 * (100 - pos.rule50_count()) + 106) / 256;
+        v = v * std::max(6, -44 + 6 * (100 - pos.rule50_count())) / 256;
 
     return v;
   }
