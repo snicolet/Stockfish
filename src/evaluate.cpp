958f57825fc199b5657d99612f6122178454655b
Stéphane Nicolet <cassio@free.fr>
2020-05-06 11:58:12 +0200

    Remove the use of pos.rule50_count() in scale factor (compensate more)
    Bench: 4558074

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -768,12 +768,12 @@ namespace {
         {
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = 20 + 4 * popcount(pe->passed_pawns(strongSide));
+                sf = 21 + 4 * popcount(pe->passed_pawns(strongSide));
             else
-                sf = 24 + 3 * pos.count<ALL_PIECES>(strongSide);
+                sf = 25 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
         else
-            sf = std::min(sf, 38 + 7 * pos.count<PAWN>(strongSide));
+            sf = std::min(sf, 39 + 7 * pos.count<PAWN>(strongSide));
     }
 
     return ScaleFactor(sf);
