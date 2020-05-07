8eeb6aa135dca0c4de35cf6a18dd4c7aa12f7fba
Stéphane Nicolet <cassio@free.fr>
2020-05-07 11:28:50 +0200

    delay use of rule50_count() in initiative (rule50_count() > 5)
    Bench: 4815771

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -739,7 +739,7 @@ namespace {
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    -  2 * pos.rule50_count()
+                    -  2 * pos.rule50_count() * (pos.rule50_count() > 5)
                     -110 ;
 
     Value mg = mg_value(score);
