04ea72a24c31de1cc88d2ecd0fdc0660892d140b
Stéphane Nicolet <cassio@free.fr>
2020-05-07 14:00:42 +0200

    initiative offset = -130
    Bench: 4821709

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -740,7 +740,7 @@ namespace {
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
                     -  2 * pos.rule50_count()
-                    -120 ;
+                    -130 ;
 
     Value mg = mg_value(score);
     Value eg = eg_value(score);
