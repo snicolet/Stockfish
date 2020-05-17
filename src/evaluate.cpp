1c1604355e78223ec123cc31df83a74431be89d2
Stéphane Nicolet <cassio@free.fr>
2020-05-17 16:51:02 +0200

    Compensate by lowering the usual mobility: take 2
    Bench: 4716833

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -832,7 +832,7 @@ namespace {
 
     // Stochastic mobility, see http://www.dcs.bbk.ac.uk/~mark/download/ply.pdf
     int random_eval = ((pos.key() + pos.this_thread()->nodes) & 31) - 15;
-    score += make_score(random_eval, 0);
+    score += make_score(random_eval, random_eval);
 
     // More complex interactions that require fully populated attack bitboards
     score +=  king<   WHITE>() - king<   BLACK>()
