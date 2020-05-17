ebbc2877b90f04a20e174efe52bd46b904582ffb
Stéphane Nicolet <cassio@free.fr>
2020-05-17 22:57:53 +0200

    half effect bis
    Bench: 4734851

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -732,7 +732,7 @@ namespace {
                      || rank_of(pos.square<KING>(BLACK)) < RANK_5;
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   9 * pe->passed_count()
+    int complexity =  10 * pe->passed_count()
                     + 12 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
