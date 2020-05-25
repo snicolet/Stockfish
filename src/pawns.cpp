dac1bbe7c138c87cceee716f47f9ee8eb4fed505
Stéphane Nicolet <cassio@free.fr>
2020-05-25 13:11:20 +0200

    Combine with an increase of Doubled penalty
    Bench: 4671278

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -32,9 +32,9 @@ namespace {
   #define S(mg, eg) make_score(mg, eg)
 
   // Pawn penalties
-  constexpr Score Backward      = S( 14, 29);
+  constexpr Score Backward      = S( 9, 24);
   constexpr Score BlockedStorm  = S(82, 82);
-  constexpr Score Doubled       = S(11, 56);
+  constexpr Score Doubled       = S(16, 61);
   constexpr Score Isolated      = S( 5, 15);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
