342c07bc7ae1770f9d2c1b431f75b44a8f598ccf
Stéphane Nicolet <cassio@free.fr>
2020-05-26 22:46:14 +0200

    Take 4bis
    Bench: 4810302

--- src/types.h
+++ src/types.h
@@ -183,7 +183,7 @@ enum Value : int {
 
   PawnValueMg   = 124,   PawnValueEg   = 206,
   KnightValueMg = 771,   KnightValueEg = 854,
-  BishopValueMg = 825,   BishopValueEg = 925,
+  BishopValueMg = 815,   BishopValueEg = 915,
   RookValueMg   = 1276,  RookValueEg   = 1380,
   QueenValueMg  = 2538,  QueenValueEg  = 2682,
   Tempo = 28,
