07734b3b628d854225a370fe655cab163043e537
Stéphane Nicolet <cassio@free.fr>
2020-05-28 13:45:52 +0200

    Take 3quarto (untested on fishtest)
    Bench: 4623118

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -741,7 +741,7 @@ namespace {
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
                     -  2 * pos.rule50_count()
-                    + (-134);
+                    + (-146);
 
 //dbg_mean_of(complexity);
 //dbg_mean_of(2 * pe->passed_count()+  8 * pe->asymmetry());
