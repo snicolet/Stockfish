830ac7d916f84ab205d006024b067d988a7d0b6d
Stéphane Nicolet <cassio@free.fr>
2020-06-03 06:44:39 +0200

    Horizontal rook on king ring bonus
    Bench: 4731430

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -290,7 +290,7 @@ namespace {
             kingAttacksCount[Us] += popcount(b & attackedBy[Them][KING]);
         }
 
-        else if (Pt == ROOK && (file_bb(s) & kingRing[Them]))
+        else if (Pt == ROOK && (attacks_bb<ROOK>(s) & kingRing[Them]))
             score += RookOnKingRing;
 
         else if (Pt == BISHOP && (attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & kingRing[Them]))
