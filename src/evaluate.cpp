94f16ddc1ee34e8c2c8cdc55fa7df7c3447e28ca
Stéphane Nicolet <cassio@free.fr>
2020-06-04 07:34:14 +0200

    Bonus = S(10,10)
    Bench: 4444815

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -143,7 +143,7 @@ namespace {
   constexpr Score ReachableOutpost    = S( 31, 22);
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);
-  constexpr Score PawnSafePush        = S( 10,  5);
+  constexpr Score PawnSafePush        = S( 10, 10);
   constexpr Score RestrictedPiece     = S(  7,  7);
   constexpr Score RookOnKingRing      = S( 16,  0);
   constexpr Score RookOnQueenFile     = S(  5,  9);
