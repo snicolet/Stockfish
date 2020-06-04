0acaed420bd74a2d38837d9ecd84a7972a7c4b00
Stéphane Nicolet <cassio@free.fr>
2020-06-04 11:33:21 +0200

    Tweak safe pawn push, if the push is pawn supported
    Bench: 4560154

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -557,7 +557,8 @@ namespace {
     b |= shift<Up>(b & TRank3BB) & ~pos.pieces();
 
     // Keep only the squares which are relatively safe
-    b &= ~attackedBy[Them][PAWN] & safe;
+    b &= safe & 
+         (~attackedBy[Them][PAWN] | attackedBy[Us][PAWN]);
 
     // Bonus for safe pawn threats on the next move
     b = pawn_attacks_bb<Us>(b) & nonPawnEnemies;
