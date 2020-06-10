c7f7de70acb37829edae6c8e38171e94bf4b372e
Stéphane Nicolet <cassio@free.fr>
2020-06-10 16:22:18 +0200

    Start hash table aging after 25 moves of shuffling
    Bench: 4918732

--- src/position.h
+++ src/position.h
@@ -348,8 +348,8 @@ inline int Position::rule50_count() const {
 }
 
 inline int Position::aging() const {
-  return st->rule50 <= 24 ? 0
-                          : (1 + (st->rule50 - 24) / 16) << 16;
+  return st->rule50 <= 50 ? 0
+                          : (1 + (st->rule50 - 50) / 16) << 16;
 }
 
 inline bool Position::opposite_bishops() const {
