6ffa6208abde5a9ce26acdcaab8d46229955ac0c
Stéphane Nicolet <cassio@free.fr>
2020-06-11 09:49:24 +0200

    Take 2
    Bench: 4575034

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -140,7 +140,7 @@ namespace {
             int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked)) / 2;
             score += make_score(v, v * (r - 2) / 4);
 
-            score += make_score(21, 21) * popcount(support);
+            score += make_score(21, 12) * popcount(support);
         }
 
         else if (!neighbours)
