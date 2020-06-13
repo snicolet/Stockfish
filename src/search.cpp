1916f15782ee37a1ebdc1106af61054d0aacb063
Stéphane Nicolet <cassio@free.fr>
2020-06-13 22:17:45 +0200

    Take 4
    Bench: 4424381

--- src/search.cpp
+++ src/search.cpp
@@ -986,7 +986,7 @@ moves_loop: // When in check, search starts from here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, depth) + 2 * ((ss->ply+1) & 1);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth) + 3 * ((ss->ply+1) & 1);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
