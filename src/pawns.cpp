7f737413a648ec47eb35102bc3c8d1155aecd838
Stéphane Nicolet <cassio@free.fr>
2020-06-17 14:34:35 +0200

    Combine with MJZ's "Reduce passed pawn path bonus at MG"
    Bench: 4718421

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -137,11 +137,10 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked)) / 2;
+            int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked)) / 2
+                   + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
-
-            score += make_score(21, 12) * popcount(support);
         }
 
         else if (!neighbours)
