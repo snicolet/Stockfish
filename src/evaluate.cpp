f13bf4ab1b6b3c2c424e76155819c4df903e3460
Stéphane Nicolet <cassio@free.fr>
2020-06-17 14:39:50 +0200

    Only change ranks 5 and 6
    Bench: 4530868

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -662,7 +662,7 @@ namespace {
                 if ((pos.pieces(Us) & bb) || (attackedBy[Us][ALL_PIECES] & blockSq))
                     k += 5;
 
-                bonus += make_score(k * w * 13 / 16, k * w);
+                bonus += make_score(k * w, k * w);
             }
         } // r > RANK_3
 
