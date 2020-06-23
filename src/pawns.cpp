d6c6c327cb8bfb2b2b6a10c6dcf664b5a667fbc5
Stéphane Nicolet <cassio@free.fr>
2020-06-23 11:20:46 +0200

    French bind bonus for e5 + (h5|h6) : bonus=S(15,15)
    Bench: 4830464

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -34,7 +34,7 @@ namespace {
   // Pawn penalties
   constexpr Score Backward      = S( 9, 24);
   constexpr Score Doubled       = S(11, 56);
-  constexpr Score FrenchBind    = S(10, 10);
+  constexpr Score FrenchBind    = S(15, 15);
   constexpr Score Isolated      = S( 5, 15);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
