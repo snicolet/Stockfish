baf4fe228da24a9676d6cc2af7c1d9b7a9f3d987
Stéphane Nicolet <cassio@free.fr>
2020-06-28 05:34:24 +0200

    Take 2
    Bench: 4350938

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -780,7 +780,7 @@ namespace {
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
                 sf = 18 + 4 * popcount(pe->passed_pawns(strongSide));
             else
-                sf = std::min(58, 22 + 4 * pos.count<ALL_PIECES>(strongSide));
+                sf = std::min(61, 22 + 4 * pos.count<ALL_PIECES>(strongSide));
         }
         else
             sf = std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide));
