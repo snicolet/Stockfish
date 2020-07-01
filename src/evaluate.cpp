727172d383e7ec53eb3a0b9697398618664ea206
Stéphane Nicolet <cassio@free.fr>
2020-07-01 23:29:38 +0200

    Remove one template parameter
    No functional change

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -176,7 +176,7 @@ namespace {
     template<Color Us> Score threats() const;
     template<Color Us> Score passed() const;
     template<Color Us> Score space() const;
-    template<Color Us, PieceType Pt> int analyse_check(Square ksq, Bitboard checks, Bitboard interpositions) const;
+    template<PieceType Pt> int analyse_check(Square ksq, Bitboard checks, Bitboard interpositions) const;
     Value winnable(Score score) const;
 
     const Position& pos;
@@ -391,7 +391,7 @@ namespace {
     return score;
   }
 
-  template<Tracing T> template<Color Us, PieceType Pt>
+  template<Tracing T> template<PieceType Pt>
   int Evaluation<T>::analyse_check(Square ksq, Bitboard checks, Bitboard interpositions) const
   {
      if (more_than_one(checks))
@@ -438,10 +438,10 @@ namespace {
     // Enemy rooks checks
     rookChecks = b1 & safe & attackedBy[Them][ROOK];
     if (rookChecks)
-        kingDanger += analyse_check<Us, ROOK>(ksq, rookChecks, interpositions &
-                                                                ( attackedBy[Us][KNIGHT]
-                                                                | attackedBy[Us][BISHOP]
-                                                                | attackedBy[Us][ROOK]));
+        kingDanger += analyse_check<ROOK>(ksq, rookChecks, interpositions &
+                                                           ( attackedBy[Us][KNIGHT]
+                                                           | attackedBy[Us][BISHOP]
+                                                           | attackedBy[Us][ROOK]));
     else
         unsafeChecks |= b1 & attackedBy[Them][ROOK];
 
@@ -453,11 +453,11 @@ namespace {
                  & ~attackedBy[Us][QUEEN]
                  & ~rookChecks;
     if (queenChecks)
-        kingDanger += analyse_check<Us, QUEEN>(ksq, queenChecks, interpositions &
-                                                                 ( attackedBy[Us][KNIGHT]
-                                                                 | attackedBy[Us][BISHOP]
-                                                                 | attackedBy[Us][ROOK]
-                                                                 | attackedBy[Us][QUEEN]));
+        kingDanger += analyse_check<QUEEN>(ksq, queenChecks, interpositions &
+                                                             ( attackedBy[Us][KNIGHT]
+                                                             | attackedBy[Us][BISHOP]
+                                                             | attackedBy[Us][ROOK]
+                                                             | attackedBy[Us][QUEEN]));
 
     // Enemy bishops checks: we count them only if they are from squares from
     // which we can't give a queen check, because queen checks are more valuable.
@@ -466,16 +466,16 @@ namespace {
                   & safe
                   & ~queenChecks;
     if (bishopChecks)
-        kingDanger += analyse_check<Us, BISHOP>(ksq, bishopChecks, interpositions &
-                                                                   ( attackedBy[Us][KNIGHT]
-                                                                   | attackedBy[Us][BISHOP]));
+        kingDanger += analyse_check<BISHOP>(ksq, bishopChecks, interpositions &
+                                                               ( attackedBy[Us][KNIGHT]
+                                                               | attackedBy[Us][BISHOP]));
     else
         unsafeChecks |= b2 & attackedBy[Them][BISHOP];
 
     // Enemy knights checks
     knightChecks = attacks_bb<KNIGHT>(ksq) & attackedBy[Them][KNIGHT];
     if (knightChecks & safe)
-        kingDanger += analyse_check<Us, KNIGHT>(ksq, knightChecks & safe, 0);
+        kingDanger += analyse_check<KNIGHT>(ksq, knightChecks & safe, 0);
     else
         unsafeChecks |= knightChecks;
 
