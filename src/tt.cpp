3dd8e838203173f5ad779d30ba5e62f67b93a611
Stéphane Nicolet <cassio@free.fr>
2020-07-06 13:18:20 +0200

    Every 3 plies after 18 plies
    Bench: 4511291

--- src/tt.cpp
+++ src/tt.cpp
@@ -128,7 +128,7 @@ TTEntry* TranspositionTable::probe(Position& pos, const Key key, bool& found) co
           int rule50 = pos.rule50_count();
           
           found =    (bool)tte[i].key16
-                  && (rule50 <= 15 || (rule50 % 9 != 0))
+                  && (rule50 <= 15 || (rule50 % 3 != 0))
                   ;
 
           return &tte[i];
