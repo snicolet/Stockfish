be44a38cb1047a748888661acdec9ace0801fdd3
Stéphane Nicolet <Stephane.Nicolet@u-paris2.fr>
2020-07-09 14:31:53 +0200

    With PvNode condition, take 1
    Bench: 5004043

--- src/search.cpp
+++ src/search.cpp
@@ -1111,7 +1111,8 @@ moves_loop: // When in check, search starts from here
 
       // Force extension when shuffling (to reach the damping regime faster)
       if (   depth <= 2
-          && move == ttMove
+          && PvNode 
+          && (move == ttMove || !ttMove)
           && pos.rule50_count() > 15
           && pos.rule50_count() < 50)
           extension = 1;
