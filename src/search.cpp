77c4f41af9db866a9e9e1ea2d9868fbd3c868b5d
Stéphane Nicolet <Stephane.Nicolet@u-paris2.fr>
2020-07-09 13:54:09 +0200

    With (move == ttMove) condition
    Bench: 4686053

--- src/search.cpp
+++ src/search.cpp
@@ -1111,7 +1111,7 @@ moves_loop: // When in check, search starts from here
 
       // Force extension when shuffling (to reach the damping regime faster)
       if (   depth <= 2
-          && (move == ttMove || !ttMove)
+          && move == ttMove
           && pos.rule50_count() > 15
           && pos.rule50_count() < 50)
           extension = 1;
