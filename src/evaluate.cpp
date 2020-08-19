0718797937b627016fead70f936e39b628b7d15e
Stéphane Nicolet <cassio@free.fr>
2020-08-19 11:40:13 +0200

    Try to simplify away the hybrid stuff
    Bench: 3762751

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -939,13 +939,10 @@ make_v:
 
 Value Eval::evaluate(const Position& pos) {
 
-  bool classical = !Eval::useNNUE
-                ||  abs(eg_value(pos.psq_score())) * 16 > NNUEThreshold1 * (16 + pos.rule50_count());
+  bool classical = !Eval::useNNUE;
+  
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
-
-  if (classical && Eval::useNNUE && abs(v) * 16 < NNUEThreshold2 * (16 + pos.rule50_count()))
-      v = NNUE::evaluate(pos) * 5 / 4 + Tempo;
 
   // Damp down the evaluation linearly when shuffling
   v = v * (100 - pos.rule50_count()) / 100;
