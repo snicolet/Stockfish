949b4e624e2a735ea0c91eea7fe63700935150c5
Stéphane Nicolet <cassio@free.fr>
2020-09-08 10:36:29 +0200

    Detect fortresses if the classical eval was called after the random trick
    Bench: 3664300

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1022,7 +1022,7 @@ Value Eval::evaluate(const Position& pos) {
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
 
-  if (   useClassical 
+  if (   classical 
       && Eval::useNNUE 
       && abs(v) * 16 < NNUEThreshold2 * (16 + pos.rule50_count()))
       v = NNUE::evaluate(pos) * 5 / 4 + Tempo;
