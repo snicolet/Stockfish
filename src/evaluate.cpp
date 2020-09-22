6480a0c6fc15d3b50f17f474f15e9db9d46f7f64
Stéphane Nicolet <cassio@free.fr>
2020-09-23 01:25:07 +0200

    Quadratic damping
    Bench: 4436687

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1039,7 +1039,7 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (100 - pos.rule50_count()) / 100;
+  v = v * (100 - pos.rule50_count()) * (100 - pos.rule50_count()) / 10000;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
