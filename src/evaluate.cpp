6dd420c8f5ca4aa4dd5a21796c68b69aaa2ea16f
Stéphane Nicolet <cassio@free.fr>
2020-09-26 10:24:33 +0200

    Simplify OCB hybrid patch via lower scale factor in classical (-8)
    Bench: 4064537

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -896,9 +896,9 @@ namespace {
         {
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = 18 + 4 * popcount(pe->passed_pawns(strongSide));
+                sf = 10 + 4 * popcount(pe->passed_pawns(strongSide));
             else
-                sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);
+                sf = 14 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
         else if (  pos.non_pawn_material(WHITE) == RookValueMg
                 && pos.non_pawn_material(BLACK) == RookValueMg
@@ -1032,14 +1032,9 @@ Value Eval::evaluate(const Position& pos) {
 
       v = classical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
 
-      // If the classical eval is small and imbalance large, use NNUE nevertheless.
-      // For the case of opposite colored bishops, switch to NNUE eval with
-      // small probability if the classical eval is less than the threshold.
+      // If the classical eval is small and imbalance large, use NNUE nevertheless
       if (   largePsq
-          && (abs(v) * 16 < NNUEThreshold2 * r50
-          || (   pos.opposite_bishops() 
-              && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
-              && !(pos.this_thread()->nodes & 0xB))))
+          && abs(v) * 16 < NNUEThreshold2 * r50)
           v = adjusted_NNUE();
   }
 
