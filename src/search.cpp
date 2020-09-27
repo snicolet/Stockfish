c298e866f9e837b7b6b36f43414e2f01fa0d7c5d
Stéphane Nicolet <cassio@free.fr>
2020-09-27 21:37:03 +0200

    Disable pruning at PV nodes
    Bench: 4901893

--- src/search.cpp
+++ src/search.cpp
@@ -1008,9 +1008,7 @@ moves_loop: // When in check, search starts from here
       newDepth = depth - 1;
 
       // Step 13. Pruning at shallow depth (~200 Elo)
-      if (   !rootNode
-          && !ss->inCheck 
-          && !PvNode
+      if (   !PvNode
           && pos.non_pawn_material(us)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
