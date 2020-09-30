d3255ce47a27800b6bbec1afc1e02d6c3200cdab
Stéphane Nicolet <cassio@free.fr>
2020-09-30 23:26:44 +0200

    Scale nnue randomly to favor wider search (16 * random
    Bench: 3795670

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1025,7 +1025,7 @@ Value Eval::evaluate(const Position& pos) {
       auto  adjusted_NNUE = [&](){
          int mat = pos.non_pawn_material() + PieceValue[MG][PAWN] * pos.count<PAWN>();
          int random = (pos.this_thread()->nodes & 1) + (pos.key() & 1) - 1;
-         return NNUE::evaluate(pos) * (720 + 4 * random + mat / 32) / 1024 + Tempo;
+         return NNUE::evaluate(pos) * (720 + 16 * random + mat / 32) / 1024 + Tempo;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
