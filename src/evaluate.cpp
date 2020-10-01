26a17a659493115376e5bcf625c0adf766c7aed8
Stéphane Nicolet <cassio@free.fr>
2020-10-01 15:46:31 +0200

    Take 6
    Bench: 3894235

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1024,7 +1024,7 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
          int mat = pos.non_pawn_material() + PieceValue[MG][PAWN] * pos.count<PAWN>();
-         int random = (pos.this_thread()->nodes + pos.key()) & 31;
+         int random = (pos.this_thread()->nodes & 15) + (pos.key() & 15) - 15;
 
          return NNUE::evaluate(pos) * (720 + random + mat / 32) / 1024 + Tempo;
       };
