4adc4c67f17458b32cfcc86c47b92fca63433ed7
Stéphane Nicolet <cassio@free.fr>
2020-10-03 13:43:27 +0200

    Try the best outflank version with nn-5d5e1d50b514.nnue
    Bench: 3720163

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1027,7 +1027,7 @@ Value Eval::evaluate(const Position& pos) {
          int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                           - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
-         return NNUE::evaluate(pos) * (860 + 16 * outflanking + mat / 32) / 1024 + Tempo;
+         return NNUE::evaluate(pos) * (820 + 16 * outflanking + mat / 32) / 1024 + Tempo;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
