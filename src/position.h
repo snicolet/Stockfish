0e6eb0395500fd10839c5180d6d9d4ea28b52d8a
Stéphane Nicolet <cassio@free.fr>
2020-10-06 12:27:09 +0200

    Revert to take 3
    Bench: 3831835

--- src/position.h
+++ src/position.h
@@ -329,8 +329,8 @@ inline int Position::pawns_on_same_color_squares(Color c, Square s) const {
 }
 
 inline Key Position::key() const {
-  return st->rule50 < 8 ? st->key
-                        : st->key ^ make_key((st->rule50 - 8) / 8);
+  return st->rule50 < 16 ? st->key
+                         : st->key ^ make_key((st->rule50 - 16) / 8);
 }
 
 inline Key Position::pawn_key() const {
