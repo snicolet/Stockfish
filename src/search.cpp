498f4fbcf98eba18d58dfef299cea9c22a9329d1
Stéphane Nicolet <cassio@free.fr>
2020-11-06 19:24:07 +0100

    Use (bestValue > TB_LOSS) in qsearch() : take 2
    Bench: 3837267

--- src/search.cpp
+++ src/search.cpp
@@ -1523,7 +1523,6 @@ moves_loop: // When in check, search starts from here
       // Futility pruning
       if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
           && !givesCheck
-          &&  futilityBase > -VALUE_KNOWN_WIN
           && !pos.advanced_pawn_push(move))
       {
           assert(type_of(move) != ENPASSANT); // Due to !pos.advanced_pawn_push
