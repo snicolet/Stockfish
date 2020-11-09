0776b04c06c9f4bf8ebd66ae114d7fa0b6f7018e
Stéphane Nicolet <cassio@free.fr>
2020-11-09 10:34:17 +0100

    More weight for material, take 1
    Bench: 3579178

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1026,10 +1026,8 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
          int mat =  pos.non_pawn_material()
-                  + BishopValueMg * pos.count<BISHOP>()
-                  + KnightValueMg * pos.count<KNIGHT>()
                   + PieceValue[MG][PAWN] * pos.count<PAWN>();
-         return NNUE::evaluate(pos) * (720 + mat / 32) / 1024 + Tempo;
+         return NNUE::evaluate(pos) * (720 + mat / 16) / 1024 + Tempo;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
