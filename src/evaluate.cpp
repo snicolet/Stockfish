e60864ce2bf398b35cdb6f39fa6c7904af4e865b
Stéphane Nicolet <cassio@free.fr>
2021-01-22 23:53:47 +0100

    Keep even more pawns (3*pawns, offset 611)
    Bench: 4075531

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1055,7 +1055,7 @@ Value Eval::evaluate(const Position& pos) {
       
       
          int mat = pos.non_pawn_material() + 3 * PawnValueMg * pos.count<PAWN>();
-         int sc = 601 + mat / 32;
+         int sc = 611 + mat / 32;
          
          // dbg_mean_of(sc);
          
