6013ef72a39e22cee48e326bd8f1f4adc0a6b633
Stéphane Nicolet <cassio@free.fr>
2021-01-24 15:48:39 +0100

    frequency = 6
    Bench: 4218315

--- src/position.h
+++ src/position.h
@@ -323,7 +323,7 @@ inline int Position::pawns_on_same_color_squares(Color c, Square s) const {
 
 inline Key Position::key() const {
   return st->rule50 < 14 ? st->key
-                         : st->key ^ make_key((st->rule50 - 14) / 7);
+                         : st->key ^ make_key((st->rule50 - 14) / 6);
 }
 
 inline Key Position::pawn_key() const {
