b495ff87a245d2e3f986c1cd4ff062bc1a644d86
Stéphane Nicolet <cassio@free.fr>
2021-01-31 23:19:29 +0100

    Score quiets += 512 * relative_rank()
    Bench: 3859451

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -110,7 +110,7 @@ void MovePicker::score() {
                    +      (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +      (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
                    +      (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0)
-                   + 64 * relative_rank(pos.side_to_move(), to_sq(m));
+                   + 512 * relative_rank(pos.side_to_move(), to_sq(m));
 
       else // Type == EVASIONS
       {
