d7950cbf74352a781e85aa8e4cac1f92fd048d1d
Stéphane Nicolet <cassio@free.fr>
2021-01-31 23:02:12 +0100

    Score quiets += 64 * relative_rank()
    Bench: 4318603

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -104,12 +104,13 @@ void MovePicker::score() {
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if (Type == QUIETS)
-          m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
-                   + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   + (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0);
+          m.value =       (*mainHistory)[pos.side_to_move()][from_to(m)]
+                   +  2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
+                   +      (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
+                   +      (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
+                   +      (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
+                   +      (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0)
+                   + 64 * relative_rank(pos.side_to_move(), to_sq(m));
 
       else // Type == EVASIONS
       {
