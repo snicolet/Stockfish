d3b8fba54aa2ae8e0769c3036969f57661c3ecaf
Stéphane Nicolet <cassio@free.fr>
2021-02-01 22:54:24 +0100

    king separation = 64
    Bench: 4333064

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1053,8 +1053,14 @@ Value Eval::evaluate(const Position& pos) {
   {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
-         int mat = pos.non_pawn_material() + 2 * PawnValueMg * pos.count<PAWN>();
-         return NNUE::evaluate(pos) * (641 + mat / 32 - 4 * pos.rule50_count()) / 1024 + Tempo;
+         int material = pos.non_pawn_material() + 2 * PawnValueMg * pos.count<PAWN>();
+         int king_separation =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
+         int scale =  641 
+                    + material / 32 
+                    - 4 * pos.rule50_count()
+                    + 64 * (king_separation >= 4);
+         
+         return NNUE::evaluate(pos) * scale / 1024 + Tempo;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
