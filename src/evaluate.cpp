d27c339ab8af59546dbdf8885d244f6a18754e5f
Stéphane Nicolet <cassio@free.fr>
2021-02-03 00:32:58 +0100

    Bonus for king separation fires later (64)
    Bench: 4096642

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1060,7 +1060,7 @@ Value Eval::evaluate(const Position& pos) {
          scale =  641
                 + material / 32
                 - 4 * pos.rule50_count()
-                + 32 * separation;
+                + 64 * (separation >= 5);
 
          return NNUE::evaluate(pos) * scale / 1024 + Tempo;
       };
