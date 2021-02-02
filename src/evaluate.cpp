d309eb7dddbb242deef2dacd68463ecf1adb755c
Stéphane Nicolet <cassio@free.fr>
2021-02-02 10:15:30 +0100

    Linear bonus
    Bench: 3980519

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1058,7 +1058,7 @@ Value Eval::evaluate(const Position& pos) {
          int scale =  641 
                     + material / 32 
                     - 4 * pos.rule50_count()
-                    + 64 * (king_separation >= 3);
+                    + 16 * king_separation;
          
          return NNUE::evaluate(pos) * scale / 1024 + Tempo;
       };
