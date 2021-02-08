e0575c60e53b4e1bb2936322758d01b92852cb8d
Stéphane Nicolet <cassio@free.fr>
2021-02-08 13:38:45 +0100

    Asymmetric matrix: [[10,5],[10,10]]
    Bench: 4057674

--- src/search.cpp
+++ src/search.cpp
@@ -91,8 +91,8 @@ namespace {
 
   // PruningSafety[rootColor][cut type] : pruning safety table
   const int PruningSafety[2][2] = {
-     { 3 , 6 },    // ~rootColor : alpha, beta
-     { 3 , 3 }     //  rootColor : alpha, beta
+     { 10 , 5 },     // ~rootColor : alpha, beta
+     { 10 , 10 }     //  rootColor : alpha, beta
   };
   enum CutType { ALPHA, BETA };
   template <CutType T> 
