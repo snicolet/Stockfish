a4c4c1c5f35f0ea9e48e6fbb0a9484fb00591e45
Stéphane Nicolet <cassio@free.fr>
2021-02-09 00:30:12 +0100

    take 5
    Bench: 4464773

--- src/search.cpp
+++ src/search.cpp
@@ -91,8 +91,8 @@ namespace {
 
   // PruningSafety[rootColor][cut type] : pruning safety table
   const int PruningSafety[2][2] = {
-     {  20  ,  10 },    // ~rootColor : alpha, beta
-     {  10  ,  20 }     //  rootColor : alpha, beta
+     {  10  ,   0 },    // ~rootColor : alpha, beta
+     {   0  ,  10 }     //  rootColor : alpha, beta
   };
   enum CutType { ALPHA, BETA };
   template <CutType T> 
