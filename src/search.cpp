f9e8213c28beea4db7922ee0955ed391d2d3c3b9
Stéphane Nicolet <cassio@free.fr>
2021-02-14 00:23:41 +0100

    Double reduction when distanceFromPv > 50: take 2
    Bench: 3872146

--- src/search.cpp
+++ src/search.cpp
@@ -1205,8 +1205,8 @@ moves_loop: // When in check, search starts from here
               r += 2;
 
           // Less reductions if we are close to the PV
-          // if ((ss+1)->distanceFromPv < 5)
-          //    r--;
+          if ((ss+1)->distanceFromPv < 5)
+              r--;
 
           // Decrease reduction if opponent's move count is high (~5 Elo)
           if ((ss-1)->moveCount > 13)
