96ab675692a5758aa1d81583d84c300eb8aa1456
Stéphane Nicolet <cassio@free.fr>
2021-02-16 22:56:15 +0100

    Keep more pawns
    Bench: 3580249

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1053,8 +1053,10 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
          int mat = pos.non_pawn_material() + 2 * PawnValueMg * pos.count<PAWN>();
+         int scale = 651 + mat / 32 - 8 * pos.rule50_count();
+         //dbg_mean_of(scale);
 
-         return NNUE::evaluate(pos) * (641 + mat * mat / 524288 - 4 * pos.rule50_count()) / 1024 + Tempo;
+         return NNUE::evaluate(pos) * scale / 1024 + Tempo;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
