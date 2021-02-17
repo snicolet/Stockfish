5de570e41826e5e8c5a22803257a79f9c5ce3538
Stéphane Nicolet <cassio@free.fr>
2021-02-17 08:34:29 +0100

    Keep more pawns: take 5
    Bench: 3668229

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1053,7 +1053,11 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
          int material = pos.non_pawn_material() + 5 * PawnValueMg * pos.count<PAWN>();
-         int scale = 580 + material / 32 - 4 * pos.rule50_count();
+
+         //int scale = 580 + material / 32 - 4 * pos.rule50_count();
+         int scale = 580 + material * material / 524288 - 4 * pos.rule50_count();
+
+         //dbg_mean_of(scale);
 
          return NNUE::evaluate(pos) * scale / 1024 + Tempo;
       };
