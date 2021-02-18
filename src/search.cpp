49055cd2a894bfdf081b1b4751406e354fe9b136
Stéphane Nicolet <cassio@free.fr>
2021-02-18 12:21:38 +0100

    Variant of safe version: only test if abs(beta) <= 29508
    Bench: 3608938

--- src/search.cpp
+++ src/search.cpp
@@ -980,7 +980,7 @@ moves_loop: // When in check, search starts from here
          && (tte->bound() & BOUND_LOWER)
          && tte->depth() >= depth - 3
          && ttValue >= probCutBeta
-         && abs(beta) <= VALUE_KNOWN_WIN)
+         && abs(beta) <= VALUE_TB_WIN_IN_MAX_PLY - 1000)
         return probCutBeta;
 
     const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory,
