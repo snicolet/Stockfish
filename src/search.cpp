bd3c8e8f9573f3f60e3c3fc32ff0467ea33de38e
Stéphane Nicolet <cassio@free.fr>
2021-02-18 13:04:51 +0100

    Replace pruning by depth reduction when depth >= 4
    Bench: 4221653

--- src/search.cpp
+++ src/search.cpp
@@ -971,7 +971,7 @@ namespace {
     }
 
     // Step 10. Depth tweaks
-    
+
     // If the position is not in TT, decrease depth by 2
     if (   PvNode
         && depth >= 6
@@ -981,7 +981,7 @@ namespace {
     // If the above heuristics have discovered a good reason to prune the node:
     // 1. prune the node when depth is low (hard pruning)
     // 2. continue with half the depth when depth is large (soft pruning)
-    if (halfPruning && depth < 6)
+    if (halfPruning && depth < 4)
         return halfPruningValue;
     if (halfPruning)
         depth = depth / 2;
