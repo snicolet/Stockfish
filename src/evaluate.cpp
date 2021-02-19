c045d68b8b019368e6dd4021070edc4b2e8fcac9
Stéphane Nicolet <cassio@free.fr>
2021-02-19 20:39:30 +0100

    8 * pawns
    Bench: 3842172

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1054,7 +1054,7 @@ Value Eval::evaluate(const Position& pos) {
       auto  adjusted_NNUE = [&](){
 
          //int mat = pos.non_pawn_material() + 2 * PawnValueMg * pos.count<PAWN>();
-         int mat = 11 * PawnValueMg * pos.count<PAWN>();
+         int mat = 8 * PawnValueMg * pos.count<PAWN>();
          int scale = 641 + mat / 32 - 4 * pos.rule50_count();
 
          //dbg_mean_of(scale);
