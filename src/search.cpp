70f747eb37889d52817b38de7f9b4190506e7052
Stéphane Nicolet <cassio@free.fr>
2021-02-19 11:52:24 +0100

    ply > 7
    Bench: 3877999

--- src/search.cpp
+++ src/search.cpp
@@ -1202,7 +1202,7 @@ moves_loop: // When in check, search starts from here
 
           // Less reductions when close to the PV
           if (   (ss+1)->distanceFromPv < 5
-              && (ss+1)->ply <= 7)
+              && (ss+1)->ply > 7)
           {
               // dbg_mean_of((ss+1)->ply);
               r--;
