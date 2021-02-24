8299aec512ab83cad0350e632f2ecd3e4ac92389
Stéphane Nicolet <cassio@free.fr>
2021-02-24 13:42:44 +0100

    Fixed weight: 16 * attack
    Bench: 4207103

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1063,7 +1063,7 @@ Value Eval::evaluate(const Position& pos) {
 
          //attack *= (material / 512 - 22);    // positive during opening, negative during endgame 
          //attack = material * attack / 256;
-         attack = 64 * attack;
+         attack = 16 * attack;
 
          // if (attack) 
          //     dbg_mean_of(abs(attack));
