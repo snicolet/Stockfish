3c851f98e1fc6eead8f84d7917f6978b73e081cf
Stéphane Nicolet <cassio@free.fr>
2021-02-24 09:03:13 +0100

    Like take 1, half effect
    Bench: 3872685

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1058,11 +1058,13 @@ Value Eval::evaluate(const Position& pos) {
          Color us = pos.side_to_move();
          Value nnue = NNUE::evaluate(pos);
          int material = pos.non_pawn_material() + 2 * PawnValueMg * pos.count<PAWN>();
-         int attack =   popcount(pos.pieces( us) & Camp[~us])
-                      - popcount(pos.pieces(~us) & Camp[ us]);
+        // int attack =   popcount(pos.pieces( us) & Camp[~us])
+        //              - popcount(pos.pieces(~us) & Camp[ us]);
+         int attack =   popcount(pos.pieces( us) & Camp[~us] & KingFlank[file_of(pos.square<KING>(~us))])
+                      - popcount(pos.pieces(~us) & Camp[ us] & KingFlank[file_of(pos.square<KING>( us))]);
          
-         //attack *= (material / 512 - 22);    // positive during opening, negative during endgame 
-         attack = material * attack / 512;
+         attack *= (material / 1024 - 11);    // positive during opening, negative during endgame 
+         //attack = material * attack / 512;
          
          // if (attack)
          //   dbg_mean_of(abs(attack));
@@ -1071,6 +1073,8 @@ Value Eval::evaluate(const Position& pos) {
                     + attack * ((nnue > 0) - (nnue < 0))
                     + material / 32
                     - 4 * pos.rule50_count();
+         
+         //dbg_mean_of(scale);
          
          return nnue * scale / 1024 + Tempo;
       };
