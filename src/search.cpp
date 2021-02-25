935ae5ee0557b86cc26bced08bdde49d01bfc3c9
Stéphane Nicolet <cassio@free.fr>
2021-02-25 11:31:12 +0100

    Revert to best version
    Bench: 4019687

--- src/search.cpp
+++ src/search.cpp
@@ -1180,7 +1180,7 @@ moves_loop: // When in check, search starts from here
 
       // Step 16. Reduced depth search (LMR, ~200 Elo). If the move fails high it will be
       // re-searched at full depth.
-      if (    depth >= 4
+      if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
@@ -1217,6 +1217,8 @@ moves_loop: // When in check, search starts from here
               r--;
 
           // Less reductions when close to the PV
+          if ((ss+1)->distanceFromPv < 2)
+              r--;
           if ((ss+1)->distanceFromPv < 5)
               r--;
 
