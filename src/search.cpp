ee4a5ee13ad4e08be07bf0d59db85a4775878862
Stéphane Nicolet <cassio@free.fr>
2021-03-10 12:39:48 +0100

    Less aggressive movecount pruning in qsearch
    Bench: 4988008

--- src/search.cpp
+++ src/search.cpp
@@ -1582,7 +1582,7 @@ moves_loop: // When in check, search starts from here
           && !pos.advanced_pawn_push(move))
       {
 
-          if (moveCount > 2)
+          if (moveCount > 3)
               continue;
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
