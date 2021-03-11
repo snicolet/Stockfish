452e246d06793c4ffb39224208c6edabb9cef247
Stéphane Nicolet <cassio@free.fr>
2021-03-11 21:20:41 +0100

    Revert to take 4
    Bench: 4793678

--- src/search.cpp
+++ src/search.cpp
@@ -1589,7 +1589,7 @@ moves_loop: // When in check, search starts from here
           && !pos.advanced_pawn_push(move))
       {
 
-          if (moveCount > 1)
+          if (moveCount > 2)
           {
               if (picked->value < 4000)
                  continue;
