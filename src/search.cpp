3730128143adaa8e01ee622d8b7a72e3fccd5f51
Stéphane Nicolet <cassio@free.fr>
2021-03-11 19:55:00 +0100

    Take 3
    Bench: 4841907

--- src/search.cpp
+++ src/search.cpp
@@ -1586,12 +1586,14 @@ moves_loop: // When in check, search starts from here
       if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
           && !givesCheck
           &&  futilityBase > -VALUE_KNOWN_WIN
-          && !pos.advanced_pawn_push(move)
-          && picked->value < 0)
+          && !pos.advanced_pawn_push(move))
       {
 
           if (moveCount > 2)
-              continue;
+          {
+              if (picked->value < -4000)
+                 continue;
+          }
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
 
