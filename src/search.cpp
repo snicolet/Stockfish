dd925359e07ff12a96b4b469f454b5a9907ed258
Stéphane Nicolet <cassio@free.fr>
2021-03-13 12:16:26 +0100

    Take 8, with policy < 4000
    Bench: 5132322

--- src/search.cpp
+++ src/search.cpp
@@ -1249,7 +1249,7 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   {
-                      if (picked->policy < 6000)
+                      if (picked->policy < 4000)
                           r++;
                   }
           }
