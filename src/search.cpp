39d02a6cd7bf6c61ed35dfb76658c25fbc61e625
Stéphane Nicolet <cassio@free.fr>
2021-03-19 07:00:58 +0100

    Increase reduction if abs(distanceFromPv - 20) <= 1
    Bench: 4603101

--- src/search.cpp
+++ src/search.cpp
@@ -1216,6 +1216,10 @@ moves_loop: // When in check, search starts from here
           if (moveCountPruning && !formerPv)
               r++;
 
+          // Increase reduction for moves which are far from the PV
+          if (abs((ss+1)->distanceFromPv - 20) <= 1)
+              r++;
+
           // Decrease reduction if opponent's move count is high (~5 Elo)
           if ((ss-1)->moveCount > 13)
               r--;
