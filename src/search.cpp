88cc1a64176913885cb456090715ca60201bb435
Stéphane Nicolet <cassio@free.fr>
2021-03-21 09:41:31 +0100

    Do a full sort when distanceFromPV <= 2
    Bench: 5192015

--- src/search.cpp
+++ src/search.cpp
@@ -1002,7 +1002,7 @@ moves_loop: // When in check, search starts from here
                                       countermove,
                                       ss->killers,
                                       ss->ply,
-                                      ss->distanceFromPv <= 3);
+                                      ss->distanceFromPv <= 2);
 
     value = bestValue;
     singularQuietLMR = moveCountPruning = false;
