469d97dcdc86db88fdecbfc3a09747c2a704dd77
Stéphane Nicolet <cassio@free.fr>
2021-03-21 20:22:26 +0100

    Let almostPvNode= (distanceFromPv<=1) for probcut
    Bench: 4670747

--- src/search.cpp
+++ src/search.cpp
@@ -619,7 +619,7 @@ namespace {
     bestValue = -VALUE_INFINITE;
     maxValue = VALUE_INFINITE;
     ss->distanceFromPv = (PvNode ? 0 : ss->distanceFromPv);
-    almostPvNode = ss->distanceFromPv <= 2;
+    almostPvNode = ss->distanceFromPv <= 1;
 
     // Check for the available remaining time
     if (thisThread == Threads.main())
@@ -895,7 +895,7 @@ namespace {
     // Step 9. ProbCut (~10 Elo)
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    if (   !PvNode
+    if (   !almostPvNode
         &&  depth > 4
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
