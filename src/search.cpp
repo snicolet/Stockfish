0191a6c76397edea80d0388c92de92b8dcba4d5a
Stéphane Nicolet <cassio@free.fr>
2021-03-21 13:43:35 +0100

    Replace PvNode by almostPvNode. Take 4, IID
    Bench: 4799804

--- src/search.cpp
+++ src/search.cpp
@@ -895,7 +895,7 @@ namespace {
     // Step 9. ProbCut (~10 Elo)
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    if (   !almostPvNode
+    if (   !PvNode
         &&  depth > 4
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
@@ -966,7 +966,7 @@ namespace {
     }
 
     // Step 10. If the position is not in TT, decrease depth by 2
-    if (   PvNode
+    if (   almostPvNode
         && depth >= 6
         && !ttMove)
         depth -= 2;
