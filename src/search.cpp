c7e9e049a40940e794168148318698491b0073a5
Stéphane Nicolet <cassio@free.fr>
2021-03-21 09:38:46 +0100

    Do a full sort when distanceFromPV <= 4
    Bench: 4412236

--- src/search.cpp
+++ src/search.cpp
@@ -1002,7 +1002,7 @@ moves_loop: // When in check, search starts from here
                                       countermove,
                                       ss->killers,
                                       ss->ply,
-                                      PvNode);
+                                      ss->distanceFromPv <= 4);
 
     value = bestValue;
     singularQuietLMR = moveCountPruning = false;
