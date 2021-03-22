78e41e489e909831debf636559f67f1b4a486d4c
Stéphane Nicolet <cassio@free.fr>
2021-03-22 13:07:47 +0100

    Revert to best version
    Bench: 4670747

--- src/search.cpp
+++ src/search.cpp
@@ -894,7 +894,7 @@ namespace {
     // Step 9. ProbCut (~10 Elo)
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    if (   ss->distanceFromPv >= 4
+    if (   ss->distanceFromPv >= 2
         && depth > 4
         && abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
