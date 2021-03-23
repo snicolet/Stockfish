51d069d389ef74de32469f6622b552ea66ad834e
Stéphane Nicolet <cassio@free.fr>
2021-03-23 22:52:11 +0100

    Less aggressive skipQuiets: !PvNode
    Bench: 4343270

--- src/search.cpp
+++ src/search.cpp
@@ -1061,7 +1061,8 @@ moves_loop: // When in check, search starts from here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          moveCountPruning =   !PvNode
+                            && moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
