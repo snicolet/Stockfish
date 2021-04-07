444670b4de8f70de6425e7153ade9e82b3431160
Stéphane Nicolet <cassio@free.fr>
2021-04-07 13:28:27 +0200

    abs(eval) > 30
    Bench: 4499924

--- src/search.cpp
+++ src/search.cpp
@@ -1230,8 +1230,8 @@ moves_loop: // When in check, search starts from here
           // Increase reduction for lines with many shuffling moves
           if (   pos.rule50_count() > 4
               && pos.non_pawn_material() >= RookValueEg
-              && depth <= 5
-              && abs(ss->staticEval) > 50)
+              && depth <= 6
+              && abs(ss->staticEval) > 30)
               r++;
 
           if (captureOrPromotion)
