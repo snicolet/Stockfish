0bbe2e7b3c5de19649daf0ae5417ab1d8aa14b51
Stéphane Nicolet <cassio@free.fr>
2021-04-08 15:32:42 +0200

    Take 2 (664)
    Bench: 4394480

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1094,7 +1094,7 @@ Value Eval::evaluate(const Position& pos) {
          int material = pos.non_pawn_material() + 4 * PawnValueMg * pos.count<PAWN>();
          int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                           - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
-         int scale =  716
+         int scale =  664
                     + material / 32
                     + 16 * outflanking
                     -  4 * pos.rule50_count();
