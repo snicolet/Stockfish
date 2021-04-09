ef3d8b4b381d386d007e13b2533033c2d0c71f69
Stéphane Nicolet <cassio@free.fr>
2021-04-09 21:54:18 +0200

    Half effect, offset=615
    Bench: 4545074

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1094,7 +1094,7 @@ Value Eval::evaluate(const Position& pos) {
          int material = pos.non_pawn_material() + 4 * PawnValueMg * pos.count<PAWN>();
          int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                           - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
-         int scale =  635
+         int scale =  615
                     + material / 32
                     + 8 * outflanking
                     - 4 * pos.rule50_count();
