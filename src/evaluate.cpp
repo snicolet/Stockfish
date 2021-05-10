e980f63b10ec09d04135f9c0e7177bef23c185f8
Stéphane Nicolet <cassio@free.fr>
2021-05-10 08:54:11 +0200

    Take 2
    Bench: 3213548

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1105,7 +1105,8 @@ Value Eval::evaluate(const Position& pos) {
       {
          int scale =   970 
                      + 32 * pos.non_pawn_material() / 1024
-                     + 17 * pos.count<PAWN>();
+                     + 17 * pos.count<PAWN>()
+                     -  4 * pos.rule50_count();
 
          Value nnue = NNUE::evaluate(pos) * scale / 1024 + Time.tempoNNUE;
 
