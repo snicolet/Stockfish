9108f9caaae10ce84c2cc07a630c27717e5973c2
Stéphane Nicolet <cassio@free.fr>
2021-05-12 19:42:50 +0200

    Distord the (queen, pawns) balance to (30, 8)
    Bench: 3930091

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1113,9 +1113,9 @@ Value Eval::evaluate(const Position& pos) {
          Score c        = (pos.side_to_move() == WHITE ? pos.this_thread()->contempt : -pos.this_thread()->contempt);
          Value contempt = mg_value(c) / 2;
 
-         int scale =  970
+         int scale =  1070
                      + 30 * material / 1024
-                     + 22 * pawns
+                     +  8 * pawns
                      -  4 * pos.rule50_count();
 
          scale = scale * bucketWeight[bucket] / 128;
