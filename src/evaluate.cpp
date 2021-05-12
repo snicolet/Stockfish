39f457f6d3764455547aa54964c29d6979f4ea97
Stéphane Nicolet <cassio@free.fr>
2021-05-12 10:51:09 +0200

    take 4
    Bench: 3616856

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1114,7 +1114,7 @@ Value Eval::evaluate(const Position& pos) {
          int scale =  970
                      + 32 * material / 1024
                      + 17 * pawns
-                     -  4 * pos.rule50_count();
+                     -  3 * pos.rule50_count();
 
          scale = scale * bucketWeight[bucket] / 128;
 
