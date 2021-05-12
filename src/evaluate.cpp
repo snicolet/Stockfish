e500bb18ff5c3253c5f67a48874c0f93718c7b6e
Stéphane Nicolet <cassio@free.fr>
2021-05-12 15:53:00 +0200

    Tempo = 0
    Bench: 3717198

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1120,7 +1120,7 @@ Value Eval::evaluate(const Position& pos) {
 
          scale = scale * bucketWeight[bucket] / 128;
 
-         nnue = (nnue - contempt) * scale / 1024 + Time.tempoNNUE / 4;
+         nnue = (nnue - contempt) * scale / 1024;
 
          if (pos.is_chess960())
              nnue += fix_FRC(pos);
