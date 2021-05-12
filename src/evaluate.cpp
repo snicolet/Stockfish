fd4c0ab87013d6ffc600d53f758a8599260cc542
Stéphane Nicolet <cassio@free.fr>
2021-05-12 12:48:27 +0200

    take 3: -contempt/8 (flat)
    Bench: 4091101

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1111,9 +1111,7 @@ Value Eval::evaluate(const Position& pos) {
          int   pawns    = pos.count<PAWN>();
          int   bucket   = (popcount(pos.pieces()) - 1) / 4;
          Score c        = (pos.side_to_move() == WHITE ? pos.this_thread()->contempt : -pos.this_thread()->contempt);
-         Value contempt = mg_value(c) / 4;
-         
-         // dbg_mean_of(abs(contempt));
+         Value contempt = mg_value(c) / 8;
 
          int scale =  970
                      + 32 * material / 1024
