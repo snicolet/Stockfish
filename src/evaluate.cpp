46c02110202d920372cd6e9bb83beeda2e0c32b3
Stéphane Nicolet <cassio@free.fr>
2021-05-17 20:25:16 +0200

    master with pure nnue (scale = 1024)
    Bench: 4871206

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1116,10 +1116,7 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         int material = pos.non_pawn_material() + 4 * PawnValueMg * pos.count<PAWN>();
-         int scale =  580
-                    + material / 32
-                    - 4 * pos.rule50_count();
+         int scale = 1024;
 
          Value nnue = NNUE::evaluate(pos) * scale / 1024 + Time.tempoNNUE;
 
