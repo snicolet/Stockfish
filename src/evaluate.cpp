4828dd5cc9e48693b9d9a3b7d6160c37e1b4f191
Stéphane Nicolet <cassio@free.fr>
2021-05-17 11:29:06 +0200

    Apply Tomasz's fix for saturation
    Bench: 4457237

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1114,12 +1114,12 @@ make_v:
 // TUNE(SetRange(-30, 30), A1, A2, A3);
 
 
-int A0 = 0;
-int A1 = 0;
-int A2 = 0;
-int A3 = 0;
-int B0 = 0;
-int B1 = 0;
+long A0 = 0;
+long A1 = 0;
+long A2 = 0;
+long A3 = 0;
+long B0 = 0;
+long B1 = 0;
 
 /// evaluate() is the evaluator for the outer world. It returns a static
 /// evaluation of the position from the point of view of the side to move.
@@ -1135,31 +1135,34 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         Value nnue   = NNUE::evaluate(pos);
+         long nnue   = NNUE::evaluate(pos);
 
-         int material = pos.non_pawn_material();
-         int pawns    = pos.count<PAWN>();
-         int pieces   = pos.count<ALL_PIECES>();
+         long material = pos.non_pawn_material();
+         long pawns    = pos.count<PAWN>();
+         long pieces   = pos.count<ALL_PIECES>();
          
 
-         int scale1 = (970 + A0)
+         long scale1 = (970 + A0)
                      + (32 + A1) * material / 1024
                      + (17 + A2) * pawns
                      - (14 + A3) * pos.rule50_count();
+        
+         scale1 = std::clamp(scale1, long(100), long(5000));
                      
-         int scale2 = (900 + B0)
+         long scale2 = (900 + B0)
                      + (10 + B1) * pieces;
 
 
          // dbg_mean_of(scale1);
          // dbg_mean_of(scale2);
 
-         nnue = nnue * (scale1 * scale2 / 1024) / 1024;
+         //nnue = nnue * (scale1 * scale2 / 1024) / 1024;
+         nnue = (nnue * scale1 * scale2) / (1024 * 1024);
 
          if (pos.is_chess960())
              nnue += fix_FRC(pos);
 
-         return nnue;
+         return Value(int(nnue));
       };
 
       // If there is PSQ imbalance we use the classical eval. We also introduce
