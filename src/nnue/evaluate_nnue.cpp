161d3031467647e94f5cc53582a65cba49172ad2
Stéphane Nicolet <cassio@free.fr>
2021-05-20 21:39:08 +0200

    (materialist, positional) = (123, 133)
    Bench: 3647976

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -168,8 +168,8 @@ namespace Stockfish::Eval::NNUE {
       int materialist = psqt;
       int positional  = output[0];
 
-      const int A = 124;
-      const int B = 132;
+      const int A = 123;
+      const int B = 133;
 
       int sum = (A * materialist + B * positional) / 128;
 
