3c536b9ca8dee8e1f90bdd49fffa407299d6b701
Stéphane Nicolet <cassio@free.fr>
2021-05-20 07:59:35 +0200

    general
    Bench: 4365242

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1132,7 +1132,7 @@ Value Eval::evaluate(const Position& pos) {
       Value psq = Value(abs(eg_value(pos.psq_score())));
       int   r50 = 16 + pos.rule50_count();
       bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
-      bool  classical = largePsq;
+      bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
 
 
       // Use classical evaluation for really low piece endgames.
@@ -1150,7 +1150,6 @@ Value Eval::evaluate(const Position& pos) {
           && !lowPieceEndgame
           && (   abs(v) * 16 < NNUEThreshold2 * r50
               || (   pos.opposite_bishops()
-                  && !(pos.this_thread()->nodes & 0xB)
                   && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50)))
           v = adjusted_NNUE();
   }
