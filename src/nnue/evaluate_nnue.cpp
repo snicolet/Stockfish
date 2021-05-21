a76a2e583f8619380e2bbc0b917e7f64493d9495
Stéphane Nicolet <cassio@free.fr>
2021-05-22 00:01:53 +0200

    Take 2 (delta_npm > Bishop - Knight)
    Bench: 3370337

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -168,7 +168,8 @@ namespace Stockfish::Eval::NNUE {
       int materialist = psqt;
       int positional  = output[0];
 
-      int entertainment = (pos.non_pawn_material(WHITE) != pos.non_pawn_material(BLACK) ? 0 : 9);
+      int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
+      int entertainment = (delta_npm > BishopValueMg - KnightValueMg ? 0 : 7);
 
       // dbg_mean_of(entertainment != 0);
       // dbg_mean_of(entertainment);
