c35e59749796250b344f4c86b2da769674c23c78
Stéphane Nicolet <cassio@free.fr>
2021-05-21 10:58:29 +0200

    entertainment = npm(WHITE) != npm(BLACK) ? 0 : 9
    Bench: 3873221

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -168,7 +168,7 @@ namespace Stockfish::Eval::NNUE {
       int materialist = psqt;
       int positional  = output[0];
 
-      int entertainment = (pos.non_pawn_material(WHITE) != pos.non_pawn_material(BLACK) ? 0 : 7);
+      int entertainment = (pos.non_pawn_material(WHITE) != pos.non_pawn_material(BLACK) ? 0 : 9);
 
       // dbg_mean_of(entertainment != 0);
       // dbg_mean_of(entertainment);
