efb2267104ee183219f98182e3f1109c89932802
Stéphane Nicolet <cassio@free.fr>
2021-05-23 00:29:55 +0200

    pawns: take 1
    Bench: 3677617

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -175,8 +175,11 @@ namespace Stockfish::Eval::NNUE {
       
       int entertainment = (delta_npm <= BishopValueMg - KnightValueMg ? 7 : 0);
       
-     // if (delta_pawn <= -2) entertainment += 7;
-      if (delta_pawn >=  2) entertainment += 7;
+      //if (entertainment == 0)
+      {
+         if (delta_pawn <= -2) entertainment += 13;
+         if (delta_pawn >=  2) entertainment -= 13;
+      }
       
       if (!adjusted)
          entertainment = 0;
