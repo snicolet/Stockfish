7179209fb44dd930e704c7c364f918e7c154078a
Stéphane Nicolet <cassio@free.fr>
2021-05-22 13:01:11 +0200

    entertainment = (delta_pawn ? 7 : 0)
    Bench: 3809691

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -169,8 +169,12 @@ namespace Stockfish::Eval::NNUE {
       int materialist = psqt;
       int positional  = output[0];
 
-      int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
-      int entertainment = (delta_npm <= RookValueMg - BishopValueMg ? 7 : 0);
+      // int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
+      // int entertainment = (delta_npm <= RookValueMg - BishopValueMg ? 7 : 0);
+      
+      int delta_pawn = pos.count<PAWN>(WHITE) - pos.count<PAWN>(BLACK);
+      int entertainment = (delta_pawn != 0 ? 7 : 0);
+      
 
       // dbg_mean_of(entertainment != 0);
       // dbg_mean_of(entertainment);
