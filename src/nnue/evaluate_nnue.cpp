aa620ba49f88e2d8a74477e614dd5b9a3645a7ed
Stéphane Nicolet <cassio@free.fr>
2021-05-23 12:13:25 +0200

    e = 7 + 2*rand(-7..7)
    Bench:

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -170,10 +170,10 @@ namespace Stockfish::Eval::NNUE {
       int positional  = output[0];
 
       int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
-      int entertainment = (delta_npm <= BishopValueMg - KnightValueMg ? 8 : 0);
+      int entertainment = (delta_npm <= BishopValueMg - KnightValueMg ? 7 : 0);
 
       if (entertainment)
-          entertainment += pos.random();
+          entertainment += 2 * pos.random();
 
       if (!adjusted)
           entertainment = 0;
