4fd141b8a3825582791d2bd962c11097f4368028
Stéphane Nicolet <cassio@free.fr>
2021-05-25 07:53:17 +0200

    only tweak positional
    Bench: 3762273

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -172,7 +172,7 @@ namespace Stockfish::Eval::NNUE {
       int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
       int entertainment = (adjusted && delta_npm <= BishopValueMg - KnightValueMg ? 7 : 0);
 
-      int A = 128 - entertainment;
+      int A = 128 ;
       int B = 128 + entertainment;
 
       int sum = (A * materialist + B * positional) / 128;
