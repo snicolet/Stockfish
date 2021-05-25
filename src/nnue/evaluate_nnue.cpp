dd58d68853d9f6dbf026163a09a758f6d20be586
Stéphane Nicolet <cassio@free.fr>
2021-05-25 12:34:36 +0200

    passed with queen: +12
    Bench: 4102693

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -178,7 +178,7 @@ namespace Stockfish::Eval::NNUE {
       
           if (   pos.count<QUEEN>()
               && pos.passed_pawns())
-              entertainment += 10;
+              entertainment += 12;
       }
 
       int A = 128 - entertainment;
