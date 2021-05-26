fba730a546e480367b30043c02bc9abbecb2cf4e
Stéphane Nicolet <cassio@free.fr>
2021-05-26 06:24:33 +0200

    940
    Bench: 3933405

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1117,7 +1117,7 @@ Value Eval::evaluate(const Position& pos) {
       auto  adjusted_NNUE = [&]()
       {
 
-         int scale = 920 + 22 * pos.count<PAWN>() + 30 * pos.non_pawn_material() / 1024;
+         int scale = 940 + 22 * pos.count<PAWN>() + 30 * pos.non_pawn_material() / 1024;
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
 
