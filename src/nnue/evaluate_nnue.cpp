f06ae14c2b146697ed0fd11211e81f08e21ac7b6
Stéphane Nicolet <cassio@free.fr>
2021-05-27 08:50:48 +0200

    compensation: +10
    Bench: 3642297

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -172,7 +172,7 @@ namespace Stockfish::Eval::NNUE {
     
         if (   materialist * positional < 0 
             && positional < -5000 )
-            entertainment += 20;
+            entertainment += 10;
     }
 
     int A = 128 - entertainment;
