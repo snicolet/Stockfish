dd15c14f36bf837120e5f6107f6dc612d2561781
Stéphane Nicolet <cassio@free.fr>
2021-05-27 10:04:20 +0200

    Relax the ambiguity constraint. comp=15
    Bench: 4133108

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -172,7 +172,7 @@ namespace Stockfish::Eval::NNUE {
     
         if (   materialist * positional < 1000000 
             && positional < -5000 )
-            entertainment += 10;
+            entertainment += 15;
     }
 
     int A = 128 - entertainment;
