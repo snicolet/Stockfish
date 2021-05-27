97b776221ac729d767c793cbf491e8fb188885ab
Stéphane Nicolet <cassio@free.fr>
2021-05-27 23:22:22 +0200

    queens on: 128
    Bench: 3536337

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1116,8 +1116,12 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
+         // dbg_mean_of(128 * pos.count<QUEEN>());
 
-         int scale = 903 + 28 * pos.count<PAWN>() + 28 * pos.non_pawn_material() / 1024;
+         int scale =  903
+                    +  28 * pos.count<PAWN>()
+                    +  28 * pos.non_pawn_material() / 1024
+                    + (-186 + 128 * pos.count<QUEEN>());
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
 
