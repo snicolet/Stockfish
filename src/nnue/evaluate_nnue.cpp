b08c6bd64a685c15787907f0880581b6ca9481b7
Stéphane Nicolet <cassio@free.fr>
2021-05-27 18:52:17 +0200

    Take 2, comp=20
    Bench: 3945622

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -175,7 +175,7 @@ namespace Stockfish::Eval::NNUE {
         if ( materialist > 0 && positional < -5000 )
             entertainment += 10;
 
-        if ( positional > 5000 )
+        if ( materialist < 0 && positional > 5000 )
         {
             //dbg_mean_of(delta_npm <= -KnightValueMg);
 
@@ -183,7 +183,7 @@ namespace Stockfish::Eval::NNUE {
             //  entertainment -= 10;
 
             if (delta_npm > -KnightValueMg)
-              entertainment += 30;
+              entertainment += 20;
         }
     }
 
