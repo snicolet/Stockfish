85a4cf204c3664952f6e5851e9330a01307973b2
Stéphane Nicolet <cassio@free.fr>
2021-05-28 07:27:20 +0200

    comp=8
    Bench: 3813625

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -174,8 +174,8 @@ namespace Stockfish::Eval::NNUE {
         entertainment = (abs(delta_npm) <= BishopValueMg - KnightValueMg ? 7 : 0);
 
         if (abs(positional) > 5000)
-           entertainment +=   positional < 0 && materialist > 0            ?  -100000 / positional
-                            : positional > 0 && delta_npm > -KnightValueMg ?   100000 / positional
+           entertainment +=   positional < 0 && materialist > 0            ?   8
+                            : positional > 0 && delta_npm > -KnightValueMg ?   8
                                                                            :   0 ;
     }
 
