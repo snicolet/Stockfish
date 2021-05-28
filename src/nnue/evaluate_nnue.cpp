96fb27b854c26f47965e51c0fba035821563be0d
Stéphane Nicolet <cassio@free.fr>
2021-05-28 07:11:51 +0200

    Try variable entertainment
    Bench: 3602267

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -174,9 +174,9 @@ namespace Stockfish::Eval::NNUE {
         entertainment = (abs(delta_npm) <= BishopValueMg - KnightValueMg ? 7 : 0);
 
         if (abs(positional) > 5000)
-           entertainment +=   positional < 0 && materialist > 0            ?  13
-                            : positional > 0 && delta_npm > -KnightValueMg ?  13
-                                                                           :  0;
+           entertainment +=   positional < 0 && materialist > 0            ?  -100000 / positional
+                            : positional > 0 && delta_npm > -KnightValueMg ?   100000 / positional
+                                                                           :   0 ;
     }
 
     int A = 128 - entertainment;
