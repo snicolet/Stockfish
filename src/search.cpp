1bb748728bc67152224e6fc4086b453924456c15
Stéphane Nicolet <cassio@free.fr>
2021-05-29 11:51:11 +0200

    Revert to "just precondition for LMR: dist <= 3"
    Bench: 4083082

--- src/search.cpp
+++ src/search.cpp
@@ -1125,7 +1125,7 @@ moves_loop: // When in check, search starts from here
           &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
               || (cutNode && (ss-1)->moveCount > 1)
-              || (ss+1)->distanceFromPv <= 6
+              || (ss+1)->distanceFromPv <= 3
               || (!PvNode && !formerPv))
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
