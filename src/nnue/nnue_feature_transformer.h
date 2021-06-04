9d19dab04eb43c4b0bad1a66a1b7ffc559ccc707
Stéphane Nicolet <cassio@free.fr>
2021-06-04 09:54:17 +0200

    Avoid overflow?
    Bench: 3785630

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -179,10 +179,10 @@ namespace Stockfish::Eval::NNUE {
       const auto& accumulation = pos.state()->accumulator.accumulation;
       const auto& psqtAccumulation = pos.state()->accumulator.psqtAccumulation;
 
-      const auto psqt = (
+      const auto psqt = 
             psqtAccumulation[static_cast<int>(perspectives[0])][bucket]
-          - psqtAccumulation[static_cast<int>(perspectives[1])][bucket]
-        ) / 2;
+            - (  psqtAccumulation[static_cast<int>(perspectives[0])][bucket]
+               + psqtAccumulation[static_cast<int>(perspectives[1])][bucket]) / 2;
 
   #if defined(USE_AVX512)
       constexpr IndexType NumChunks = HalfDimensions / (SimdWidth * 2);
