22300e20f63d5ef3effe51648d242e4333507264
Stéphane Nicolet <cassio@free.fr>
2021-06-06 15:50:58 +0200

    Select most important features of opponent (-5)
    Bench: 4007506

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -185,14 +185,14 @@ namespace Stockfish::Eval::NNUE {
       // convert input features for side to move
       for (int i = 0; i < halfD; ++i)
       {
-          int sum = accumulation[stm][i] - 11;
+          int sum = accumulation[stm][i] ;
           output[i] = std::clamp(sum, 0, 127);
       }
 
       // convert input features for opponent
       for (int i = 0; i < halfD; ++i)
       {
-          int sum = accumulation[opponent][i] + 11;
+          int sum = accumulation[opponent][i] - 5;
           output[halfD + i] = std::clamp(sum, 0, 127);
       }
 
