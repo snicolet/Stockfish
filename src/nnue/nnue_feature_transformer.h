775cc378e5c801c3b6cf81c0126181fb91d4d531
Stéphane Nicolet <cassio@free.fr>
2021-06-06 14:09:16 +0200

    tempo = -7
    Bench: 4555088

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -185,14 +185,14 @@ namespace Stockfish::Eval::NNUE {
       // convert input features for side to move
       for (int i = 0; i < halfD; ++i)
       {
-          int sum = accumulation[stm][i] - 5;
+          int sum = accumulation[stm][i] - 7;
           output[i] = std::clamp(sum, 0, 127);
       }
 
       // convert input features for opponent
       for (int i = 0; i < halfD; ++i)
       {
-          int sum = accumulation[opponent][i] + 5;
+          int sum = accumulation[opponent][i] + 7;
           output[halfD + i] = std::clamp(sum, 0, 127);
       }
 
