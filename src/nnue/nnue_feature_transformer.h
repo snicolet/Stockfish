3deb507cac4de4eb5c954f3a0a790796b88b8073
Stéphane Nicolet <cassio@free.fr>
2021-06-06 21:09:42 +0200

    Revert to master in C++

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -185,7 +185,7 @@ namespace Stockfish::Eval::NNUE {
       // convert input features for side to move
       for (int i = 0; i < halfD; ++i)
       {
-          int sum = accumulation[stm][i] - 3;
+          int sum = accumulation[stm][i];
           output[i] = std::clamp(sum, 0, 127);
       }
 
