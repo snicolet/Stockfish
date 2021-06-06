1cdba56e5ff4e82c9175bc3d1c2088a399c0ac26
Stéphane Nicolet <cassio@free.fr>
2021-06-06 22:26:55 +0200

    Decrease reduction when distanceFromPv <= 1
    Bench: 4849906

--- src/search.cpp
+++ src/search.cpp
@@ -1136,7 +1136,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           // Decrease reduction if the current node is very near the PV
-          if (ss->distanceFromPv == 0)
+          if (ss->distanceFromPv <= 1)
               r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
