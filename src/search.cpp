29f47c440788985d16417851c3df409c8ec0383e
Stéphane Nicolet <cassio@free.fr>
2021-06-07 09:55:29 +0200

    Randomized extension (0.5) if r < 0
    Bench: 5303549

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,11 @@ moves_loop: // When in check, search starts from here
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
           // to be searched deeper than the first move.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5));
+          int deeper =   r < -1 && moveCount <= 5  ? 1 
+                      :  r < 0  && (pos.key() & 1) ? 1
+                                                   : 0;
+
+          Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
