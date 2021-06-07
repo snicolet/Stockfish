b3a26f53166bd7400d700623d609f226d080f357
Stéphane Nicolet <cassio@free.fr>
2021-06-07 20:25:56 +0200

    Take 5
    Bench: 5073475

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -165,8 +165,9 @@ namespace Stockfish::Eval::NNUE {
     int positional  = output[0];
 
     int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
-    int entertainment = (adjusted && delta_npm <= BishopValueMg - KnightValueMg) ? (pos.key() & 15) 
-                                                                                 : 0;
+    int entertainment = (adjusted && delta_npm <= BishopValueMg - KnightValueMg) ? 7 : 0;
+    
+    entertainment += (pos.key() & 15) - 7;
 
     int A = 128 - entertainment;
     int B = 128 + entertainment;
