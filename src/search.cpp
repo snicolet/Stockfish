9913690527777c04dacba049b43be433c18f3c12
Stéphane Nicolet <cassio@free.fr>
2021-06-13 18:02:48 +0200

    No double extension after a check extension
    Bench: 4703530

--- src/search.cpp
+++ src/search.cpp
@@ -1193,7 +1193,12 @@ moves_loop: // When in check, search starts from here
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
           // to be searched deeper than the first move, unless ttMove was extended by 2.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && !doubleExtension));
+          int deeper = (   r < -1 
+                        && moveCount <= 5
+                        && extension == 0
+                        && !doubleExtension) ? 1 : 0;
+                       
+          Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
