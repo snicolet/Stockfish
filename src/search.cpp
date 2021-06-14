cc257a1e5f459c69b3d9ab58a98fe7d20dcbd8a2
Stéphane Nicolet <cassio@free.fr>
2021-06-14 11:21:38 +0200

    randomized extensions
    Bench: 5182911

--- src/search.cpp
+++ src/search.cpp
@@ -1192,8 +1192,15 @@ moves_loop: // When in check, search starts from here
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
-          // to be searched deeper than the first move, unless ttMove was extended by 2.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && !doubleExtension));
+          // to be searched deeper than the first move.
+          
+          int deeper =   doubleExtension           ? 0
+                       : r < -2 && (pos.key() & 1) ? 2
+                       : r < -1 && moveCount <= 5  ? 1
+                                                   : 0;
+
+          Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
+
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
