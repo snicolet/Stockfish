d97f651446d971d2f733638b0bbd4c8be0c5658c
Stéphane Nicolet <cassio@free.fr>
2021-06-20 09:11:06 +0200

    quicker (6)
    Bench: 5023806

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1102,10 +1102,10 @@ Value Eval::evaluate(const Position& pos) {
          return nnue;
       };
 
-      // If there is PSQ imbalance we use the classical eval.
+      // If there is PSQ imbalance we use the classical eval
+      int r50 = pos.rule50_count();
       Value psq = Value(abs(eg_value(pos.psq_score())));
-      int   r50 = 16 + pos.rule50_count();
-      bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
+      bool largePsq = psq * 6 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * (6 + r50);
 
       v = largePsq ? Evaluation<NO_TRACE>(pos).value()  // classical
                    : adjusted_NNUE();                   // NNUE
