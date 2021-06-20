31e05b1c449abf7d037de463a8c4438ad6b7b801
Stéphane Nicolet <cassio@free.fr>
2021-06-20 10:32:09 +0200

    weight=32
    Bench: 4965430

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1091,7 +1091,7 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         int weight = 31;
+         int weight = 32;
          
          int scale =   903 
                      + weight * pos.count<PAWN>() 
