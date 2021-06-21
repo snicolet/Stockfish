192346dae447e355432317cbbf8ecd6e871ce27e
Stéphane Nicolet <cassio@free.fr>
2021-06-21 12:10:42 +0200

    Offset = 921
    Bench: 4702206

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1091,7 +1091,7 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         int scale =   912
+         int scale =   921
                      + 32 * pos.count<PAWN>()
                      + 32 * pos.non_pawn_material() / 1024;
 
