f4b65a24aa05cb92f33059464c4b03eb5c1071bb
Stéphane Nicolet <cassio@free.fr>
2021-06-23 16:15:41 +0200

    4 * r50
    Bench: 5116967

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1093,7 +1093,7 @@ Value Eval::evaluate(const Position& pos) {
          int scale =   903
                      + 32 * pos.count<PAWN>()
                      + 32 * pos.non_pawn_material() / 1024
-                     -  8 * pos.rule50_count();
+                     -  4 * pos.rule50_count();
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
 
