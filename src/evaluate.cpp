c7632196d1ff0f2628726f5a114d6f743c29c524
Stéphane Nicolet <cassio@free.fr>
2021-06-30 00:04:36 +0200

    shuffling (8)
    Bench: 5085175

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1090,9 +1090,11 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         int scale =   903
+         int shuffling = clamp(pos.rule50_count() - 8, 0, 100);
+         int scale =  903
                      + 32 * pos.count<PAWN>()
-                     + 32 * pos.non_pawn_material() / 1024;
+                     + 32 * pos.non_pawn_material() / 1024
+                     -  4 * shuffling;
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
 
