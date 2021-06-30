607a2de59d6c2622c0aad3d83e7680eba4bbd611
Stéphane Nicolet <cassio@free.fr>
2021-06-30 18:58:51 +0200

    blah

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1090,13 +1090,13 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         Value nnue      = NNUE::evaluate(pos, true);
-     //  int   shuffling = clamp(pos.rule50_count() - 8, 0, 100);
+         Value nnue     = NNUE::evaluate(pos, true);
+         int shuffling  = clamp(pos.rule50_count() - 8, 0, 100);
 
          int scale =   903
                      + 32 * pos.count<PAWN>()
                      + 32 * pos.non_pawn_material() / 1024
-                  // -  4 * shuffling
+                     -  4 * shuffling
                      ;
 
          nnue = nnue * scale / 1024;
