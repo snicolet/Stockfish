1681a476bf28b127bbe58855ac59fa8c547daa1a
Stéphane Nicolet <cassio@free.fr>
2021-07-01 00:38:13 +0200

    non continuous (8,2)
    Bench: 4885899

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1090,11 +1090,13 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         int shuffling = clamp(pos.rule50_count() - 8, 0, 100);
+         
+         int shuffling = pos.rule50_count() < 8 ? 0 : pos.rule50_count();
+         
          int scale =  903
                      + 32 * pos.count<PAWN>()
                      + 32 * pos.non_pawn_material() / 1024
-                     -  5 * shuffling;
+                     -  2 * shuffling;
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
 
