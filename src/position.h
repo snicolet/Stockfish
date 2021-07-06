7109b5cbea3beb1b293c53c962a9c4ed86af43f5
Stéphane Nicolet <cassio@free.fr>
2021-07-06 14:10:27 +0200

    Revert to best version (shorter span (1024))
    Bench: 5039201

--- src/position.h
+++ src/position.h
@@ -46,7 +46,7 @@ struct StateInfo {
   int    castlingRights;
   int    rule50;
   int    pliesFromNull;
-  int    shuffling[6];
+  int    shuffling[4];
   int    shufflingIndex;
   int    shufflingTotal;
   Square epSquare;
@@ -354,11 +354,11 @@ inline int Position::shuffling() const {
 }
 
 inline void Position::update_shuffling() {
-  // invariant : shufflingTotal is equal to the sum `shuffling[0] + ... + shuffling[5]`
+  // invariant : shufflingTotal is equal to the sum `shuffling[0] + ... + shuffling[3]`
   int i = st->shufflingIndex;
   st->shufflingTotal += st->rule50 - st->shuffling[i];
   st->shuffling[i]    = st->rule50;
-  st->shufflingIndex  = (i + 1) % 6;
+  st->shufflingIndex  = (i + 1) % 4;
 }
 
 inline bool Position::opposite_bishops() const {
