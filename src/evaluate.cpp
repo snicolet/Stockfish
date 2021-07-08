66c566afb4831d44fc8562b389ac91be72e9d9a4
Stéphane Nicolet <cassio@free.fr>
2021-07-08 12:17:06 +0200

    divisor 160
    Bench: 5152316

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1113,8 +1113,8 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Try to detect fortresses
-  int shuffling = std::min(pos.shuffling() , 1024);
-  v = v * (1032 - shuffling) / 1024;
+  int shuffling = std::min(pos.shuffling() , 159);
+  v = v * (168 - shuffling) / 160;
 
   //dbg_mean_of(shuffling);
   /*
