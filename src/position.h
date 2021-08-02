73cc28ff0cb4bd80ed20518bb7cb4fafccda830e
Stéphane Nicolet <cassio@free.fr>
2021-08-02 21:01:55 +0200

    divisor 8
    Bench: 5503646

--- src/position.h
+++ src/position.h
@@ -351,7 +351,7 @@ inline int Position::rule50_count() const {
 }
 
 inline int Position::shuffling() const {
-  return st->rule50 + std::clamp(st->shufflingTotal - 16, 0, 64) / 6;
+  return st->rule50 + std::clamp(st->shufflingTotal - 16, 0, 128) / 8;
 }
 
 inline void Position::update_shuffling() {
