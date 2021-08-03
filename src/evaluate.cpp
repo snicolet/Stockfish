843e87e3a48b7a17c307f43a7d5d2b5df9449514
Stéphane Nicolet <cassio@free.fr>
2021-08-03 06:22:59 +0200

    -4 * shuffling
    Bench: 5383265

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1093,7 +1093,7 @@ Value Eval::evaluate(const Position& pos) {
          int scale =   900
                      + 32 * pos.count<PAWN>()
                      + 32 * pos.non_pawn_material() / 1024
-                     - 7 * pos.shuffling();
+                     - 4 * pos.shuffling();
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
 
