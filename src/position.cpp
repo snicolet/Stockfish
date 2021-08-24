94481bf0ded2667cf1fdef038afa1b75a6cc42f6
Stéphane Nicolet <cassio@free.fr>
2021-08-24 12:56:00 +0200

    SHUFFLING_HISTORY = 2
    Bench: 5370392

--- src/position.cpp
+++ src/position.cpp
@@ -774,6 +774,7 @@ void Position::do_move(Move m, StateInfo& newSt, bool givesCheck) {
       prefetch(thisThread->materialTable[st->materialKey]);
 
       // Reset rule 50 counter
+      update_shuffling();
       st->rule50 = 0;
   }
 
@@ -853,6 +854,7 @@ void Position::do_move(Move m, StateInfo& newSt, bool givesCheck) {
       st->pawnKey ^= Zobrist::psq[pc][from] ^ Zobrist::psq[pc][to];
 
       // Reset rule 50 draw counter
+      update_shuffling();
       st->rule50 = 0;
   }
 
