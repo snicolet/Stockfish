3c458b6195bc960d543097a86e3bcec1397a4fd3
Stéphane Nicolet <cassio@free.fr>
2021-09-01 13:12:01 +0200

    square root of pawns
    Bench: 5134969

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -22,6 +22,7 @@
 #include <cstring>   // For std::memset
 #include <fstream>
 #include <iomanip>
+#include <math.h>
 #include <sstream>
 #include <iostream>
 #include <streambuf>
@@ -1092,10 +1093,10 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         int scale =   920
-                     + 32 * pos.count<PAWN>()
-                     + 32 * pos.non_pawn_material() / 1024
-                     + 64 * pos.count<QUEEN>();
+         int pawns = pos.count<PAWN>();
+         int scale =   883
+                     + 128 * sqrt(pawns)
+                     + 32 * pos.non_pawn_material() / 1024;
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
 
