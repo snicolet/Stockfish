87cfc7c0dac57c379cfb50bf1bc295aac806b0fb
Stéphane Nicolet <cassio@free.fr>
2021-09-06 19:34:50 +0200

    variant for sg
    Bench: 6617396

--- src/search.cpp
+++ src/search.cpp
@@ -1323,9 +1323,13 @@ moves_loop: // When in check, search starts here
                          quietsSearched, quietCount, capturesSearched, captureCount, depth);
 
     // Bonus for prior countermove that caused the fail low
-    else if (   (depth >= 3 || PvNode)
-             && !priorCapture)
-        update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth + (PvNode || cutNode)));
+    else 
+    {
+        Depth dd = depth + 2 * (PvNode || cutNode);
+
+        if (dd >= 3 && !priorCapture)
+            update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(dd));
+    }
 
     if (PvNode)
         bestValue = std::min(bestValue, maxValue);
