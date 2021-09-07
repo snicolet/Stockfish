d8bea9db9c456e9a0bbfcadd3f3102c4b036d6b0
Stéphane Nicolet <cassio@free.fr>
2021-09-07 11:44:21 +0200

    try capture extensions before singular search
    Bench: 6130736

--- src/search.cpp
+++ src/search.cpp
@@ -1038,12 +1038,18 @@ moves_loop: // When in check, search starts here
 
       // Step 14. Extensions (~75 Elo)
 
+      // Capture extensions
+      if (   (PvNode || cutNode) 
+          && captureOrPromotion 
+          && moveCount != 1)
+          extension = 1;
+
       // Singular extension search (~70 Elo). If all moves but one fail low on a
       // search of (alpha-s, beta-s), and just one fails high on (alpha, beta),
       // then that move is singular and should be extended. To verify this we do
       // a reduced search on all the other moves but the ttMove and if the
       // result is lower than ttValue minus a margin, then we will extend the ttMove.
-      if (   !rootNode
+      else if (   !rootNode
           &&  depth >= 7
           &&  move == ttMove
           && !excludedMove // Avoid recursive singular search
@@ -1094,12 +1100,6 @@ moves_loop: // When in check, search starts here
                   return beta;
           }
       }
-
-      // Capture extensions for PvNodes and cutNodes
-      else if (   (PvNode || cutNode) 
-               && captureOrPromotion 
-               && moveCount != 1)
-          extension = 1;
 
       // Check extensions
       else if (   givesCheck
