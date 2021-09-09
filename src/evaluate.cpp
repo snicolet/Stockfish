ee117c78f4b894ad56d46dad4d5e65f262f8a085
Stéphane Nicolet <cassio@free.fr>
2021-09-09 11:51:40 +0200

    Bonus for pawn asymmetry
    Bench: 5983958

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1094,7 +1094,14 @@ Value Eval::evaluate(const Position& pos) {
       {
          int scale =   883
                      + 32 * pos.count<PAWN>()
-                     + 32 * pos.non_pawn_material() / 1024;
+                     + 32 * pos.non_pawn_material() / 1024
+                     + 16 * pos.pawn_asymmetry();
+        
+        /*
+        std::cerr << pos.pawn_asymmetry() << std::endl;
+        if (pos.pawn_asymmetry() >= 6)
+           std::cerr << pos << std::endl;
+        */
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
 
