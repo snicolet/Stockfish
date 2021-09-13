de293e8c9c1eafd6342e620107ce9f851396b14e
Stéphane Nicolet <cassio@free.fr>
2021-09-13 11:36:39 +0200

    take 4 (200)
    Bench: 6530929

--- src/search.cpp
+++ src/search.cpp
@@ -1179,7 +1179,7 @@ moves_loop: // When in check, search starts here
           // Explore more the second moves, as they are the most likely to create a cutoff
           if (   moveCount == 2
               && abs(beta) < 10000
-              && ss->staticEval > beta - 50)
+              && ss->staticEval > beta - 200)
               r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
