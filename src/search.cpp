b3c8b46cef99be6c01412e02d1f6152a43e5f47e
Stéphane Nicolet <cassio@free.fr>
2021-09-22 21:08:35 +0200

    singularBeta - 50*(1 + doubleExtensions)
    Bench: 6121941

--- src/search.cpp
+++ src/search.cpp
@@ -1108,9 +1108,7 @@ moves_loop: // When in check, search starts here
 
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
-                  && value < singularBeta - 75
-                  && ss->doubleExtensions <= 8
-                  )
+                  && value < singularBeta - 50 * (1 + ss->doubleExtensions))
               {
                   extension = 2;
                   doubleExtension = true;
