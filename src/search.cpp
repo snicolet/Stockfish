b5e5a5f5a298b860551d652c21b10721a87e2818
Stéphane Nicolet <cassio@free.fr>
2021-09-23 23:48:52 +0200

    Double extensions in LMR if moveCount == 2
    Bench: 6719213

--- src/search.cpp
+++ src/search.cpp
@@ -1228,6 +1228,7 @@ moves_loop: // When in check, search starts here
           int deeper =   r >= -1               ? 0
                        : extension             ? 0
                        : noLMRExtension        ? 0
+                       : moveCount == 2        ? 2
                        : moveCount <= 5        ? 1
                        : (depth > 6 && PvNode) ? 1
                        :                         0;
