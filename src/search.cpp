dc57992dee52ca7c1d1dbf3e603f6d8250ce905f
Stéphane Nicolet <cassio@free.fr>
2021-09-24 00:51:30 +0200

    Count all double extensions (<= 3)
    Bench: 5918460

--- src/search.cpp
+++ src/search.cpp
@@ -1109,7 +1109,7 @@ moves_loop: // When in check, search starts here
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
                   && value < singularBeta - 75
-                  && ss->doubleExtensions <= 8)
+                  && ss->doubleExtensions <= 3)
               {
                   extension = 2;
                   noLMRExtension = true;
