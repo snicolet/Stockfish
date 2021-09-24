9beefe691e6fa89c422aeb3febca695a0f017811
Stéphane Nicolet <cassio@free.fr>
2021-09-24 07:50:48 +0200

    doubleExt <= 7, margin = 80
    Bench: 6352248

--- src/search.cpp
+++ src/search.cpp
@@ -1108,7 +1108,7 @@ moves_loop: // When in check, search starts here
 
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
-                  && value < singularBeta - 70
+                  && value < singularBeta - 80
                   && ss->doubleExtensions <= 7)
               {
                   extension = 2;
