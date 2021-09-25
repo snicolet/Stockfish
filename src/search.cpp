18a38f49b49e36be253eabc709abfcc3fc0c4618
Stéphane Nicolet <cassio@free.fr>
2021-09-25 21:07:27 +0200

    extension = -4  (for case 2)
    Bench: 5737714

--- src/search.cpp
+++ src/search.cpp
@@ -1122,21 +1122,19 @@ moves_loop: // When in check, search starts here
           // that multiple moves fail high, and we can prune the whole subtree by returning
           // a soft bound.
           else if (singularBeta >= beta)
-              return singularBeta;
+              extension = -4;
 
           // If the eval of ttMove is greater than beta we try also if there is another
           // move that pushes it over beta, if so the position also has probably multiple
           // moves giving fail highs. We will then reduce the ttMove (negative extension).
           else if (ttValue >= beta)
           {
-              singularDepth = (depth + 3) / 2;
-
               ss->excludedMove = move;
-              value = search<NonPV>(pos, ss, beta - 1, beta, singularDepth, cutNode);
+              value = search<NonPV>(pos, ss, beta - 1, beta, (depth + 3) / 2, cutNode);
               ss->excludedMove = MOVE_NONE;
 
               if (value >= beta)
-                  extension = singularDepth - depth;
+                  extension = -2;
           }
       }
 
