50c23b2da8c3d85c7346b85ab5e63f8becca241d
Stéphane Nicolet <cassio@free.fr>
2021-09-25 08:11:24 +0200

    extension = -5
    Bench: 5142761

--- src/search.cpp
+++ src/search.cpp
@@ -1133,8 +1133,7 @@ moves_loop: // When in check, search starts here
               ss->excludedMove = MOVE_NONE;
 
               if (value >= beta)
-                  extension = -4;
-              
+                  extension = -5;
           }
       }
 
