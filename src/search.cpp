82e2bd73bc0c0422870e0940476467e70d647f31
Stéphane Nicolet <cassio@free.fr>
2021-09-26 16:25:25 +0200

    take 2
    Bench: 6432401

--- src/search.cpp
+++ src/search.cpp
@@ -1266,10 +1266,10 @@ moves_loop: // When in check, search starts here
           // If the move passed LMR update its stats
           if (didLMR && !captureOrPromotion)
           {
-              int bonus = value > alpha ?  stat_bonus(newDepth)
-                                        : -stat_bonus(newDepth);
+              int bonus = value > alpha ?  stat_bonus(newDepth + moveCount)
+                                        : -stat_bonus(newDepth + moveCount);
 
-              update_continuation_histories(ss, movedPiece, to_sq(move), bonus * moveCount);
+              update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
       }
 
