40a9ce95f40fde724a14751a39bd82a717705363
Stéphane Nicolet <cassio@free.fr>
2021-09-29 19:49:31 +0200

    (800, -1200)
    Bench: 6145836

--- src/search.cpp
+++ src/search.cpp
@@ -1266,7 +1266,7 @@ moves_loop: // When in check, search starts here
           // If the move passed LMR update its stats
           if (didLMR && !captureOrPromotion)
           {
-              int bonus = value > alpha ?  1200
+              int bonus = value > alpha ?  800
                                         : -1200;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
@@ -1772,7 +1772,7 @@ moves_loop: // When in check, search starts here
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
     thisThread->mainHistory[us][from_to(move)] << bonus;
-    update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), 550);
+    update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
 
     // Penalty for reversed move in case of moved piece not being a pawn
     if (type_of(pos.moved_piece(move)) != PAWN)
