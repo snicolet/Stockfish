e39d24649f535cc160cbc659388e287427dfa0e2
Stéphane Nicolet <cassio@free.fr>
2021-10-02 12:40:19 +0200

    take 4
    Bench: 6091396

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -170,7 +170,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(*cur, Value(std::min(0, -69 * cur->value / 1024))) ?
+                       return pos.see_ge(*cur, Value(std::min(1, -69 * cur->value / 1024))) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
