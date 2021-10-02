435d18df0e02ca09f66bdd50ffefb4cec35ae776
Stéphane Nicolet <cassio@free.fr>
2021-10-02 22:42:24 +0200

    take 2
    Bench: 6131327

--- src/search.cpp
+++ src/search.cpp
@@ -1142,20 +1142,20 @@ moves_loop: // When in check, search starts here
       else if (   (PvNode || cutNode)
                && captureOrPromotion
                && moveCount != 1)
-          extension = 1;
+          extension += 1;
 
       // Check extensions
       else if (   givesCheck
                && depth > 6
                && abs(ss->staticEval) > 100)
-          extension = 1;
+          extension += 1;
 
       // Quiet ttMove extensions
       else if (   PvNode
                && move == ttMove
                && move == ss->killers[0]
                && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
-          extension = 1;
+          extension += 1;
 
       // Add extension to new depth
       newDepth += extension;
