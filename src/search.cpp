d20021a4783b96bdb68a39770f7bcfa29ca986a9
Stéphane Nicolet <cassio@free.fr>
2021-10-04 20:45:43 +0200

    capping bonus at 4000
    Bench: 6066701

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   int stat_bonus(Depth d) {
     int bonus = 6 * d * d + 229 * d - 215;
 
-    bonus = std::min(bonus, 3000);
+    bonus = std::min(bonus, 4000);
 
     //dbg_mean_of(bonus);
 
