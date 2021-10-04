548ea2d192cf2c252852189990f43f73646b1eef
Stéphane Nicolet <cassio@free.fr>
2021-10-04 13:40:46 +0200

    constant bonus (1140)
    Bench: 6384325

--- src/search.cpp
+++ src/search.cpp
@@ -1266,8 +1266,8 @@ moves_loop: // When in check, search starts here
           // If the move passed LMR update its stats
           if (didLMR && !captureOrPromotion)
           {
-              int bonus = value > alpha ?  1210
-                                        : -1210;
+              int bonus = value > alpha ?  1140
+                                        : -1140;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
