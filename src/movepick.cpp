e758defc2c5bb7b5b7c39781126f279e5371b704
Stéphane Nicolet <cassio@free.fr>
2021-10-07 10:51:03 +0200

    try the opposite
    Bench: 6033691

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -103,7 +103,7 @@ void MovePicker::score() {
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
-                   + int(PieceValue[MG][pos.piece_on(from_sq(m))])
+                   - int(PieceValue[MG][pos.piece_on(from_sq(m))])
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
