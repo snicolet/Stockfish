c2aaa1286cbec397e2bd100f82cd5128313eff18
Stéphane Nicolet <cassio@free.fr>
2021-10-08 05:32:43 +0200

    constant bonus for didLMR (1150)
    Bench: 6713474

--- src/search.cpp
+++ src/search.cpp
@@ -1266,8 +1266,8 @@ moves_loop: // When in check, search starts here
           // If the move passed LMR update its stats
           if (didLMR && !captureOrPromotion)
           {
-              int bonus = value > alpha ?  stat_bonus(newDepth)
-                                        : -stat_bonus(newDepth);
+              int bonus = value > alpha ?  1050
+                                        : -1050;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
