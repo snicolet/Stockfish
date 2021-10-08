f4f28812f3e17abf8577c9864833c90e3b46f0ff
Stéphane Nicolet <cassio@free.fr>
2021-10-08 05:44:12 +0200

    capping at 1700
    Bench: 6664235

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((6 * d + 229) * d - 215 , 1650);
+    return std::min((6 * d + 229) * d - 215 , 1700);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
