d53d8a5a002bee161fd470bd7997784d88ab12c3
Stéphane Nicolet <cassio@free.fr>
2021-10-16 08:31:09 +0200

    Use cutNode instead of !PvNode for futility pruning
    Bench: 4889786

--- src/search.cpp
+++ src/search.cpp
@@ -818,6 +818,7 @@ namespace {
     // Step 7. Futility pruning: child node (~50 Elo).
     // The depth condition is important for mate finding.
     if (   !PvNode
+        &&  cutNode
         &&  depth < 9
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
