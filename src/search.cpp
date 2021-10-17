47b0f02c50e97b29e0d469c091585138038582e3
Stéphane Nicolet <cassio@free.fr>
2021-10-17 13:12:46 +0200

    take 4
    Bench: 5633442

--- src/search.cpp
+++ src/search.cpp
@@ -1147,7 +1147,7 @@ moves_loop: // When in check, search starts here
 
       // Add extension to new depth
       newDepth += extension;
-      ss->doubleExtensions = (ss-1)->doubleExtensions + (extension == 2);
+      ss->doubleExtensions = (ss-1)->doubleExtensions + (extension >= 2);
 
       // Speculative prefetch as early as possible
       prefetch(TT.first_entry(pos.key_after(move)));
