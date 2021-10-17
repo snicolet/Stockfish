977b90977fb1a16b19c7167fc97885edc5c20c9c
Stéphane Nicolet <cassio@free.fr>
2021-10-17 18:43:01 +0200

    Count all double extensions, take 3
    Bench: 4734721

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,7 @@ moves_loop: // When in check, search starts here
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
                   && value < singularBeta - 75
-                  && ss->doubleExtensions <= 7)
+                  && ss->doubleExtensions <= 5)
                   extension = 2;
           }
 
