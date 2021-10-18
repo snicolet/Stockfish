91b4a0d0ee4152611bd5782d480009a6dc4c82c6
Stéphane Nicolet <cassio@free.fr>
2021-10-18 13:08:45 +0200

    Limit double extensions (13)
    Bench: 4849541

--- src/search.cpp
+++ src/search.cpp
@@ -553,7 +553,7 @@ namespace {
         && depth > (ss-1)->depth)
        depth = (ss-1)->depth;
 
-    if (   (ss-1)->doubleExtensions >= 11
+    if (   (ss-1)->doubleExtensions >= 13
         && depth > (ss-1)->depth)
        depth = (ss-1)->depth;
 
