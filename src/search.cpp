5b8fcceaedd2d235ec526b18462f8f1a94785ee6
Stéphane Nicolet <cassio@free.fr>
2021-10-20 22:21:01 +0200

    Double extend only quiet moves: take 2
    Bench: 6519420

--- src/search.cpp
+++ src/search.cpp
@@ -1100,7 +1100,7 @@ moves_loop: // When in check, search starts here
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
                   && value < singularBeta - 75
-                  && ttCapture)
+                  && !ttCapture)
                   extension = 2;
           }
 
