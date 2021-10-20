1f98e0c84225253198a9f09cb5bcd0813e9c5022
Stéphane Nicolet <cassio@free.fr>
2021-10-20 22:11:35 +0200

    Double extend only captures: take 2
    Bench: 5398369

--- src/search.cpp
+++ src/search.cpp
@@ -1100,8 +1100,7 @@ moves_loop: // When in check, search starts here
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
                   && value < singularBeta - 75
-                  && ttCapture
-                  && ss->doubleExtensions <= 6)
+                  && ttCapture)
                   extension = 2;
           }
 
