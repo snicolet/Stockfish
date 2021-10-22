fd6e2589708fb75fc1f9896e39fc66b2c40cae14
Stéphane Nicolet <cassio@free.fr>
2021-10-22 18:02:10 +0200

    Randomize positions with more than 13 pawns
    Bench: 6186479

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1100,7 +1100,7 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Randomize eval a little
-  if (pos.count<ALL_PIECES>() >= 24)
+  if (pos.count<PAWN>() >= 13)
       v += Value(2 * (pos.key() & 7) - 7);
 
   // Damp down the evaluation linearly when shuffling
