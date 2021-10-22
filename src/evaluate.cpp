67cc0844955fbc86e56100dcc3ab7933d9ace2d4
Stéphane Nicolet <cassio@free.fr>
2021-10-22 14:25:32 +0200

    Randomize positions with more than 20 pieces
    Bench: 5735248

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1100,7 +1100,7 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Randomize eval a little
-  if (pos.count<BISHOP>() == 0)
+  if (pos.count<ALL_PIECES>() >= 20)
       v += Value(2 * (pos.key() & 7) - 7);
 
   // Damp down the evaluation linearly when shuffling
