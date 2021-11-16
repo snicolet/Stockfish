06f5d182361a4a09d4ec223f9c5105592b1773cd
Stéphane Nicolet <cassio@free.fr>
2021-11-16 22:14:33 +0100

    mat += epsilon
    Bench: 6556896

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1093,7 +1093,7 @@ Value Eval::evaluate(const Position& pos) {
   {
        int scale =   898
                    + 24 * pos.count<PAWN>()
-                   + 33 * pos.non_pawn_material() / 1024;
+                   + 34 * pos.non_pawn_material() / 1024;
 
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
        Color stm      = pos.side_to_move();
