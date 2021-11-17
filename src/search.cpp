7bebfa14f703ab059bad337c19c48fa0d7e42f39
Stéphane Nicolet <cassio@free.fr>
2021-11-17 23:26:22 +0100

    clamp(-40, 40)
    Bench: 7053783

--- src/search.cpp
+++ src/search.cpp
@@ -389,7 +389,7 @@ void Thread::search() {
               trend = (us == WHITE ?  make_score(tr, tr / 2)
                                    : -make_score(tr, tr / 2));
 
-              int opt = std::clamp(25 + tr, -50, 50);
+              int opt = std::clamp(25 + tr, -40, 40);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }
