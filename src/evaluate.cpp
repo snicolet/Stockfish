8e605cbbc4b93022860263b5a166db5e7d4e4945
Stéphane Nicolet <cassio@free.fr>
2021-11-28 14:05:45 +0100

    990 + 16 * pawns
    Bench: 6765050

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1097,7 +1097,7 @@ Value Eval::evaluate(const Position& pos) {
 //                    + 20 * pos.non_pawn_material() / 1024;
                    
                    
-       int scale =  970
+       int scale =  990
                    + 16 * pos.count<PAWN>()
                    + 20 * pos.non_pawn_material() / 1024;            
                    
