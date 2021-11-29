ad4774b6f89cfe653949d18e1de52e52b1f5be6e
Stéphane Nicolet <cassio@free.fr>
2021-11-29 22:26:58 +0100

    Optimism = 24765, y0=30, C=200
    Bench: 6702691

--- src/search.cpp
+++ src/search.cpp
@@ -388,7 +388,7 @@ void Thread::search() {
               trend = (us == WHITE ?  make_score(tr, tr / 2)
                                    : -make_score(tr, tr / 2));
 
-              int opt = sigmoid(prev, 0, 35, 147, 24765, 256);
+              int opt = sigmoid(prev, 0, 30, 200, 24765, 256);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }
