dcce577379d002b2d58f4a04efe27621b98bb888
Stéphane Nicolet <cassio@free.fr>
2021-12-05 12:15:23 +0100

    accelerate, take 2
    Bench: 5284839

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1091,10 +1091,10 @@ Value Eval::evaluate(const Position& pos) {
       v = Evaluation<NO_TRACE>(pos).value();          // classical
   else
   {
-       int scale = 1066
+       int scale = 1051
                    +  8 * pos.count<PAWN>()
                    + 20 * pos.non_pawn_material() / 1024
-                   -  8 * pos.rule50_count();
+                   -      pos.rule50_count();
 
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
        Color stm      = pos.side_to_move();
