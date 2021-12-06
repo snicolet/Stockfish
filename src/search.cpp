2704a5ef444e430831e0fa8aa3eddb3f9d784c38
Stéphane Nicolet <cassio@free.fr>
2021-12-06 18:02:33 +0100

    ttValue - 3 * depth - 20
    Bench: 3702146

--- src/search.cpp
+++ src/search.cpp
@@ -1097,7 +1097,7 @@ moves_loop: // When in check, search starts here
           && (tte->bound() & BOUND_LOWER)
           &&  tte->depth() >= depth - 3)
       {
-          Value singularBeta = ttValue - 3 * depth;
+          Value singularBeta = ttValue - 3 * depth - 20;
           Depth singularDepth = (depth - 1) / 2;
 
           ss->excludedMove = move;
@@ -1111,7 +1111,7 @@ moves_loop: // When in check, search starts here
 
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
-                  && value < singularBeta - 85
+                  && value < singularBeta - 75
                   && ss->doubleExtensions <= 6)
                   extension = 2;
           }
