aa5a0cb4666d47e9de3685a830c4f7728c7cc615
Stéphane Nicolet <cassio@free.fr>
2021-12-08 00:03:56 +0100

    Simplify LMR precondition
    Bench: 4763524

--- src/search.cpp
+++ src/search.cpp
@@ -1172,10 +1172,7 @@ moves_loop: // When in check, search starts here
       // been searched. In general we would like to reduce them, but there are many
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 3
-          &&  moveCount > 1 + 2 * rootNode
-          && (   !ss->ttPv
-              || !captureOrPromotion
-              || (cutNode && (ss-1)->moveCount > 1)))
+          &&  moveCount > 1 + 2 * rootNode)
       {
           Depth r = reduction(improving, depth, moveCount, rangeReduction > 2);
 
