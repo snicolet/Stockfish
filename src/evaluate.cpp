89263910c901ec791a8f27280795fbffc9071a6f
Stéphane Nicolet <cassio@free.fr>
2021-12-20 19:41:38 +0100

    take 6
    Bench: 4621370

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1091,9 +1091,9 @@ Value Eval::evaluate(const Position& pos) {
       v = Evaluation<NO_TRACE>(pos).value();          // classical
   else
   {
-       int scale =  990
+       int scale = 1070
                    + 20 * pos.non_pawn_material() / 1024
-                   + 16 * pos.count<PAWN>();
+                   +  8 * pos.count<PAWN>();
 
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
        Color stm      = pos.side_to_move();
