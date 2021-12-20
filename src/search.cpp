ac916175c61ec76a516ac500123d48a229a946e2
Stéphane Nicolet <cassio@free.fr>
2021-12-20 09:43:30 +0100

    margin = -218 * depth - abs(history / 8)
    Bench: 5157860

--- src/search.cpp
+++ src/search.cpp
@@ -1049,7 +1049,7 @@ moves_loop: // When in check, search starts here
 
               // SEE based pruning
               int hist = pos.empty(to_sq(move)) ? 0 : captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))];
-              int margin = -218 * depth - std::abs(hist / 32);
+              int margin = -218 * depth - std::abs(hist / 8);
 
               if (!pos.see_ge(move, Value(margin))) // (~25 Elo)
                   continue;
