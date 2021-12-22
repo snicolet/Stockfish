fa37bd17bcc5886e0620460131a0fdad4ad6a698
Stéphane Nicolet <cassio@free.fr>
2021-12-22 12:02:33 +0100

    mean (7/8, 1/8) rebased
    bench: 4157941

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1106,7 +1106,7 @@ Value Eval::evaluate(const Position& pos) {
        Value optimism = pos.this_thread()->optimism[stm];
 
        if (classical != VALUE_NONE)
-           nnue = (15 * nnue + classical) / 16;
+           nnue = (7 * nnue + classical) / 8;
 
        v = (nnue + optimism) * scale / 1024 - optimism;
 
