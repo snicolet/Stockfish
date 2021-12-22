db235c402e6f389c31f0745f4090e07564e76aee
Stéphane Nicolet <cassio@free.fr>
2021-12-22 09:17:06 +0100

    mean (7/8, 1/8)
    Bench: 4365159

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1106,7 +1106,7 @@ Value Eval::evaluate(const Position& pos) {
        Value optimism = pos.this_thread()->optimism[stm];
 
        if (classical != VALUE_NONE)
-           nnue = (3 * nnue + classical) / 4;
+           nnue = (7 * nnue + classical) / 8;
 
        v = (nnue + optimism) * scale / 1024 - optimism;
 
