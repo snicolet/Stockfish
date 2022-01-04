8e6ef158b5d2fb9b07ab7d6142580465c21aa263
Stéphane Nicolet <cassio@free.fr>
2022-01-04 14:04:03 +0100

    r -= 2
    Bench: 6356028

--- src/search.cpp
+++ src/search.cpp
@@ -1186,7 +1186,7 @@ moves_loop: // When in check, search starts here
         
           // distanceFromPv
           if ((ss+1)->distanceFromPv <= 4)
-              r--;
+              r -= 2;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
