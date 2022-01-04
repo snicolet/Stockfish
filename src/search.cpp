71be7b5fdba6d31901aac2870564b3eb8f0a0a44
Stéphane Nicolet <cassio@free.fr>
2022-01-04 15:18:35 +0100

    dist <= 5 (take 2)
    bench:

--- src/search.cpp
+++ src/search.cpp
@@ -1197,7 +1197,8 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
-                       : (ss+1)->distanceFromPv <= 5 ? 2
+                       : (ss+1)->distanceFromPv <= 5 ? 1
+                       : moveCount <= 5            ? 2
                        : PvNode && depth > 6       ? 1
                        : cutNode && moveCount <= 7 ? 1
                        :                             0;
