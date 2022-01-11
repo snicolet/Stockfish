f83ec2481317914c0f219293826e7206b4900777
Stéphane Nicolet <cassio@free.fr>
2022-01-11 22:41:36 +0100

    take 2ter
    Bench: 4626491

--- src/search.cpp
+++ src/search.cpp
@@ -92,7 +92,7 @@ namespace {
 
     return   VALUE_DRAW
            - optimism
-           + Value(2 * (pos.key() & 3) - 3);
+           + Value((th->nodes & 3) + (pos.key() & 3) - 3);
   }
 
   // Check if the current thread is in a search explosion
