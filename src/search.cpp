63469b90543d4044a4fa7c9dd8313872a3a91e66
Stéphane Nicolet <cassio@free.fr>
2022-02-21 16:56:30 +0100

    take 6
    Bench: 5015545

--- src/search.cpp
+++ src/search.cpp
@@ -66,7 +66,7 @@ namespace {
       uint64_t nodes = thisThread->nodes;
       
       return nodes <  300000 ? 0 :
-             nodes < 1200000 ? 1 :
+             nodes < 2400000 ? 1 :
                                2 ;
   }
 
