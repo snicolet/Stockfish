396d2eef296a2b2a28c4d7b05b0c631a416a3153
Stéphane Nicolet <cassio@free.fr>
2022-02-27 14:12:59 +0100

    multicut: reduce(-4), don't prune
    Bench: 4855033

--- src/search.cpp
+++ src/search.cpp
@@ -1101,7 +1101,7 @@ moves_loop: // When in check, search starts here
               // that multiple moves fail high, and we can prune the whole subtree by returning
               // a soft bound.
               else if (singularBeta >= beta)
-                  extension = -2;
+                  extension = -4;
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
