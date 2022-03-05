7c28e9edf8f37247c27a99d3358f5cd5c3fa22c5
Stéphane Nicolet <cassio@free.fr>
2022-03-05 09:39:29 +0100

    (depthSTC, depthLTC) = (15, 19)
    bench: 4644022

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
      int rootDepth = thisThread->rootDepth;
      int stage = rootDepth < 15 ? 0 :
-                 rootDepth < 21 ? 1 :
+                 rootDepth < 19 ? 1 :
                                   2 ;
      return stage;
   }
