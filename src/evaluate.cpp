dc6b7a7973b78044e0161137272d4964fff48a20
Stéphane Nicolet <cassio@free.fr>
2022-03-13 09:57:44 +0100

    negative extension = -4, s/32
    bench: 6109990

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1101,7 +1101,7 @@ Value Eval::evaluate(const Position& pos) {
        int shuffle    = pos.rule50_count();
        int scale      = 1036 
                         + 20 * pos.non_pawn_material() / 1024
-                        - shuffle * shuffle / 16;
+                        - shuffle * shuffle / 32;
 
        v = nnue * scale / 1024;
 
