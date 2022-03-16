133c81c66b07afa8391521d3d743a73f4891674f
Stéphane Nicolet <cassio@free.fr>
2022-03-16 22:27:05 +0100

    take 5 with optimism=70
    bench: 6244915

--- src/search.cpp
+++ src/search.cpp
@@ -358,7 +358,7 @@ void Thread::search() {
               trend = (us == WHITE ?  make_score(tr, tr / 2)
                                    : -make_score(tr, tr / 2));
 
-              int opt = sigmoid(prev, -100, 50, 144, 13966, 183);
+              int opt = sigmoid(prev, -100, 70, 144, 13966, 183);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }
@@ -1095,7 +1095,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2;
+                  extension = -3;
           }
 
           // Check extensions (~1 Elo)
