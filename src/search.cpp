45b4164abbda5a332440a58efc9f96ca95570cab
Stéphane Nicolet <cassio@free.fr>
2022-03-16 22:00:02 +0100

    take 6
    bench: 6639512

--- src/search.cpp
+++ src/search.cpp
@@ -1095,7 +1095,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -3;
+                  extension = -2;
           }
 
           // Check extensions (~1 Elo)
