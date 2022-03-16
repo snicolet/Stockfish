ad2f0869642a2f1cd1b1e4246b7a784d8e19bb4a
Stéphane Nicolet <cassio@free.fr>
2022-03-16 08:13:03 +0100

    take 1 with s/21
    bench: 6691349

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1099,9 +1099,9 @@ Value Eval::evaluate(const Position& pos) {
   {
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
        int shuffle    = pos.rule50_count();
-       int scale      = 1050
+       int scale      = 1036
                         + 22 * pos.non_pawn_material() / 1024
-                        - shuffle * shuffle / 16;
+                        - shuffle * shuffle / 21;
 
        v = nnue * scale / 1024;
 
