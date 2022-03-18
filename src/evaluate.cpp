59c3fb1879dd2b0cb42bab6b8790836127aadec7
Stéphane Nicolet <cassio@free.fr>
2022-03-18 21:32:04 +0100

    take 2
    bench: 6769070

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1098,7 +1098,10 @@ Value Eval::evaluate(const Position& pos) {
   if (useNNUE && !useClassical)
   {
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
-       int scale      = 1036 + 20 * pos.non_pawn_material() / 1024;
+       int shuffle    = pos.rule50_count();
+       int scale      = 1036
+                        + 20 * pos.non_pawn_material() / 1024
+                        - shuffle * shuffle / 32;
 
        v = nnue * scale / 1024;
 
