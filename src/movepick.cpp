c6257a77b500fec2ef9386eacb53f6235775560f
Stéphane Nicolet <cassio@free.fr>
2022-04-01 11:57:46 +0200

    clean-up comments

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -110,11 +110,9 @@ void MovePicker::score() {
   if constexpr (Type == QUIETS)
   {
       Color us = pos.side_to_move();
-      // squares threatened by pawns
+
       threatenedByPawn  = pos.attacks_by<PAWN>(~us);
-      // squares threatened by minors or pawns
       threatenedByMinor = pos.attacks_by<KNIGHT>(~us) | pos.attacks_by<BISHOP>(~us) | threatenedByPawn;
-      // squares threatened by rooks, minors or pawns
       threatenedByRook  = pos.attacks_by<ROOK>(~us) | threatenedByMinor;
 
       // pieces threatened by pieces of lesser material value
