5a1dd74c195a0314731525c439ae59db37b3c87a
Stéphane Nicolet <cassio@free.fr>
2022-04-11 09:27:52 +0200

    take 4.6 (1012 - s/32)
    bench: 7680627

--- src/position.h
+++ src/position.h
@@ -373,7 +373,7 @@ inline int Position::shuffling() const {
         + st->shuffling[5] * st->shuffling[5]
         + st->shuffling[6] * st->shuffling[6]
         + st->shuffling[7] * st->shuffling[7]
-        + st->rule50       * st->rule50
+        // + st->rule50       * st->rule50
         ;
 }
 
