107380a7905b3ffbeaf41af119c5b4e0eb3bf463
Stéphane Nicolet <cassio@free.fr>
2022-04-21 21:26:43 +0200

    take 4
    bench: 8508136

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1102,7 +1102,7 @@ Value Eval::evaluate(const Position& pos) {
        int scale      = 1036 
                         + 22 * pos.non_pawn_material() / 1024
                         +  8 * pos.count<PAWN>()
-                        -  4 * pos.rule50_count();
+                        -  2 * pos.rule50_count();
        Color stm      = pos.side_to_move();
        Value optimism = pos.this_thread()->optimism[stm];
        Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
