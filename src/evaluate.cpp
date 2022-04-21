7dd26da934c95503577449025f2b6597a043e164
Stéphane Nicolet <cassio@free.fr>
2022-04-21 20:49:06 +0200

    take 1
    bench: 7510222

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1099,7 +1099,9 @@ Value Eval::evaluate(const Position& pos) {
   if (useNNUE && !useClassical)
   {
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
-       int scale      = 1036 + 22 * pos.non_pawn_material() / 1024;
+       int scale      = 1036 
+                        + 22 * pos.non_pawn_material() / 1024
+                        +  8 * pos.count<PAWN>();
        Color stm      = pos.side_to_move();
        Value optimism = pos.this_thread()->optimism[stm];
        Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
