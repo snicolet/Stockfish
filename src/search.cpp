73218eb74c3efaa51dc3fe2d5e385b5fcd8018c8
Stéphane Nicolet <cassio@free.fr>
2022-04-24 17:22:26 +0200

    take 2
    bench: 8236560

--- src/search.cpp
+++ src/search.cpp
@@ -556,7 +556,7 @@ namespace {
     bool givesCheck, improving, didLMR, priorCapture;
     bool capture, doFullDepthSearch, moveCountPruning, ttCapture;
     Piece movedPiece;
-    int moveCount, captureCount, quietCount, improvement, complexity;
+    int moveCount, captureCount, quietCount, bestMoveCount, improvement, complexity;
 
     // Step 1. Initialize node
     Thread* thisThread = pos.this_thread();
@@ -564,7 +564,7 @@ namespace {
     ss->inCheck        = pos.checkers();
     priorCapture       = pos.captured_piece();
     Color us           = pos.side_to_move();
-    moveCount          = captureCount = quietCount = ss->moveCount = 0;
+    moveCount          = bestMoveCount = captureCount = quietCount = ss->moveCount = 0;
     bestValue          = -VALUE_INFINITE;
     maxValue           = VALUE_INFINITE;
     initialAlpha       = alpha;
@@ -993,9 +993,12 @@ moves_loop: // When in check, search starts here
       capture = pos.capture(move);
       movedPiece = pos.moved_piece(move);
       givesCheck = pos.gives_check(move);
+      int bestMoveCountReduction =    bestMoveCount >= 2
+                                   && alpha > -VALUE_KNOWN_WIN
+                                   && beta  <  VALUE_KNOWN_WIN;
 
       // Calculate new depth for this move
-      newDepth = depth - 1;
+      newDepth = depth - 1 - bestMoveCountReduction;
 
       Value delta = beta - alpha;
 
@@ -1299,12 +1302,7 @@ moves_loop: // When in check, search starts here
               if (PvNode && value < beta) // Update alpha! Always alpha < beta
               {
                   alpha = value;
-
-                  // Reduce other moves if we have found at least one score improvement
-                  if (   beta         <  VALUE_KNOWN_WIN 
-                      && initialAlpha > -VALUE_KNOWN_WIN
-                      && moveCount >= 2)
-                     depth -= 1;
+                  bestMoveCount++;
               }
               else
               {
