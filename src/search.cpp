c8dab7cc00b492d62eade393cd75ea8e35532503
Stéphane Nicolet <cassio@free.fr>
2022-05-01 18:17:19 +0200

    take 10
    bench: 6445826

--- src/search.cpp
+++ src/search.cpp
@@ -1300,12 +1300,12 @@ moves_loop: // When in check, search starts here
                   alpha = value;
 
                   // Reduce other moves if we have found at least one score improvement
-                  if (   depth > 2
-                      && depth < 7
+                  if (   depth > 1
+                      && depth < 6
                       && beta  <  VALUE_KNOWN_WIN 
                       && alpha > -VALUE_KNOWN_WIN)
                      depth -= 1;
-                
+
                   assert(depth > 0);
               }
               else
