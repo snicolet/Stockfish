08cf4a0e40e2f6a8e5ad4e44e572ba570b6ca265
Stéphane Nicolet <cassio@free.fr>
2022-08-18 08:56:37 +0200

    take 4 (scale = 1000+128*pair)
    bench:5831329

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1068,7 +1068,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
 
       int bishopPairs = (pos.count<BISHOP>(WHITE) >= 2) || (pos.count<BISHOP>(BLACK) >= 2);
 
-      int scale =   936 
+      int scale =  1000 
                   + 106 * pos.non_pawn_material() / 5120
                   + 128 * bishopPairs;
                   
