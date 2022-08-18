a67753b959b673013664f8153cfb1aee1dbb6165
Stéphane Nicolet <cassio@free.fr>
2022-08-18 08:52:24 +0200

    take 3 (scale = 936+128*pair)
    bench: 5910988

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1068,9 +1068,9 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
 
       int bishopPairs = (pos.count<BISHOP>(WHITE) >= 2) || (pos.count<BISHOP>(BLACK) >= 2);
 
-      int scale =  1032 
+      int scale =   936 
                   + 106 * pos.non_pawn_material() / 5120
-                  +  64 * bishopPairs;
+                  + 128 * bishopPairs;
                   
       Value optimism = pos.this_thread()->optimism[stm];
 
