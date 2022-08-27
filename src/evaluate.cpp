c4d8697733e9a1ca51b1a71a924c1e9c53a5146d
Stéphane Nicolet <cassio@free.fr>
2022-08-28 01:00:01 +0200

    XOR for bishop pair
    bench: 5956897

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1066,7 +1066,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   {
       int nnueComplexity;
 
-      int bishopPairs = (pos.count<BISHOP>(WHITE) >= 2) || (pos.count<BISHOP>(BLACK) >= 2);
+      int bishopPairs = (pos.count<BISHOP>(WHITE) >= 2) ^ (pos.count<BISHOP>(BLACK) >= 2);
       int imbalance   = (pos.count<ROOK>(WHITE) != pos.count<ROOK>(BLACK));
       int scale       = 1064 + 106 * pos.non_pawn_material() / 5120;
       Value optimism  = pos.this_thread()->optimism[stm];
