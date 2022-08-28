8ad168705523c0a66db9ba37e5819981abda3f77
Stéphane Nicolet <cassio@free.fr>
2022-08-28 08:44:11 +0200

    ent = 72 - m
    bench: 5920290

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -143,7 +143,7 @@ namespace Stockfish::Eval::NNUE {
     // overaligning stack variables with alignas() doesn't work correctly.
 
     constexpr uint64_t alignment = CacheLineSize;
-    int delta = 48 - pos.non_pawn_material() / 9560;
+    int delta = 72 - pos.non_pawn_material() / 9560;
 
 #if defined(ALIGNAS_ON_STACK_VARIABLES_BROKEN)
     TransformedFeatureType transformedFeaturesUnaligned[
