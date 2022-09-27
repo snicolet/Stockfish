f696f62e761f0551f6da38e925b5d89cbb171676
Stéphane Nicolet <cassio@free.fr>
2022-09-27 09:13:12 +0200

    probcut margin = 400
    bench: 4497148

--- src/search.cpp
+++ src/search.cpp
@@ -842,7 +842,7 @@ namespace {
         }
     }
 
-    probCutBeta = beta + 375 - 54 * improving;
+    probCutBeta = beta + 400 - 54 * improving;
 
     // Step 10. ProbCut (~4 Elo)
     // If we have a good enough capture and a reduced search returns a value
