f52b7ac7e8bf9a9b695784acea2d52b9ce9fa359
Stéphane Nicolet <cassio@free.fr>
2022-09-27 16:02:15 +0200

    simplify improving flag from probcut margin (225)
    bench: 4333219

--- src/search.cpp
+++ src/search.cpp
@@ -842,7 +842,7 @@ namespace {
         }
     }
 
-    probCutBeta = beta + 197;
+    probCutBeta = beta + 225;
 
     // Step 10. ProbCut (~4 Elo)
     // If we have a good enough capture and a reduced search returns a value
