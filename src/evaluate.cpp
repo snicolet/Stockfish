04fcd50629b444b9c4e476592d316b95e6cfb8b0
Stéphane Nicolet <cassio@free.fr>
2022-09-30 00:49:13 +0200

    take 2
    bench: 4124821

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1058,7 +1058,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   // option is set to false. Otherwise we use the NNUE eval unless the
   // PSQ advantage is decisive and several pieces remain (~3 Elo)
   bool useClassical =    !useNNUE 
-                      || (pos.count<ALL_PIECES>() > 7 && abs(psq) > 1500 + 64 * pos.rule50_count());
+                      || (pos.count<ALL_PIECES>() > 7 && abs(psq) > 1500 + 32 * pos.rule50_count());
   if (useClassical)
       v = Evaluation<NO_TRACE>(pos).value();
   else
