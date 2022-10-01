1b388c7b2294e753129c229dc1f0ebe84a3d9181
Stéphane Nicolet <cassio@free.fr>
2022-10-02 01:46:47 +0200

    Prefer captures towards the center
    bench: 4680102

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -106,10 +106,10 @@ void MovePicker::score() {
 
   static_assert(Type == CAPTURES || Type == QUIETS || Type == EVASIONS, "Wrong type");
 
+  [[maybe_unused]] Color us = pos.side_to_move();
   [[maybe_unused]] Bitboard threatened, threatenedByPawn, threatenedByMinor, threatenedByRook;
   if constexpr (Type == QUIETS)
   {
-      Color us = pos.side_to_move();
       // squares threatened by pawns
       threatenedByPawn  = pos.attacks_by<PAWN>(~us);
       // squares threatened by minors or pawns
@@ -126,7 +126,8 @@ void MovePicker::score() {
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
           m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
-                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
+                   - 64 * distance(relative_square(us, to_sq(m)), relative_square(us, SQ_E1));
 
       else if constexpr (Type == QUIETS)
           m.value =  2 * (*mainHistory)[pos.side_to_move()][from_to(m)]
