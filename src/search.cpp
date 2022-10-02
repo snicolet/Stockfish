eee4eb13b73502ba62a5da82829e81ebedcaf49a
Stéphane Nicolet <cassio@free.fr>
2022-10-02 09:41:25 +0200

    Skip step 14 at PvNode
    bench: 4290778

--- src/search.cpp
+++ src/search.cpp
@@ -988,7 +988,8 @@ moves_loop: // When in check, search starts here
       Value delta = beta - alpha;
 
       // Step 14. Pruning at shallow depth (~98 Elo). Depth conditions are important for mate finding.
-      if (  !rootNode
+      if (   !rootNode
+          && !PvNode
           && pos.non_pawn_material(us)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
