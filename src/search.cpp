c14a8d9bcce6f04684d12b4dac1b5dd16ba67e93
Stéphane Nicolet <cassio@free.fr>
2022-10-03 23:45:01 +0200

    More aggressive moveCountPruning, take 3
    bench: 4108952

--- src/search.cpp
+++ src/search.cpp
@@ -993,7 +993,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~7 Elo)
-          moveCountPruning =  (!PvNode || depth < 10)
+          moveCountPruning =  !PvNode
                             && moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
