fe05b707f263c527ede4a9ca43db50504daf6791
Stéphane Nicolet <cassio@free.fr>
2022-10-03 22:57:22 +0200

    More aggressive moveCountPruning, take 2
    bench: 4465201

--- src/search.cpp
+++ src/search.cpp
@@ -75,8 +75,8 @@ namespace {
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
-    return improving ? (depth * depth) / 2
-                     : (depth * depth) / 3;
+    return improving ? 3 + (depth * depth) / 2
+                     : 3 + (depth * depth) / 3;
   }
 
   // History and stats update bonus, based on depth
