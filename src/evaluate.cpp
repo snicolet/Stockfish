e22adcbadb550e0c9c8c18e8e88c241155b02228
Stéphane Nicolet <cassio@free.fr>
2022-10-07 02:10:33 +0200

    Damp eval faster, and compensate (98 * material)
    bench: 4337195

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1065,8 +1065,8 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   else
   {
       int nnueComplexity;
-      int scale =  1060
-                 + 106 * pos.non_pawn_material() / 5120
+      int scale =  980
+                 + 98 * pos.non_pawn_material() / 5120
                  + 2 * pos.rule50_count();
 
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1082,7 +1082,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (195 - pos.rule50_count()) / 211;
+  v = v * (100 - pos.rule50_count()) / 100;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
