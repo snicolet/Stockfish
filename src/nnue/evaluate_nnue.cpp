1ddeb90ba50f1471cb29de800886f2cf9711b231
Stéphane Nicolet <cassio@free.fr>
2022-10-07 08:12:44 +0200

    Simplify away nnueComplexity (compensate 28)
    Simplify away nnueComplexity
    Compensate with delta=28-mat in nnue:evaluate()
    
    bench: 4330613

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -143,7 +143,7 @@ namespace Stockfish::Eval::NNUE {
     // overaligning stack variables with alignas() doesn't work correctly.
 
     constexpr uint64_t alignment = CacheLineSize;
-    int delta = 26 - pos.non_pawn_material() / 9560;
+    int delta = 28 - pos.non_pawn_material() / 9560;
 
 #if defined(ALIGNAS_ON_STACK_VARIABLES_BROKEN)
     TransformedFeatureType transformedFeaturesUnaligned[
