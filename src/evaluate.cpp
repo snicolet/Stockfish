18dd472f8341d259891fa345dfb81951e5e6cb98
Stéphane Nicolet <cassio@free.fr>
2022-10-09 09:02:52 +0200

    mix old and new, take 10
    bench: 4271024

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1073,8 +1073,8 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
 
       int s =    (stm == stockfish && optimism >= 0) ?  1
                : (stm == stockfish && optimism <= 0) ? -1
-               : (stm != stockfish && optimism >= 0) ? -1
-               : (stm != stockfish && optimism <= 0) ?  1
+               : (stm != stockfish && optimism >= 0) ?  1
+               : (stm != stockfish && optimism <= 0) ? -1
                :                                        0;
 
       // Blend nnue complexity with (semi)classical complexity
