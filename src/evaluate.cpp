8301362c33d4839d286bfa5978ed1cdb2fb5dc3e
Stéphane Nicolet <cassio@free.fr>
2022-10-10 23:33:26 +0200

    mix old and new, take 16
    bench: 4353795

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1071,11 +1071,11 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
       // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  832 * nnueComplexity
-                        + 848 * abs(psq - nnue)
-                        + int(optimism) * int(psq - nnue)
-                        + abs(optimism) * int(psq - nnue)
-                        ) / 2048;
+      nnueComplexity = (  1664 * nnueComplexity
+                        + 1696 * abs(psq - nnue)
+                        +    3 * int(optimism) * int(psq - nnue)
+                        +    1 * abs(optimism) * int(psq - nnue)
+                        ) / 4096;
 
       if (complexity) // Return hybrid NNUE complexity to caller
           *complexity = nnueComplexity;
