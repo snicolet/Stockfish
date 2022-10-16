1556b0f5b1b9ab0ab56edf25a44ac533661fd9a2
Stéphane Nicolet <cassio@free.fr>
2022-10-17 00:56:18 +0200

    add damp
    bench: 4616588

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1063,7 +1063,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   else
   {
       int nnueComplexity;
-      int scale = 1082
+      int scale = 1100
                  + 106 * pos.non_pawn_material() / 5120
                  -   8 * (pos.rule50_count() / 2);
 
@@ -1087,7 +1087,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (195 - pos.rule50_count()) / 211;
+  v = v * (195 - 2 * (pos.rule50_count() / 2)) / 211;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
