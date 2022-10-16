67d95035f7d041b70347773c621a27a7d4cbd71f
Stéphane Nicolet <cassio@free.fr>
2022-10-16 22:15:42 +0200

    use move count, not ply count, to damp down eval
    bench: 4446606

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1085,7 +1085,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (195 - pos.rule50_count()) / 211;
+  v = v * (195 - 2 * (pos.rule50_count() / 2)) / 211;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
