533f0d6347831a4d837d069fb8e9443f2d6a760b
Stéphane Nicolet <cassio@free.fr>
2022-10-17 12:51:13 +0200

    delay : 11 (by plies)
    bench: 4839451

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1065,7 +1065,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       int nnueComplexity;
       int scale = 1080                 
                  + 106 * pos.non_pawn_material() / 5120
-                 -   4 * abs( pos.rule50_count() - 9 );
+                 -   4 * abs(pos.rule50_count() - 11);
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
