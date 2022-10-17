2d5a8420992fa26b62f74ee8713bd1d6185594fa
Stéphane Nicolet <cassio@free.fr>
2022-10-17 11:41:28 +0200

    delay : 9, double effect
    bench: 4173569

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1065,7 +1065,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       int nnueComplexity;
       int scale = 1120                 
                  + 106 * pos.non_pawn_material() / 5120
-                 -  16 * (abs(pos.rule50_count() - 11) / 2);
+                 -  16 * (abs(pos.rule50_count() - 9) / 2);
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
