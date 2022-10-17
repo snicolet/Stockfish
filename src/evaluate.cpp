de93d70672bdae9d27899ff66d6d08cf62aa3156
Stéphane Nicolet <cassio@free.fr>
2022-10-17 10:04:40 +0200

    delay : 9
    bench: 4692901

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1065,7 +1065,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       int nnueComplexity;
       int scale = 1080                 
                  + 106 * pos.non_pawn_material() / 5120
-                 -   8 * (abs(pos.rule50_count() - 7) / 2);
+                 -   8 * (abs(pos.rule50_count() - 9) / 2);
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1087,7 +1087,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (195 - 2 * (pos.rule50_count() / 2)) / 211;
+  v = v * (195 - pos.rule50_count()) / 211;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
