0544557fe25bf1f034e50d4c50bf28ff0bd30fb5
Stéphane Nicolet <cassio@free.fr>
2022-10-18 15:55:55 +0200

    offset = 40
    bench: 4470584

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1073,7 +1073,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  416 * nnueComplexity
                         + 424 * abs(psq - nnue)
-                        + (optimism  > 0 ? int(optimism) * int(psq - nnue) : 0)
+                        + ((optimism + 40)  > 0 ? int(optimism + 40) * int(psq - nnue) : 0)
                         ) / 1024;
 
       // Return hybrid NNUE complexity to caller
