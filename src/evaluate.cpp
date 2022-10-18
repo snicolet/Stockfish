89b48501287dbb4f901c149d9f9b074e32867c87
Stéphane Nicolet <cassio@free.fr>
2022-10-18 09:02:17 +0200

    delay 9 with 104 * material
    bench: 4613616

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1064,7 +1064,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   {
       int nnueComplexity;
       int scale = 1080                 
-                 + 106 * pos.non_pawn_material() / 5120
+                 + 104 * pos.non_pawn_material() / 5120
                  -   8 * (abs(pos.rule50_count() - 9) / 2);
 
       Color stm = pos.side_to_move();
