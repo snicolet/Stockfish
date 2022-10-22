7f397b1ac0614701db1f35f07fee9671d216c77c
Stéphane Nicolet <cassio@free.fr>
2022-10-22 08:35:52 +0200

    Also try with clamp(factor, 0.5, 3.0)
    bench: 4419657

--- src/search.cpp
+++ src/search.cpp
@@ -473,8 +473,8 @@ void Thread::search() {
           double reduction = (1.4 + mainThread->previousTimeReduction) / (2.15 * timeReduction);
           double bestMoveInstability = 1 + 1.7 * totBestMoveChanges / Threads.size();
 
-          int complexity = std::abs(mainThread->complexityAverage.value());
-          double complexPosition = std::clamp(1.0 + (complexity - 261) / 1738.7, 0.5, 1.5);
+          int complexity = mainThread->complexityAverage.value();
+          double complexPosition = std::clamp(1.0 + (complexity - 261) / 1738.7, 0.5, 3.0);
 
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition;
 
