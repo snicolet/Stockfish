9fa2efbf1372e2ed2b2a17bcc7a74fd85f6dac0f
Stéphane Nicolet <cassio@free.fr>
2022-11-13 12:16:55 +0100

    shuffling with delay=11
    bench: 4628596

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1067,7 +1067,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
 
       int scale = 1080                 
                  + 106 * pos.non_pawn_material() / 5120
-                 -   8 * (abs(pos.shuffling() - 9) / 2);
+                 -   8 * (abs(pos.shuffling() - 11) / 2);
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1087,10 +1087,6 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       optimism = optimism * (269 + nnueComplexity) / 256;
       v = (nnue * scale + optimism * (scale - 754)) / 1024;
   }
-  
-  // Try to detect fortresses
-  int shuffling = std::min(pos.shuffling(), 512);
-  v = v * (512 - shuffling) / 512;
 
   //dbg_mean_of(shuffling);
   /*
