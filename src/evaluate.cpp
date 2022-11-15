49e10e8780de127ad422c3c5a525c5efd11f5e9f
Stéphane Nicolet <cassio@free.fr>
2022-11-15 16:01:44 +0100

    simulate EvalDiff = 100 (take 2)
    bench: 4401149

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1072,7 +1072,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
 
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  416 * nnueComplexity
-                        + 424 * abs(psq - nnue)
+                        + 224 * abs(psq - nnue)
                         + (optimism  > 0 ? int(optimism) * int(psq - nnue) : 0)
                         ) / 1024;
 
