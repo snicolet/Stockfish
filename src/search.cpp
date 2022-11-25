1cff8c0de7d390062d1e29e25ca75702524354ff
Stéphane Nicolet <cassio@free.fr>
2022-11-26 00:34:58 +0100

    Use distanceFromPv <= 4 for doDeeperSearch
    bench: 4448275

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,8 @@ moves_loop: // When in check, search starts here
           {
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
-              const bool doDeeperSearch = value > (alpha + 64 + 11 * (newDepth - d));
+              const bool doDeeperSearch =    value > (alpha + 64 + 11 * (newDepth - d))
+                                          || (ss+1)->distanceFromPv <= 4;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
               newDepth += doDeeperSearch - doShallowerSearch;
