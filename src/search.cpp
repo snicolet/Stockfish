82190cd01e8dca974feb2b464034ea81651a8e25
Stéphane Nicolet <cassio@free.fr>
2022-11-26 00:37:46 +0100

    Combo:  distanceFromPv <= 4 with pvdepth < 13 by Stefan
    bench: 3526615

--- src/search.cpp
+++ src/search.cpp
@@ -1290,7 +1290,7 @@ moves_loop: // When in check, search starts here
 
                   // Reduce other moves if we have found at least one score improvement
                   if (   depth > 1
-                      && depth < 6
+                      && depth < 13
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
                      depth -= 1;
