843b3054159de8611e9d5c025a4b6153c5a6d92f
Stéphane Nicolet <cassio@free.fr>
2023-02-13 10:48:08 +0100

    tweak complexity (400)
    bench: 3832362

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1072,7 +1072,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
 
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  406 * nnueComplexity
-                        + 424 * abs(psq - nnue)
+                        + 400 * abs(psq - nnue)
                         + int(optimism) * int(psq - nnue)
                         ) / 1024;
 
