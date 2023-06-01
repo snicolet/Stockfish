c5377f292d0f160915f2235f905eff89eca9d69d
Stéphane Nicolet <cassio@free.fr>
2023-06-01 14:31:54 +0200

    try 3 4 11 11
    bench: 2889061

--- src/search.cpp
+++ src/search.cpp
@@ -1321,10 +1321,11 @@ moves_loop: // When in check, search starts here
                   if (   depth > 1
                       && beta  <  14001
                       && value > -12754)
-                      depth -= ( depth == 2 ? 1 :
-                                 depth == 3 ? 2 :
-                                 depth < 12 ? 3 :
-                                              1);
+                      depth -= ( depth <= 3  ? 1 :
+                                 depth <= 4  ? 2 :
+                                 depth <= 11 ? 3 :
+                                 depth <= 11 ? 2 :
+                                               1);
 
                   assert(depth > 0);
                   alpha = value; // Update alpha! Always alpha < beta
