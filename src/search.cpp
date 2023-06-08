23c87aa36e8e2f6e5784294dfa2ee738f2bab932
Stéphane Nicolet <cassio@free.fr>
2023-06-08 21:00:48 +0200

    Try beta + 100
    bench : 2612499

--- src/search.cpp
+++ src/search.cpp
@@ -1072,7 +1072,7 @@ moves_loop: // When in check, search starts here
               else if (singularBeta >= beta)
                   return singularBeta;
 
-              else if (value >= beta)
+              else if (value >= beta + 100)
                   extension = -1;
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
