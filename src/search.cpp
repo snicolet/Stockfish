228b26e039a8bc527ac2e5644d9e52d6aadea8b6
Stéphane Nicolet <cassio@free.fr>
2023-06-08 16:03:29 +0200

    multi-cut tweak: take 3
    bench: 2633011

--- src/search.cpp
+++ src/search.cpp
@@ -1073,7 +1073,7 @@ moves_loop: // When in check, search starts here
                   return singularBeta;
 
               else if (value >= beta)
-                  extension = -3;
+                  extension = -1;
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
               else if (ttValue >= beta)
