d04be83612b6b532f9d8e8982dddb6ef468cfb5c
Stéphane Nicolet <Stephane.Nicolet@u-paris2.fr>
2023-06-10 00:28:00 +0200

    tweak best version with extension = -3 even at PVNode
    bench : 2642342

--- src/search.cpp
+++ src/search.cpp
@@ -1072,12 +1072,12 @@ moves_loop: // When in check, search starts here
               else if (singularBeta >= beta)
                   extension = -3;
 
-              else if (value >= beta + 30)
+              else if (value >= beta + 50)
                   extension = -3;
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
               else if (ttValue >= beta)
-                  extension = -2 - !PvNode;
+                  extension = -3;
 
               // If the eval of ttMove is less than value, we reduce it (negative extension) (~1 Elo)
               else if (ttValue <= value)
