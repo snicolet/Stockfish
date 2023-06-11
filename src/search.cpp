d6a3ec0c8262357ca2fe95a6f501da3192092bdd
Stéphane Nicolet <Stephane.Nicolet@u-paris2.fr>
2023-06-11 15:34:31 +0200

    take 8
    bench: 2744693

--- src/search.cpp
+++ src/search.cpp
@@ -1091,6 +1091,9 @@ moves_loop: // When in check, search starts here
               else if (singularBeta >= beta)
                   return singularBeta;
 
+              else if (value >= beta + 50)
+                  extension = -5;
+
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
               else if (ttValue >= beta)
                   extension = -2 - !PvNode;
