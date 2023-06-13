cdbb38521d2ed14c8c0cec2bd7d406548a014cbf
Stéphane Nicolet <Stephane.Nicolet@u-paris2.fr>
2023-06-13 16:56:01 +0200

    On singular reductions, reduce every siblings
    bench: 2552831

--- src/search.cpp
+++ src/search.cpp
@@ -1123,6 +1123,8 @@ moves_loop: // When in check, search starts here
       // Add extension to new depth
       newDepth += extension;
       ss->doubleExtensions = (ss-1)->doubleExtensions + (extension == 2);
+      if (extension < 0)
+          depth = std::clamp(depth + extension, 1, MAX_PLY - 1);
 
       // Speculative prefetch as early as possible
       prefetch(TT.first_entry(pos.key_after(move)));
