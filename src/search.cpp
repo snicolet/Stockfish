674055718b682ccd1309c620432eb107633fcb95
Stéphane Nicolet <cassio@free.fr>
2023-06-16 16:19:09 +0200

    simpler tweak
    bench: 2724498

--- src/search.cpp
+++ src/search.cpp
@@ -1065,7 +1065,7 @@ moves_loop: // When in check, search starts here
               &&  tte->depth() >= depth - 3)
           {
               int margin =   depth * depth / 32
-                           + (64 + 52 * (ss->ttPv && !PvNode)) * depth / 64;
+                           + (1 + (ss->ttPv && !PvNode)) * depth;
               Value singularBeta = ttValue - margin;
               Depth singularDepth = (depth - 1) / 2;
 
