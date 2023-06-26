e7c0bcc0916064cb5f36a0490fc0d83747518402
Stéphane Nicolet <cassio@free.fr>
2023-06-26 08:13:01 +0200

    Additive malus instead of multiplicative (linear)
    bench: 2691422

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1074,7 +1074,9 @@ Value Eval::evaluate(const Position& pos) {
 
       // Blend optimism with nnue complexity and (semi)classical complexity
       optimism += optimism * (nnueComplexity + abs(psq - nnue)) / 512;
-      v = (nnue * (950 + npm - shuffling) + optimism * (150 + npm)) / 1024;
+      v = (nnue * (950 + npm) + optimism * (150 + npm)) / 1024;
+
+      v = v - shuffling * ((v > 0) - (v < 0));
   }
 
   // Damp down the evaluation linearly when shuffling
