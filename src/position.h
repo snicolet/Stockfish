0819c7afe3f650aa3280fb95118bb1cd55e04089
Stéphane Nicolet <cassio@free.fr>
2023-07-06 09:20:23 +0200

    try GHI offset = 13
    bench: 2706946

--- src/position.h
+++ src/position.h
@@ -334,8 +334,8 @@ inline Key Position::key() const {
 template<bool AfterMove>
 inline Key Position::adjust_key50(Key k) const
 {
-  return st->rule50 < 12 - AfterMove
-      ? k : k ^ make_key((st->rule50 - (12 - AfterMove)) / 8);
+  return st->rule50 < 13 - AfterMove
+      ? k : k ^ make_key((st->rule50 - (13 - AfterMove)) / 8);
 }
 
 inline Key Position::pawn_key() const {
