77095b8be1125f4e3bc41195c049520de8dd20b6
Stéphane Nicolet <cassio@free.fr>
2023-07-14 11:54:31 +0200

    at 16 with GIP = 15
    bench: 1530679

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -156,7 +156,7 @@ Value Eval::evaluate(const Position& pos) {
 
   bool Stockfish_is_winning = (stm == pos.this_thread()->rootColor) == (nnue > 0);
   int shuffling = Stockfish_is_winning ?  0
-                                       :  11 * pos.rule50_count();
+                                       :  16 * pos.rule50_count();
 
   // Blend optimism with nnue complexity and (semi)classical complexity
   optimism += optimism * (nnueComplexity + abs(psq - nnue)) / 512;
