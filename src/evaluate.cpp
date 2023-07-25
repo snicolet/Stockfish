f56426e1ec56ac670ae1e05ca2ae8df9ef463cb5
Stéphane Nicolet <cassio@free.fr>
2023-07-25 19:16:11 +0200

    seek active defense, take 3
    bench: 1626701

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -156,12 +156,12 @@ Value Eval::evaluate(const Position& pos) {
   bool Stockfish_is_winning = (stm == pos.this_thread()->rootColor) == (nnue > 0);
 
   int offset =  Stockfish_is_winning ?  0  :
-                nnue < 0             ?  2 * cnt :
-                nnue > 0             ? -2 * cnt :
+                nnue < 0             ?  cnt :
+                nnue > 0             ? -cnt :
                                         0  ;
 
   int shuffling = Stockfish_is_winning ?     -pos.rule50_count()
-                                       :  4 * pos.rule50_count();
+                                       :  2 * pos.rule50_count();
 
   // Blend optimism with nnue complexity and (semi)classical complexity
   optimism += optimism * (nnueComplexity + abs(psq - nnue)) / 512;
