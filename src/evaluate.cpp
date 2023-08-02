3d5ec7666d46136bc60f60de7063cbf147957cde
Stéphane Nicolet <cassio@free.fr>
2023-08-02 12:27:31 +0200

    coeffs tuned by hand, take 3
    bench: 1661437

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -160,7 +160,7 @@ Value Eval::evaluate(const Position& pos) {
 
   // Blend optimism with nnue complexity and (semi)classical complexity
   optimism += optimism * (nnueComplexity + abs(material - nnue)) / 512;
-  nnue     -= nnue     * (nnueComplexity + abs(material - nnue)) / 32768;
+  nnue     -= nnue     * (nnueComplexity + abs(material - nnue)) / 16384;
 
   v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
        + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
