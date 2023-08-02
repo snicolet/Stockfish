84d20727d846c513f50eadedb09c5728266e806e
Stéphane Nicolet <cassio@free.fr>
2023-08-02 11:58:28 +0200

    coeffs tuned by hand, take 1
    bench: 1700831

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -153,13 +153,14 @@ Value Eval::evaluate(const Position& pos) {
   Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
   int material =   67 * (pos.count<PAWN>(stm)   - pos.count<PAWN>(~stm))
-                + 395 * (pos.count<KNIGHT>(stm) - pos.count<KNIGHT>(~stm))
+                + 205 * (pos.count<KNIGHT>(stm) - pos.count<KNIGHT>(~stm))
                 + 288 * (pos.count<BISHOP>(stm) - pos.count<BISHOP>(~stm))
                 + 630 * (pos.count<ROOK>(stm)   - pos.count<ROOK>(~stm))
-                + 857 * (pos.count<QUEEN>(stm)  - pos.count<QUEEN>(~stm));
+                + 908 * (pos.count<QUEEN>(stm)  - pos.count<QUEEN>(~stm));
 
   // Blend optimism with nnue complexity and (semi)classical complexity
   optimism += optimism * (nnueComplexity + abs(material - nnue)) / 512;
+  nnue     -= nnue     * (nnueComplexity + abs(material - nnue)) / 65536;
 
   v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
        + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
