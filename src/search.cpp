4b59ecc77b104fcf31f73ef24b53216a95159c05
Stéphane Nicolet <cassio@free.fr>
2023-08-06 23:57:24 +0200

    Futility (parent node): do not prune, but reduce by 2
    bench: 1917949

--- src/search.cpp
+++ src/search.cpp
@@ -1032,7 +1032,7 @@ moves_loop: // When in check, search starts here
               if (   !ss->inCheck
                   && lmrDepth < 12
                   && ss->staticEval + 112 + 138 * lmrDepth <= alpha)
-                  extension -= 3;
+                  extension -= 2;
 
               lmrDepth = std::max(lmrDepth, 0);
 
