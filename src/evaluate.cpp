cc6f7043f729771b8f60ee7b860f125c7a8e12bb
Stéphane Nicolet <cassio@free.fr>
2023-08-14 01:23:32 +0200

    materialistic: take 1
    bench: 1605744

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -152,7 +152,10 @@ Value Eval::evaluate(const Position& pos) {
 
   Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
-  int material = ((nnue > PawnValue) - (nnue < -PawnValue)) * 400;
+  int material =  pos.non_pawn_material(stm) - pos.non_pawn_material(~stm)
+                + 128 * (pos.count<PAWN>(stm) - pos.count<PAWN>(~stm));
+
+  nnue += material / 64;
 
   // Blend optimism with nnue complexity and (semi)classical complexity
   optimism += optimism * (nnueComplexity + abs(material - nnue)) / 512;
