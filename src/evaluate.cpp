8207ffe899cfbfa16ab4431d30b3eb685fdc4d62
Stéphane Nicolet <cassio@free.fr>
2023-08-13 20:13:18 +0200

    take 2
    bench: 1443228

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -152,10 +152,8 @@ Value Eval::evaluate(const Position& pos) {
 
   Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
-  int material =  pos.non_pawn_material(stm) - pos.non_pawn_material(~stm)
-                + 126 * (pos.count<PAWN>(stm) - pos.count<PAWN>(~stm));
-
   // Blend optimism with nnue complexity and (semi)classical complexity
+  int material = ((nnue > 100) - (nnue < -100)) * 600;
   optimism += optimism * (nnueComplexity + abs(material - nnue)) / 512;
   nnue     -= nnue     * (nnueComplexity + abs(material - nnue)) / 65536;
 
