8a18092dde17a83a45e24be5914e091f4aef63e9
Stéphane Nicolet <cassio@free.fr>
2023-08-14 22:07:55 +0200

    Play turbulent when defending (take 3, don't touch attack)
    bench: 1645042

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -152,14 +152,18 @@ Value Eval::evaluate(const Position& pos) {
 
   Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
-  int material = ((nnue > PawnValue) - (nnue < -PawnValue)) * 300;
+  int material =  pos.non_pawn_material(stm) - pos.non_pawn_material(~stm)
+                + 126 * (pos.count<PAWN>(stm) - pos.count<PAWN>(~stm));
 
   // Blend optimism with nnue complexity and (semi)classical complexity
   optimism += optimism * (nnueComplexity + abs(material - nnue)) / 512;
 
-  bool Stockfish_is_winning = (stm == pos.this_thread()->rootColor) == (nnue > 0);
-  if (!Stockfish_is_winning)
-      nnue -= nnue * (nnueComplexity + abs(material - nnue)) / 32768;
+  bool Stockfish_is_losing = (stm != pos.this_thread()->rootColor) == (nnue > 0);
+  if (Stockfish_is_losing)
+  {
+      int material2 = ((nnue > PawnValue) - (nnue < -PawnValue)) * 300;
+      nnue -= nnue * (nnueComplexity + abs(material2 - nnue)) / 65536;
+  }
 
   v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
        + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
