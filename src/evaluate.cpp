869446dbc9f48e66aee2efb8102ed50820de091a
Stéphane Nicolet <cassio@free.fr>
2023-08-14 15:32:41 +0200

    take 5 with material target = 350

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -152,10 +152,11 @@ Value Eval::evaluate(const Position& pos) {
 
   Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
-  int material = ((nnue > PawnValue) - (nnue < -PawnValue)) * 300;
+  int material = ((nnue > PawnValue) - (nnue < -PawnValue)) * 350;
 
   // Blend optimism with nnue complexity and (semi)classical complexity
   optimism += optimism * (nnueComplexity + abs(material - nnue)) / 512;
+  nnue     -= nnue     * (nnueComplexity + abs(material - nnue)) / 65536;
 
   v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
        + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
