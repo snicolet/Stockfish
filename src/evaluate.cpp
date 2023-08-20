c21aadc114cae760108c25c4a9bc9458614c6b2d
Stéphane Nicolet <cassio@free.fr>
2023-08-20 19:54:36 +0200

    Play turbulent when defending (take 6bis, quadruple effect)
    bench: 1533553

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -160,7 +160,7 @@ Value Eval::evaluate(const Position& pos) {
 
   bool Stockfish_is_losing = (stm != pos.this_thread()->rootColor) == (nnue > 0);
   if (Stockfish_is_losing)
-      nnue -= nnue * (nnueComplexity + abs(material - nnue)) / 32768;
+      nnue -= nnue * (nnueComplexity + abs(material - nnue)) / 16384;
 
   v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
        + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
