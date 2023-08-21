93e095717b58551b659a71a35adc99968306451c
Stéphane Nicolet <cassio@free.fr>
2023-08-21 07:38:10 +0200

    mix, take 1
    bench: 1590996

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -161,6 +161,8 @@ Value Eval::evaluate(const Position& pos) {
   bool Stockfish_is_losing = (stm != pos.this_thread()->rootColor) == (nnue > 0);
   if (Stockfish_is_losing)
       nnue -= nnue * abs(material - nnue) / 16384;
+  else
+      nnue -= nnue * nnueComplexity / 16384;
 
   v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
        + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
