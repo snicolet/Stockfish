e8437917834780a23df37ce8700f7a079e6b22b6
Stéphane Nicolet <cassio@free.fr>
2023-08-22 23:10:52 +0200

    mix, take 4 (rebased)
    bench: 1684254

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -158,6 +158,12 @@ Value Eval::evaluate(const Position& pos) {
   // Blend optimism with nnue complexity and (semi)classical complexity
   optimism += optimism * (nnueComplexity + abs(material - nnue)) / 512;
 
+  bool Stockfish_is_losing = (stm != pos.this_thread()->rootColor) == (nnue > 0);
+  if (Stockfish_is_losing)
+      nnue -= nnue * (nnueComplexity + abs(material - nnue)) / 32768;
+  else
+      nnue -= nnue * nnueComplexity / 16384;
+
   v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
        + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
 
