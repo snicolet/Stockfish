406b0d7eb700e38071fd4f5a61518705038cf042
Stéphane Nicolet <cassio@free.fr>
2023-08-26 21:04:07 +0200

    add explicit material imbalance to complexity
    bench: 1527983

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -156,8 +156,13 @@ Value Eval::evaluate(const Position& pos) {
                 + 126 * (pos.count<PAWN>(stm) - pos.count<PAWN>(~stm));
 
   // Blend optimism and eval with nnue complexity and material imbalance
-  optimism += optimism * (nnueComplexity + abs(material - nnue)) / 512;
-  nnue     -= nnue     * (nnueComplexity + abs(material - nnue)) / 32768;
+
+  int complexity =   nnueComplexity 
+                   + abs(material - nnue)
+                   + abs(pos.non_pawn_material(stm) - pos.non_pawn_material(~stm));
+
+  optimism += optimism * complexity / 512;
+  nnue     -= nnue     * complexity / 32768;
 
   v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
        + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
