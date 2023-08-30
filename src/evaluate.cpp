7b549cf555d88ae3e6aae5f60d647611a2dfcbe2
Stéphane Nicolet <cassio@free.fr>
2023-08-30 12:58:42 +0200

    Use simple_eval() to do lazy evaluation at queen level
    bench: 1585657

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -160,7 +160,7 @@ Value Eval::evaluate(const Position& pos) {
   int simpleEval = simple_eval(pos, stm);
 
 
-  if (abs(simpleEval) >= RookValue + 16 * shuffling * shuffling)
+  if (abs(simpleEval) >= QueenValue + 16 * shuffling * shuffling)
       v = Value(simpleEval);
   else
   {
