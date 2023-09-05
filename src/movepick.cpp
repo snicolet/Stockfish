f61aa77a372c323b4dd17b534f1803876518465a
Stéphane Nicolet <cassio@free.fr>
2023-09-05 09:16:16 +0200

    tweak capture scoring for move ordering: (26, 4)
    bench: 1573884

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -127,7 +127,7 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
-          m.value =  32 * int(PieceValue[pos.piece_on(to_sq(m))])
+          m.value =  26 * int(PieceValue[pos.piece_on(to_sq(m))])
                    +  4 * (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
