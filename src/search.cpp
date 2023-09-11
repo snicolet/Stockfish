6adacf87eac286df27d6a21b70767868841ad518
Stéphane Nicolet <cassio@free.fr>
2023-09-11 10:48:39 +0200

    x0 = 72, y0 = 15
    bench: 1553005

--- src/search.cpp
+++ src/search.cpp
@@ -363,7 +363,7 @@ void Thread::search() {
           beta  = std::min(prev + delta, VALUE_INFINITE);
 
           // Adjust optimism based on root move's previousScore
-          int opt = -15 + 109 * (prev - 28) / (std::abs(prev - 28) + 141);
+          int opt = 15 + 109 * (prev - 72) / (std::abs(prev - 72) + 141);
           optimism[ us] = Value(opt);
           optimism[~us] = -optimism[us];
 
