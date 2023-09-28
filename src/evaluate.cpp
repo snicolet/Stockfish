130f81ecb73923a1091a2d01971b82dec282a931
Stéphane Nicolet <cassio@free.fr>
2023-09-28 07:39:39 +0200

    Try over-compensation, take 2
    bench: 1353240

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -180,7 +180,7 @@ Value Eval::evaluate(const Position& pos) {
 
       int npm = pos.non_pawn_material() / 64;
       v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
-           + optimism * (154 + npm -     pos.count<PAWN>())) / 1024;
+           + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
 
       v -= 5 * pos.this_thread()->tension[stm] / 4;
   }
