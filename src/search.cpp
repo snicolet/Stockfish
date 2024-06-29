957114c9c4169098cc4c78b01cf269c156d4ff6e
Stéphane Nicolet <Stephane.Nicolet@u-paris2.fr>
2024-06-29 20:32:31 +0200

    Try an Elo gainer (using Joost's fix)
    Try an Elo gainer by updating alpha to VALUE_DRAW if has_game_cycle() fires. Using Joost version of the fix for the mate scores, as it it the simpler version.
    
    Bench: 1185364

--- src/search.cpp
+++ src/search.cpp
@@ -536,9 +536,10 @@ Value Search::Worker::search(
 
     // Check if we have an upcoming move that draws by repetition, or
     // if the opponent had an alternative move earlier to this position.
-    if (!rootNode && alpha < VALUE_DRAW && pos.has_game_cycle(ss->ply))
+    if (!rootNode && alpha < VALUE_DRAW && alpha > VALUE_TB_LOSS_IN_MAX_PLY
+        && pos.has_game_cycle(ss->ply))
     {
-        alpha = value_draw(this->nodes);
+        alpha = VALUE_DRAW;
         if (alpha >= beta)
             return alpha;
     }
@@ -1420,9 +1421,9 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
 
     // Check if we have an upcoming move that draws by repetition, or if
     // the opponent had an alternative move earlier to this position. (~1 Elo)
-    if (alpha < VALUE_DRAW && pos.has_game_cycle(ss->ply))
+    if (alpha < VALUE_DRAW && alpha > VALUE_TB_LOSS_IN_MAX_PLY && pos.has_game_cycle(ss->ply))
     {
-        alpha = value_draw(this->nodes);
+        alpha = VALUE_DRAW;
         if (alpha >= beta)
             return alpha;
     }
