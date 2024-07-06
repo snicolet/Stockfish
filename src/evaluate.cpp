faba33887ce4c1c73338e32c363e017ab7c42783
Stéphane Nicolet <cassio@free.fr>
2024-07-06 10:16:20 +0200

    quantum = 16, take 3
    bench: 1336007

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -84,12 +84,12 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
 
     int material = 554 * pos.count<PAWN>() + pos.non_pawn_material();
     v            = (nnue * (73921 + material) + optimism * (8112 + material)) / 73260;
+    
+    // Quantum
+    v = (v / 16) * 16 - 1 + (pos.key() & 0x2);
 
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 212;
-
-    // Quantum
-    v = (v / 16) * 16;
 
     // Guarantee evaluation does not hit the tablebase range
     v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
