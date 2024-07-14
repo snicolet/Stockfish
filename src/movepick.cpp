cf4ab4b2e359e0d8d301391338d13f0f266c6e6d
Stéphane Nicolet <cassio@free.fr>
2024-07-14 19:10:07 +0200

    take 3 (1 * from, 1 * to)
    bench: 1448112

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -183,8 +183,8 @@ void MovePicker::score() {
             m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
 
-            //dbg_mean_of(m == killer);
-            m.value += (m == killer) * 2 * 65536;
+            m.value += (from == killer.from_sq()) * 65536;
+            m.value += (to == killer.to_sq()) * 65536;
 
             // bonus for checks
             m.value += bool(pos.check_squares(pt) & to) * 16384;
