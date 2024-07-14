916767a9caea31a9b89dd7d359cf4cbd95f6e7b2
Stéphane Nicolet <cassio@free.fr>
2024-07-14 23:34:46 +0200

    Take 6 (1 *to)
    bench: 1119938

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -184,7 +184,7 @@ void MovePicker::score() {
             m.value += (*continuationHistory[5])[pc][to];
 
             m.value += (m == killer) * 65536;
-            m.value += (from == killer.from_sq()) * 65536;
+            m.value += (to == killer.to_sq()) * 65536;
 
             // bonus for checks
             m.value += bool(pos.check_squares(pt) & to) * 16384;
