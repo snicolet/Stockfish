41ff91834c81814ce631b643202972c39f9397ea
Stéphane Nicolet <cassio@free.fr>
2024-07-15 10:48:15 +0200

    6 * killer, 3 * cont[0]
    bench: 1233059

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -177,7 +177,7 @@ void MovePicker::score() {
             // histories
             m.value = (*mainHistory)[pos.side_to_move()][m.from_to()];
             m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
-            m.value += 4 * (*continuationHistory[0])[pc][to];
+            m.value += 3 * (*continuationHistory[0])[pc][to];
             m.value += (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to];
             m.value += (*continuationHistory[3])[pc][to];
