2c8bddcd7a4d2f9d5793bc449b8e6aca058ec5ee
Stéphane Nicolet <cassio@free.fr>
2024-07-26 12:58:01 +0200

    Help good captures in qsearch movepicker (take 1, fixed)
    bench: 1151403

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -147,7 +147,12 @@ void MovePicker::score() {
               7 * int(PieceValue[pos.piece_on(m.to_sq())])
               + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))];
             
-            if (depth < 0 && !pos.see_ge(m, -m.value / 18))
+            
+            // if (depth <= 0)
+                // dbg_mean_of( 100 * (-m.value / 16) / PawnValue);
+                // dbg_mean_of( std::abs(m.value) );
+            
+            if (stage == QCAPTURE_INIT && depth <= 0 && !pos.see_ge(m, -m.value / 18))
                m.value -= 25000;
         }
 
