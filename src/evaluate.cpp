8107ab9b4d07294fa804c493f7fa51356965ab2a
Stéphane Nicolet <cassio@free.fr>
2025-06-14 12:55:31 +0200

    Exchange material (335)
    bench: 1991407

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -77,8 +77,8 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     optimism += optimism * nnueComplexity / 468;
     nnue -= nnue * nnueComplexity / 18000;
 
-    int material = 435 * pos.count<PAWN>() + pos.non_pawn_material();
-    int v        = (nnue * (77777 + material) + optimism * (7777 + material)) / 77777;
+    int material = 335 * pos.count<PAWN>() + pos.non_pawn_material();
+    int v        = (nnue * (77777 + 2 * material) + optimism * (7777 + material)) / 77777;
 
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 212;
