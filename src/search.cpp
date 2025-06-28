43f8e42c2ac877180454774803aa1e1054e556e8
Stéphane Nicolet <cassio@free.fr>
2025-06-28 14:49:23 +0200

    Explore losing singular moves, take 7
    bench: 2070109

--- src/search.cpp
+++ src/search.cpp
@@ -1298,10 +1298,10 @@ moves_loop:  // When in check, search starts here
         // explore this branch a little bit more in the future.
         if (   moveCount == 1
             && extension > 0
-            && value <  -50
+            && value <  -10
             && value > -2000
-            && value <= alpha - 25)
-           value += 20;
+            && value <= alpha - 6)
+           value = std::min(value + 40, alpha - 1);
 
         // Step 19. Undo move
         undo_move(pos, move);
