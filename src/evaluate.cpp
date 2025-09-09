14ebfa9c1e0e2c76616f48f7255aa75c6fdf8010
Stéphane Nicolet <cassio@free.fr>
2025-09-09 17:40:45 +0200

    delay = 512, with guard material >= 0
    bench: 3089011

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -77,7 +77,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     optimism += optimism * nnueComplexity / 468;
     nnue -= nnue * nnueComplexity / 18000;
 
-    int material = 535 * pos.count<PAWN>() + pos.non_pawn_material() - 256 * pos.rule50_count();
+    int material = 535 * pos.count<PAWN>() + pos.non_pawn_material() - 512 * pos.rule50_count();
     material = std::max(material, 0);
     int v        = (nnue * (77777 + material) + optimism * (7777 + material)) / 77777;
 
