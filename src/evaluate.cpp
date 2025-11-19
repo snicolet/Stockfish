9b93aa09e9f8ca8a167f264d145de826df194827
Stéphane Nicolet <cassio@free.fr>
2025-11-19 06:56:15 +0100

    take 6
    bench : 2906532

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -79,7 +79,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
 
     int pieces = pos.count<ALL_PIECES>();
     int material = 535 * pos.count<PAWN>() + pos.non_pawn_material();
-    material += 64 * pieces - 1024;
+    material += 64 * pieces - 512;
     material -= 16 * pos.rule50_count();
 
     int v        = (nnue * (77777 + material) + optimism * (7777 + material)) / 77777;
