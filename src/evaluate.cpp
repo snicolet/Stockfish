bdba474989ac3927bf60c26f24ebfeca43e0fb0b
Stéphane Nicolet <cassio@free.fr>
2026-01-15 21:17:46 +0100

    Tal style = 10 when simple*nnue > 0
    bench: 2178509

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -82,7 +82,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     // while negative weight for simpleEval is less materialistic (Tal style).
     int simpleEval = simple_eval(pos);
     int w = -10;
-    if (simpleEval * nnue < 0)
+    if (simpleEval * nnue > 0)
         nnue = ((128 - w) * nnue + w * simpleEval) / 128;
 
     int material = 534 * pos.count<PAWN>() + pos.non_pawn_material();
