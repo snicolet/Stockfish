89368c53f98b0663e62fd97d2bb68177614a4413
Stéphane Nicolet <cassio@free.fr>
2026-01-15 19:03:11 +0100

    Kortchnoi style = 3 when simple*nnue < 0
    bench: 2197691

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -81,8 +81,9 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     // Positive weight for simpleEval is more materialistic (Korchnoi style), 
     // while negative weight for simpleEval is less materialistic (Tal style).
     int simpleEval = simple_eval(pos);
-    int w = 3 * (nnue > 0);
-    nnue = ((128 - w) * nnue + w * simpleEval) / 128;
+    int w = 3;
+    if (simpleEval * nnue < 0)
+        nnue = ((128 - w) * nnue + w * simpleEval) / 128;
 
     int material = 534 * pos.count<PAWN>() + pos.non_pawn_material();
     int v        = (nnue * (77871 + material) + optimism * (7191 + material)) / 77871;
