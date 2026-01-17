f8e8f35125af8b75a1f0ac57c672ce5a180f96b3
Stéphane Nicolet <cassio@free.fr>
2026-01-17 12:42:01 +0100

    Tal style = 1 when simple*nnue < 0
    bench: 2223916

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -81,8 +81,8 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     // Positive weight for simpleEval is more materialistic (Korchnoi style), 
     // while negative weight for simpleEval is less materialistic (Tal style).
     int simpleEval = simple_eval(pos);
-    int w = -5;
-    if (simpleEval != 0)
+    int w = -1;
+    if (simpleEval * nnue < 0)
         nnue = ((128 - w) * nnue + w * 2 * simpleEval) / 128;
 
 
