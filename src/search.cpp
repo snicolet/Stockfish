3a5d15b27762cde3e742e1fcbae9cae60e49fa32
Stéphane Nicolet <cassio@free.fr>
2026-02-15 20:27:32 +0100

    SMP: speculative fail-low with risk = 2
    bench: 2515956

--- src/search.cpp
+++ src/search.cpp
@@ -848,7 +848,7 @@ Value Search::Worker::search(
     // If other thread(s) are exploring the node, the node is most probably
     // EXACT or LOWERBOUND, so we can take a fail-low from the transposition
     // table (with some level of risk).
-    int risk = 1;
+    int risk = 2;
     if (   held.by_other()
         && is_valid(ttData.value)
         && !is_decisive(ttData.value)
