6021b71db4307460549ee884069140431e4722eb
Stéphane Nicolet <cassio@free.fr>
2026-03-11 00:34:20 +0100

    Tweak doDeeperSearch and doShallowerSearch (Reckless)
    bench: 2516909

--- src/search.cpp
+++ src/search.cpp
@@ -1247,8 +1247,8 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = d < newDepth && value > bestValue + 50;
-                const bool doShallowerSearch = value < bestValue + 9;
+                const bool doDeeperSearch    = d < newDepth && value > bestValue + 50 + 4 * d;
+                const bool doShallowerSearch = value < bestValue + 5 + d;
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
