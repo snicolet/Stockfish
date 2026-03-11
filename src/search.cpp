195d958b433a5a4e7af3b3295862f4a3837ef73b
Stéphane Nicolet <cassio@free.fr>
2026-03-11 21:58:37 +0100

    mixed formula: take 1
    bench: 2344528

--- src/search.cpp
+++ src/search.cpp
@@ -995,7 +995,7 @@ Value Search::Worker::search(
                                probCutDepth + 1, move, unadjustedStaticEval, tt.generation());
 
                 if (!is_decisive(value))
-                    return (3 * value + beta) / 4;
+                    return (7 * value + 5 * beta - 4 * probCutBeta) / 8;
             }
         }
     }
