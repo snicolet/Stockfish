e7e3c1e07a6f3910a93572b825054d1d22094cde
Stéphane Nicolet <cassio@free.fr>
2026-03-15 15:59:12 +0100

    Scale eval less with material when SF is losing: take 2
    bench: 2869132

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -79,8 +79,9 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     nnue -= nnue * nnueComplexity / 18236;
 
     int material = 534 * pos.count<PAWN>() + pos.non_pawn_material();
-
-    if ((pos.side_to_move() == stockfish) == (nnue < 0))
+    
+    bool sf_is_losing = ((pos.side_to_move() == stockfish) == (nnue < 0));
+    if (sf_is_losing && abs(nnue) < 150)
         material = 0;
 
     int v = (nnue * (77871 + material) + optimism * (7191 + material)) / 77871;
