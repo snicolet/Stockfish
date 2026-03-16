d5b7f0ae12c7e6899229e6715a35ef3f0306fa65
Stéphane Nicolet <cassio@free.fr>
2026-03-16 06:25:29 +0100

    Add some noise to the eval (std dev = 12)
    bench: 2132003

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -77,6 +77,17 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     optimism += optimism * nnueComplexity / 476;
     nnue -= nnue * nnueComplexity / 18236;
 
+    // Create a quasi gaussian random variable by summing four uniform discrete variables
+    int64_t h = pos.key();
+    int64_t a = h & 0x00ff;
+    int64_t b = (h >> 16) & 0x00ff;
+    int64_t c = (h >> 32) & 0x00ff;
+    int64_t d = (h >> 48) & 0x00ff;
+    int64_t r = 2783 * (a + b + c + d - 510) / 4096;  // mean 0, std dev 100
+
+    // Add some noise to the eval
+    nnue += 12 * r / 100;
+
     int material = 534 * pos.count<PAWN>() + pos.non_pawn_material();
     int v        = (nnue * (77871 + material) + optimism * (7191 + material)) / 77871;
 
