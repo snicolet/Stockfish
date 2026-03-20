9112e314d321a93568825d900cc91f16cefb129b
Stéphane Nicolet <cassio@free.fr>
2026-03-20 09:14:47 +0100

    Standardize fail-firm in ProbCut: take 7
    bench: 3065270

--- src/search.cpp
+++ src/search.cpp
@@ -995,7 +995,7 @@ Value Search::Worker::search(
                                probCutDepth + 1, move, unadjustedStaticEval, tt.generation());
 
                 if (!is_decisive(value))
-                    return (value + probCutBeta) / 2;
+                    return (value + 2 * probCutBeta) / 3;
             }
         }
     }
