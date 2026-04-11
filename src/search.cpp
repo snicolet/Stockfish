980448a4da45131622a65b7fe77ec561ffdbf2f8
Stéphane Nicolet <cassio@free.fr>
2026-04-11 15:51:02 +0200

    Try values 15% into tune_risk3
    bench: 2729831

--- src/search.cpp
+++ src/search.cpp
@@ -96,8 +96,8 @@ int correction_value(const Worker& w, const Position& pos, const Stack* const ss
 
 // PruningSafety[rootColor][cut type] : pruning safety table
 const int PruningSafety[2][2] = {
-      {  10  ,  -9  },   //  rootColor : alpha, beta
-      {  -3  ,  10  }    // ~rootColor : alpha, beta
+      { -22  , -12  },   //  rootColor : alpha, beta
+      {  12  ,  0   }    // ~rootColor : alpha, beta
 };
 enum CutType { ALPHA, BETA };
 template <CutType T> 
