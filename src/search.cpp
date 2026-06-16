5fffc815b2c4f3f85028f75025de1ed8079f340e
Stéphane Nicolet <cassio@free.fr>
2026-06-17 01:14:37 +0200

    Take 3 (Rook + Bishop + Pawn)
    bench: 3007861

--- src/search.cpp
+++ src/search.cpp
@@ -1851,7 +1851,7 @@ Value Search::Worker::evaluate(const Position& pos) {
 
     int shuffling  = pos.rule50_count();
     int simpleEval = Eval::simple_eval(pos) + (int(pos.key() & 7) - 3);
-    bool lazy = abs(simpleEval) >=   RookValue + BishopValue
+    bool lazy = abs(simpleEval) >=   RookValue + BishopValue + PawnValue
                                    + 16 * shuffling * shuffling
                                    + abs(rootBestValue)
                                    + abs(rootSimpleEval);
